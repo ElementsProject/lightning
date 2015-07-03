f510e29e22f146ebf5f087722b2f428e84f691a9
Rusty Russell <rusty@rustcorp.com.au>
2015-07-03 13:36:25 +0930

    alpha: switch over to Elements Alpha by default.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- test-cli/scripts/vars.sh
+++ test-cli/scripts/vars.sh
@@ -1,14 +1,14 @@
 # Sourced by other scripts
-STYLE=bitcoin
-CLI="bitcoin-cli -regtest"
-DATADIR=$HOME/.bitcoin
-REGTESTDIR=regtest
-DAEMON=bitcoind
+#STYLE=bitcoin
+#CLI="bitcoin-cli -regtest"
+#DATADIR=$HOME/.bitcoin
+#REGTESTDIR=regtest
+#DAEMON=bitcoind
 
-#STYLE=alpha
-#DATADIR=$HOME/.alpha
-#REGTESTDIR=alpharegtest
-#CLI="alpha-cli -datadir=$DATADIR -regtest -testnet=0"
-#DAEMON="alphad -datadir=$DATADIR"
+STYLE=alpha
+DATADIR=$HOME/.alpha
+REGTESTDIR=alpharegtest
+CLI="alpha-cli -datadir=$DATADIR -regtest -testnet=0"
+DAEMON="alphad -datadir=$DATADIR"
 
 #PREFIX="valgrind --vgdb-error=1"
