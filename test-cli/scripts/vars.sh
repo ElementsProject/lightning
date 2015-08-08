0b33da08c16ac8e5e33b6034dea346667fa0bc5a
Rusty Russell <rusty@rustcorp.com.au>
2015-08-08 15:02:19 +0930

    scripts: auto-detect alpha vs bitcoin
    A bit hacky, but saves editing two places.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- test-cli/scripts/vars.sh
+++ test-cli/scripts/vars.sh
@@ -1,14 +1,17 @@
 # Sourced by other scripts
-#STYLE=bitcoin
-#CLI="bitcoin-cli -regtest"
-#DATADIR=$HOME/.bitcoin
-#REGTESTDIR=regtest
-#DAEMON=bitcoind
 
-STYLE=alpha
-DATADIR=$HOME/.alpha
-REGTESTDIR=alpharegtest
-CLI="alpha-cli -datadir=$DATADIR -regtest -testnet=0"
-DAEMON="alphad -datadir=$DATADIR"
+if grep ^FEATURES.*ALPHA ../Makefile; then
+    STYLE=alpha
+    DATADIR=$HOME/.alpha
+    REGTESTDIR=alpharegtest
+    CLI="alpha-cli -datadir=$DATADIR -regtest -testnet=0"
+    DAEMON="alphad -datadir=$DATADIR"
+else
+    STYLE=bitcoin
+    CLI="bitcoin-cli -regtest"
+    DATADIR=$HOME/.bitcoin
+    REGTESTDIR=regtest
+    DAEMON="bitcoind -regtest"
+fi
 
 #PREFIX="valgrind --vgdb-error=1"
