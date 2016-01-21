6443629f6adf7a5be8b213fdfa3435d5d1f310bc
Rusty Russell <rusty@rustcorp.com.au>
2016-01-22 06:45:27 +1030

    daemon/test/test.sh: add --valgrind and --gdb options.
    We add a second to the sleep, as valgrind can be a little slow.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- daemon/test/test.sh
+++ daemon/test/test.sh
@@ -10,13 +10,29 @@ scripts/setup.sh
 DIR1=/tmp/lightning.$$.1
 DIR2=/tmp/lightning.$$.2
 
+REDIR1="$DIR1/output"
+REDIR2="$DIR2/output"
+
+if [ x"$1" = x"--valgrind" ]; then
+    PREFIX1="valgrind --vgdb-error=1"
+    PREFIX2="valgrind --vgdb-error=1"
+    REDIR1="/dev/tty"
+    REDIR2="/dev/tty"
+elif [ x"$1" = x"--gdb1" ]; then
+    PREFIX1="gdb --args -ex run"
+    REDIR1="/dev/tty"
+elif [ x"$1" = x"--gdb2" ]; then
+    PREFIX2="gdb --args -ex run"
+    REDIR2="/dev/tty"
+fi
+
 LCLI1="../daemon/lightning-cli --lightning-dir=$DIR1"
 LCLI2="../daemon/lightning-cli --lightning-dir=$DIR2"
 
 trap "echo Results in $DIR1 and $DIR2" EXIT
 mkdir $DIR1 $DIR2
-../daemon/lightningd --log-level=debug --lightning-dir=$DIR1 > $DIR1/output &
-../daemon/lightningd --log-level=debug --lightning-dir=$DIR2 > $DIR2/output &
+$PREFIX1 ../daemon/lightningd --log-level=debug --lightning-dir=$DIR1 > $REDIR1 &
+$PREFIX2 ../daemon/lightningd --log-level=debug --lightning-dir=$DIR2 > $REDIR2 &
 
 i=0
 while ! $LCLI1 getlog | grep Hello; do
@@ -40,7 +56,7 @@ done
 PORT2=`$LCLI2 getlog | sed -n 's/.*on port \([0-9]*\).*/\1/p'`
 
 $LCLI1 connect localhost $PORT2 999999
-sleep 1
+sleep 2
 
 # Expect them to be waiting for anchor.
 $LCLI1 getpeers | grep STATE_OPEN_WAITING_OURANCHOR
