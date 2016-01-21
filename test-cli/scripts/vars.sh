e272d759a5e3cfab3a8f5000bcdb190d140696b0
Rusty Russell <rusty@rustcorp.com.au>
2016-01-22 06:38:08 +1030

    Force -walletbroadcast=1 for tests.
    Since lightningd wants walletbroadcast=0, override it on cmdline.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- test-cli/scripts/vars.sh
+++ test-cli/scripts/vars.sh
@@ -5,14 +5,14 @@ if grep -q ^FEATURES.*ALPHA ../Makefile; then
     DATADIR=$HOME/.alpha
     REGTESTDIR=alpharegtest
     CLI="alpha-cli -datadir=$DATADIR -regtest -testnet=0"
-    DAEMON="alphad -datadir=$DATADIR"
+    DAEMON="alphad -datadir=$DATADIR -regtest -testnet=0 -walletbroadcast=1"
     SEQ_ENFORCEMENT=true
 else
     STYLE=bitcoin
-    CLI="bitcoin-cli -regtest"
+    CLI="bitcoin-cli -regtest -testnet=0"
     DATADIR=$HOME/.bitcoin
     REGTESTDIR=regtest
-    DAEMON="bitcoind -regtest"
+    DAEMON="bitcoind -regtest -testnet=0 -walletbroadcast=1"
     if grep ^FEATURES ../Makefile | cut -d'#' -f1 | grep -q BIP68; then
 	SEQ_ENFORCEMENT=true
     else
