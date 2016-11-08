9b045dac024af82f02a15bc5efc9638cb9c11263
Rusty Russell <rusty@rustcorp.com.au>
2016-11-08 21:55:55 +1030

    test: shutdown harder.
    Sometimes bitcoind takes a while to shutdown.  Just kill it.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- daemon/test/scripts/shutdown.sh
+++ daemon/test/scripts/shutdown.sh
@@ -2,5 +2,10 @@
 
 . `dirname $0`/vars.sh
 
+[ ! -f $DATADIR/regtest/bitcoind.pid ] || BITCOIN_PID=`cat $DATADIR/regtest/bitcoind.pid`
+
 $CLI stop
 sleep 1 # Make sure socket is closed.
+
+# Now make sure it's dead.
+if [ -n "$BITCOIN_PID" ]; then kill -9 $BITCOIN_PID 2>/dev/null || true; fi
