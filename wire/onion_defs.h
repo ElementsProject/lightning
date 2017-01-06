d8feec21972369004baf118ddffd1035e1c8d845
Rusty Russell <rusty@rustcorp.com.au>
2017-01-06 13:40:35 +1030

    wire: split onion messages from peer wire messages.
    The recent update to BOLT #4 includes failure message specifications,
    which are completely orthogonal to the normal ones.  Don't include
    them in the gen_peer_wire_csv.
    
    This onion_defs.h file assumes we are using 2-byte failure codes as per
    the onion-failmsg-cleanup branch.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ wire/onion_defs.h
@@ -0,0 +1,18 @@
+/* Macro definitions for constants used in BOLT #4 */
+#ifndef LIGHTNING_WIRE_ONION_DEFS_H
+#define LIGHTNING_WIRE_ONION_DEFS_H
+
+/* BOLT #4:
+ *
+ * The top byte of `failure-code` can be read as a set of flags:
+ * * 0x8000 (BADONION): unparsable onion, encrypted by previous node.
+ * * 0x4000 (PERM): permanent failure (otherwise transient)
+ * * 0x2000 (NODE): node failure (otherwise channel)
+ * * 0x1000 (UPDATE): new channel update enclosed
+ */
+#define BADONION	0x8000
+#define PERM		0x4000
+#define NODE		0x2000
+#define UPDATE		0x1000
+
+#endif /* LIGHTNING_WIRE_ONION_DEFS_H */
