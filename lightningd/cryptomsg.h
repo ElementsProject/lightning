597687d1f0abbc3452053a1810d278a0cb72e607
Christian Decker <decker.christian@gmail.com>
2017-02-21 17:56:35 +0100

    Fix CI build
    Extraneous whitespace in cryptomsg.h

--- lightningd/cryptomsg.h
+++ lightningd/cryptomsg.h
@@ -49,7 +49,7 @@ struct io_plan *peer_write_message(struct io_conn *conn,
 
 void towire_crypto_state(u8 **pptr, const struct crypto_state *cs);
 void fromwire_crypto_state(const u8 **ptr, size_t *max, struct crypto_state *cs);
- 
+
 /* Low-level functions for sync comms. */
 u8 *cryptomsg_encrypt_msg(const tal_t *ctx,
 			  struct crypto_state *cs,
