7297d7d5174424b043923516d82a324b0b8c74c8
Christian Decker <decker.christian@gmail.com>
2017-03-15 13:36:35 +0100

    jsonrpc: Added nested messages for the getroute reply
    The `route_hop` struct introduced in the previous refactoring is
    reused when returning the reply to a `getroute` request. Since these
    are nested messages I added the serialization and deserialization
    methods.

--- lightningd/gossip_msg.h
+++ lightningd/gossip_msg.h
@@ -2,6 +2,7 @@
 #define LIGHTNING_LIGHTNINGD_GOSSIP_MSG_H
 #include "config.h"
 #include <bitcoin/pubkey.h>
+#include <daemon/routing.h>
 
 struct gossip_getnodes_entry {
 	struct pubkey nodeid;
@@ -11,5 +12,8 @@ struct gossip_getnodes_entry {
 
 void fromwire_gossip_getnodes_entry(const tal_t *ctx, const u8 **pptr, size_t *max, struct gossip_getnodes_entry *entry);
 void towire_gossip_getnodes_entry(u8 **pptr, const struct gossip_getnodes_entry *entry);
+
+void fromwire_route_hop(const u8 **pprt, size_t *max, struct route_hop *entry);
+void towire_route_hop(u8 **pprt, const struct route_hop *entry);
 
 #endif /* LIGHTNING_LIGHTGNINGD_GOSSIP_MSG_H */
