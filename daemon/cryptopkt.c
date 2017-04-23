b21aec350552cb6db8c328fbcdb8b491d451fe2c
Ben Gorlick <ben@bengorlick.com>
2017-04-22 20:52:33 -0700

    Update to check if tal_free already freed memory
    post valgrind, reproduced memory error and fix suggestions from @jgriffiths

--- daemon/cryptopkt.c
+++ daemon/cryptopkt.c
@@ -444,7 +444,8 @@ static struct io_plan *recv_body_negotiate(struct io_conn *conn,
 		return io_close(conn);
 
 	plan = neg->cb(conn, neg->dstate, neg->iod, neg->log, &id, neg->arg);
-	tal_free(neg);
+	if (plan)
+	tal_free(neg); /* If cb failed, neg is freed already */
 	return plan;
 }
 
