db17078085259b8bfa820029d8c7900a68d7843d
Rusty Russell <rusty@rustcorp.com.au>
2017-05-23 21:47:34 +0930

    dns: expose multiaddress connect code.
    Internally the dns code has the ability to try connecting to multiple
    addresses in a sequence.  Expose this, as we'll want it for reconnection.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- daemon/dns.h
+++ daemon/dns.h
@@ -29,4 +29,25 @@ struct dns_async *dns_resolve_and_connect_(struct lightningd_state *dstate,
 		  void (*fail)(struct lightningd_state *, void *arg),
 		  void *arg);
 
+/* Don't do lookup, just try to connect to these addresses. */
+#define multiaddress_connect(dstate, addresses, initfn, failfn, arg) \
+	multiaddress_connect_((dstate), (addresses),			\
+			typesafe_cb_preargs(struct io_plan *, void *, \
+					    (initfn), (arg),		\
+					    struct io_conn *,		\
+					    struct lightningd_state *,	\
+					    const struct netaddr *),	\
+			typesafe_cb_preargs(void, void *, (failfn), (arg), \
+					    struct lightningd_state *), \
+				 (arg))
+
+struct dns_async *multiaddress_connect_(struct lightningd_state *dstate,
+		  const struct netaddr *addresses,
+		  struct io_plan *(*init)(struct io_conn *,
+					  struct lightningd_state *,
+					  const struct netaddr *,
+					  void *arg),
+		  void (*fail)(struct lightningd_state *, void *arg),
+		  void *arg);
+
 #endif /* PETTYCOIN_DNS_H */
