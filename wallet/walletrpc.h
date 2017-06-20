c96b025e4c7342b392f5a1baf6491548f0e0a829
Christian Decker <decker.christian@gmail.com>
2017-06-20 17:15:25 +0200

    walletrpc: Implemented the withdrawal flow

---
+++ wallet/walletrpc.h
@@ -0,0 +1,11 @@
+#ifndef WALLET_WALLETRPC_H
+#define WALLET_WALLETRPC_H
+
+#include "config.h"
+
+/**
+ * Placeholder file, we aren't really exposing anything directly. RPC
+ * handlers are registered using AUTODATA.
+ */
+
+#endif /* WALLET_WALLETRPC_H */
