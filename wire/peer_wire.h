28cc92cd1554e88fdadef820124d2fd8baff8171
Christian Decker <decker.christian@gmail.com>
2017-08-08 14:18:30 +0200

    gossip: Use bit 3 (mask 0x08) to signal initial routing sync
    After quite some back and forth we seem to finally agree on the bit
    3 (mask 0x08) to signal optional initial_routing_sync.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- wire/peer_wire.h
+++ wire/peer_wire.h
@@ -26,7 +26,20 @@ bool is_gossip_msg(const u8 *cursor);
  * of the funding flow wishes to advertise this channel publicly to
  * the network as detailed within [BOLT #7]
  */
-#define CHANNEL_FLAGS_ANNOUNCE_CHANNEL	1
+#define CHANNEL_FLAGS_ANNOUNCE_CHANNEL 1
+
+/* BOLT #7:
+ *
+ * Upon establishing a connection, the two endpoints negotiate whether
+ * to perform an initial sync by setting the `initial_routing_sync`
+ * flags in the `init` message. The endpoint SHOULD set the
+ * `initial_routing_sync` flag if it requires a full copy of the other
+ * endpoint's routing state. Upon receiving an init message with the
+ * `initial_routing_sync` flag set the node sends `channel_announcement`s,
+ * `channel_update`s and `node_announcement`s for all known channels and
+ * nodes as if they were just received.
+ */
+#define LOCALFEATURES_INITIAL_ROUTING_SYNC 0x08
 
 /* Compare two short_channel_ids and return true if they are the equal */
 bool short_channel_id_eq(const struct short_channel_id *a,
