3e42485ddbd99fdd36a7c312dc8217de844dccd6
Christian Decker <decker.christian@gmail.com>
2017-08-18 14:38:50 +0200

    fix: Typo in the error message for adding an output to DB
    Fixes #216
    
    Reported-by: @jl777
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- wallet/walletrpc.c
+++ wallet/walletrpc.c
@@ -303,7 +303,7 @@ static void json_addfunds(struct command *cmd,
 	num_utxos =
 	    wallet_extract_owned_outputs(ld->wallet, tx, &total_satoshi);
 	if (num_utxos < 0) {
-		command_fail(cmd, "Could add outputs to wallet");
+		command_fail(cmd, "Could not add outputs to wallet");
 		return;
 	} else if (!num_utxos) {
 		command_fail(cmd, "No usable outputs");
