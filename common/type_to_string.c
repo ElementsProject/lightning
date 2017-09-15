563735d44a6081ebb443823b0791bae7d16ef7f0
Rusty Russell <rusty@rustcorp.com.au>
2017-09-16 09:04:06 +0930

    type_to_string: support preimage.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/type_to_string.c
+++ common/type_to_string.c
@@ -1,3 +1,4 @@
+#include <bitcoin/preimage.h>
 #include <ccan/crypto/ripemd160/ripemd160.h>
 #include <ccan/crypto/sha256/sha256.h>
 #include <ccan/tal/str/str.h>
@@ -8,6 +9,8 @@
 /* We need at least one, and these are in CCAN so register it here. */
 REGISTER_TYPE_TO_HEXSTR(sha256);
 REGISTER_TYPE_TO_HEXSTR(ripemd160);
+/* This one in bitcoin/ but doesn't have its own C file */
+REGISTER_TYPE_TO_HEXSTR(preimage);
 
 char *type_to_string_(const tal_t *ctx,  const char *typename,
 		      union printable_types u)
