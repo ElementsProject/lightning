7a98429738c92f3db0a117bd7be0102ec293f7de
Rusty Russell <rusty@rustcorp.com.au>
2017-10-11 20:37:50 +1030

    gossipd: include ccan/io version of handshake code, with tests.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ gossipd/handshake.h
@@ -0,0 +1,47 @@
+#ifndef LIGHTNING_LIGHTNINGD_GOSSIP_HANDSHAKE_H
+#define LIGHTNING_LIGHTNINGD_GOSSIP_HANDSHAKE_H
+#include "config.h"
+#include <ccan/typesafe_cb/typesafe_cb.h>
+
+struct crypto_state;
+struct io_conn;
+struct pubkey;
+
+#define initiator_handshake(conn, my_id, their_id, cb, cbarg)		\
+	initiator_handshake_((conn), (my_id), (their_id),		\
+			     typesafe_cb_preargs(struct io_plan *, void *, \
+						 (cb), (cbarg),		\
+						 struct io_conn *,	\
+						 const struct pubkey *,	\
+						 const struct crypto_state *), \
+			     (cbarg))
+
+
+struct io_plan *initiator_handshake_(struct io_conn *conn,
+				     const struct pubkey *my_id,
+				     const struct pubkey *their_id,
+				     struct io_plan *(*cb)(struct io_conn *,
+							   const struct pubkey *,
+							   const struct crypto_state *,
+							   void *cbarg),
+				     void *cbarg);
+
+
+#define responder_handshake(conn, my_id, cb, cbarg)			\
+	responder_handshake_((conn), (my_id),				\
+			     typesafe_cb_preargs(struct io_plan *, void *, \
+						 (cb), (cbarg),		\
+						 struct io_conn *,	\
+						 const struct pubkey *,	\
+						 const struct crypto_state *), \
+			     (cbarg))
+
+struct io_plan *responder_handshake_(struct io_conn *conn,
+				     const struct pubkey *my_id,
+				     struct io_plan *(*cb)(struct io_conn *,
+							   const struct pubkey *,
+							   const struct crypto_state *,
+							   void *cbarg),
+				     void *cbarg);
+
+#endif /* LIGHTNING_LIGHTNINGD_GOSSIP_HANDSHAKE_H */
