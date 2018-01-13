7e92c8ca88599848b0966b0764fa6a44faacd41e
Björge Dijkstra <bjd@jooz.net>
2018-01-13 20:11:37 +0100

    Fix unitialized fields in htlc_out constructed from wallet database entry

--- wallet/wallet.c
+++ wallet/wallet.c
@@ -1049,6 +1049,9 @@ static bool wallet_stmt2htlc_out(const struct wallet_channel *channel,
 	 * htlcs, will wire using origin_htlc_id */
 	out->in = NULL;
 
+	out->pay_command = NULL;
+	out->payment = NULL;
+
 	return ok;
 }
 
