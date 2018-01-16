865ec28c345c7749d5897576425fa95eb611ec35
Rusty Russell <rusty@rustcorp.com.au>
2018-01-17 06:13:14 +1030

    lightningd: --deprecated-api option to turn off deprecated APIs.
    This can be used for upgrades to make sure you're not using deprecated
    options, JSON commands, JSON fields, etc.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/options.h
+++ lightningd/options.h
@@ -15,4 +15,7 @@ bool handle_opts(struct lightningd *ld, int argc, char *argv[]);
 
 /* Derive default color and alias from the pubkey. */
 void setup_color_and_alias(struct lightningd *ld);
+
+/* Global to allow deprecated options. */
+extern bool deprecated_apis;
 #endif /* LIGHTNING_LIGHTNINGD_OPTIONS_H */
