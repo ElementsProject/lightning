89d4057f4a087edb113158330bec9c053da6231e
gdassori <guido.dassori@gmail.com>
2018-01-27 16:55:06 +0100

    version bump

--- contrib/pylightning/setup.py
+++ contrib/pylightning/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pylightning',
-      version='0.0.2',
+      version='0.0.3',
       description='Client library for lightningd',
       url='http://github.com/ElementsProject/lightning',
       author='Christian Decker',
