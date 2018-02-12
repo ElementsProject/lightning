fdfd7cbaca7e30eb87ca1305a32f01e831d26ecb
Christian Decker <decker.christian@gmail.com>
2018-02-12 14:50:02 +0100

    peer: Extract htlc_{in,out} detection into separate functions
    Will be used when forgetting peer as well.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- lightningd/peer_control.c
+++ lightningd/peer_control.c
@@ -91,29 +91,47 @@ static void peer_set_owner(struct peer *peer, struct subd *owner)
 		subd_release_peer(old_owner, peer);
 }
 
-static void destroy_peer(struct peer *peer)
+static struct htlc_out *peer_first_htlc_out(struct peer *peer)
 {
-	/* Must not have any HTLCs! */
 	struct htlc_out_map_iter outi;
 	struct htlc_out *hout;
-	struct htlc_in_map_iter ini;
-	struct htlc_in *hin;
-
 	for (hout = htlc_out_map_first(&peer->ld->htlcs_out, &outi);
 	     hout;
 	     hout = htlc_out_map_next(&peer->ld->htlcs_out, &outi)) {
 		if (hout->key.peer != peer)
 			continue;
-		fatal("Freeing peer %s has hout %s",
-		      peer_state_name(peer->state),
-		      htlc_state_name(hout->hstate));
+		return hout;
 	}
+	return NULL;
+}
 
+static struct htlc_in *peer_first_htlc_in(struct peer *peer)
+{
+	struct htlc_in_map_iter ini;
+	struct htlc_in *hin;
 	for (hin = htlc_in_map_first(&peer->ld->htlcs_in, &ini);
 	     hin;
 	     hin = htlc_in_map_next(&peer->ld->htlcs_in, &ini)) {
 		if (hin->key.peer != peer)
 			continue;
+		return hin;
+	}
+	return NULL;
+}
+
+static void destroy_peer(struct peer *peer)
+{
+	/* Must not have any HTLCs! */
+	struct htlc_out *hout = peer_first_htlc_out(peer);
+	struct htlc_in *hin = peer_first_htlc_in(peer);
+
+	if (hout) {
+		fatal("Freeing peer %s has hout %s",
+		      peer_state_name(peer->state),
+		      htlc_state_name(hout->hstate));
+	}
+
+	if (hin) {
 		fatal("Freeing peer %s has hin %s",
 		      peer_state_name(peer->state),
 		      htlc_state_name(hin->hstate));
@@ -2996,6 +3014,7 @@ static void json_dev_forget_channel(struct command *cmd, const char *buffer,
 	forget->peer = peer_from_json(cmd->ld, buffer, nodeidtok);
 	if (!forget->peer)
 		command_fail(cmd, "Could not find channel with that peer");
+
 	if (!forget->peer->funding_txid) {
 		process_dev_forget_channel(cmd->ld->topology->bitcoind, NULL, forget);
 	} else {
