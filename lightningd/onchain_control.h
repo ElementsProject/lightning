9ff19f634aae86604dc9962fdaf59e16d5a411a9
Rusty Russell <rusty@rustcorp.com.au>
2018-02-21 07:29:09 +1030

    lightningd/onchain_control: routines to control onchaind (move from peer_control.c)
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ lightningd/onchain_control.h
@@ -0,0 +1,16 @@
+#ifndef LIGHTNING_LIGHTNINGD_ONCHAIN_CONTROL_H
+#define LIGHTNING_LIGHTNINGD_ONCHAIN_CONTROL_H
+#include "config.h"
+#include <ccan/short_types/short_types.h>
+
+struct channel;
+struct bitcoin_tx;
+struct block;
+
+enum watch_result funding_spent(struct channel *channel,
+				const struct bitcoin_tx *tx,
+				size_t input_num,
+				const struct block *block,
+				void *unused);
+
+#endif /* LIGHTNING_LIGHTNINGD_ONCHAIN_CONTROL_H */
