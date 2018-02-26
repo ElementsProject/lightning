4188c05add9a593fdeca04f27ed3a1d706d4a49a
Christian Decker <decker.christian@gmail.com>
2018-02-26 12:57:30 +0100

    db: Add index covering the output heights
    Avoids performing a table scan, now deletes on blocks are a lot
    faster.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- wallet/db.c
+++ wallet/db.c
@@ -215,6 +215,8 @@ char *dbmigrations[] = {
      * die. */
     "ALTER TABLE outputs ADD COLUMN confirmation_height INTEGER REFERENCES blocks(height) ON DELETE SET NULL;",
     "ALTER TABLE outputs ADD COLUMN spend_height INTEGER REFERENCES blocks(height) ON DELETE SET NULL;",
+    /* Create a covering index that covers both fields */
+    "CREATE INDEX output_height_idx ON outputs (confirmation_height, spend_height);",
     NULL,
 };
 
