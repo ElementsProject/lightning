629c3db421c8dd55db45e2da91a789a4748c5cbb
Rusty Russell <rusty@rustcorp.com.au>
2018-03-02 12:56:30 +1030

    ccan: update to get tal_free fix.
    This allows us to make heirarchy loops, such as reference counting to
    free our own parent.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/ccan/intmap/test/run-signed-int.c
+++ ccan/ccan/intmap/test/run-signed-int.c
@@ -9,13 +9,14 @@ int main(void)
 	int64_t s;
 
 	/* This is how many tests you plan to run */
-	plan_tests(35);
+	plan_tests(38);
 
 	sintmap_init(&map);
 	/* Test boundaries. */
 	ok1(!sintmap_get(&map, 0x7FFFFFFFFFFFFFFFLL));
 	ok1(!sintmap_get(&map, -0x8000000000000000LL));
 	ok1(sintmap_first(&map, &s) == NULL);
+	ok1(sintmap_last(&map, &s) == NULL);
 	ok1(errno == ENOENT);
 	s = 0x7FFFFFFFFFFFFFFFLL;
 	ok1(sintmap_after(&map, &s) == NULL);
@@ -29,12 +30,14 @@ int main(void)
 	ok1(sintmap_add(&map, 0x7FFFFFFFFFFFFFFFLL, first));
 	ok1(sintmap_get(&map, 0x7FFFFFFFFFFFFFFFLL) == first);
 	ok1(sintmap_first(&map, &s) == first && s == 0x7FFFFFFFFFFFFFFFLL);
+	ok1(sintmap_last(&map, &s) == first && s == 0x7FFFFFFFFFFFFFFFLL);
 	ok1(errno == 0);
 	ok1(sintmap_add(&map, -0x8000000000000000LL, second));
 	ok1(sintmap_get(&map, 0x7FFFFFFFFFFFFFFFLL) == first);
 	ok1(sintmap_get(&map, -0x8000000000000000LL) == second);
 	ok1(sintmap_first(&map, &s) == second && s == -0x8000000000000000LL);
 	ok1(sintmap_after(&map, &s) == first && s == 0x7FFFFFFFFFFFFFFFLL);
+	ok1(sintmap_last(&map, &s) == first && s == 0x7FFFFFFFFFFFFFFFLL);
 	ok1(errno == 0);
 	s = 0x7FFFFFFFFFFFFFFELL;
 	ok1(sintmap_after(&map, &s) == first && s == 0x7FFFFFFFFFFFFFFFLL);
