5492f3cda8603929f9cf2fabbec731c089c706c8
Christian Decker <decker.christian@gmail.com>
2018-03-20 14:17:07 +0100

    gossip: Add desctructor to gossip_store
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- gossipd/gossip_store.c
+++ gossipd/gossip_store.c
@@ -16,12 +16,21 @@ struct gossip_store {
 	__off_t replaysize;
 };
 
+static void gossip_store_destroy(struct gossip_store *gs)
+{
+	if (gs->read_fd != -1)
+		close(gs->read_fd);
+	close(gs->write_fd);
+}
+
 struct gossip_store *gossip_store_new(const tal_t *ctx)
 {
 	struct gossip_store *gs = tal(ctx, struct gossip_store);
 	gs->write_fd = open(GOSSIP_STORE_FILENAME, O_RDWR|O_APPEND|O_CREAT, 0600);
 	gs->read_fd = open(GOSSIP_STORE_FILENAME, O_RDONLY);
 	gs->replaysize = lseek(gs->write_fd, 0, SEEK_END);
+
+	tal_add_destructor(gs, gossip_store_destroy);
 
 	return gs;
 }
