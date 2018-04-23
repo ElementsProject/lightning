ElementsProject/lightning

commit d822ba1eee3f03cb453ee78af0da5c8479b53204

lightningd: allow a new channel open from peer if no *active* channels.
And return the correct error message for the channel they give, if
they try to re-establish on an error channel.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

lightningd/peer_control.c
@@ -89,12 +89,6 @@ static void peer_accept_channel(struct lightningd *ld,
 				int peer_fd, int gossip_fd,
 				const u8 *open_msg);
 
-/* FIXME: This is here until we get multi-channel support */
-static struct channel *peer2channel(const struct peer *peer)
-{
-	return list_top(&peer->channels, struct channel, list);
-}
-
 static void destroy_peer(struct peer *peer)
 {
 	list_del_from(&peer->ld->peers, &peer->list);
@@ -287,7 +281,7 @@ void peer_connected(struct lightningd *ld, const u8 *msg,
 	u8 *error;
 	u8 *supported_global_features;
 	u8 *supported_local_features;
-	struct peer *peer;
+	struct channel *channel;
 	struct wireaddr addr;
 	u64 gossip_index;
 
@@ -316,16 +310,15 @@ void peer_connected(struct lightningd *ld, const u8 *msg,
 		goto send_error;
 	}
 
-	/* Now, do we already know this peer? */
-	peer = peer_by_id(ld, &id);
-	if (peer) {
-		/* FIXME: Only consider active channels! */
-		struct channel *channel = peer2channel(peer);
-
+	/* If we're already dealing with this peer, hand off to correct
+	 * subdaemon.  Otherwise, we'll respond iff they ask about an inactive
+	 * channel. */
+	channel = active_channel_by_id(ld, &id);
+	if (channel) {
 		log_debug(channel->log, "Peer has reconnected, state %s",
 			  channel_state_name(channel));
 
-		/* FIXME: We can have errors for multiple channels. */
+		/* If we have a canned error, deliver it now. */
 		if (channel->error) {
 			error = channel->error;
 			goto send_error;
@@ -355,9 +348,9 @@ void peer_connected(struct lightningd *ld, const u8 *msg,
 		case ONCHAIND_OUR_UNILATERAL:
 		case FUNDING_SPEND_SEEN:
 		case ONCHAIND_MUTUAL:
-			/* If they try to reestablish channel, we'll send
-			 * error then */
-			goto return_to_gossipd;
+		case CLOSINGD_COMPLETE:
+			/* Channel is active! */
+			abort();
 
 		case CHANNELD_AWAITING_LOCKIN:
 		case CHANNELD_NORMAL:
@@ -366,19 +359,18 @@ void peer_connected(struct lightningd *ld, const u8 *msg,
 			 * on this peer. */
 			channel_set_owner(channel, NULL);
 
-			peer->addr = addr;
+			channel->peer->addr = addr;
 			peer_start_channeld(channel, &cs, gossip_index,
 					    peer_fd, gossip_fd, NULL,
 					    true);
 			goto connected;
 
 		case CLOSINGD_SIGEXCHANGE:
-		case CLOSINGD_COMPLETE:
 			/* Stop any existing daemon, without triggering error
 			 * on this peer. */
 			channel_set_owner(channel, NULL);
 
-			peer->addr = addr;
+			channel->peer->addr = addr;
 			peer_start_closingd(channel, &cs, gossip_index,
 					    peer_fd, gossip_fd,
 					    true);
@@ -455,6 +447,27 @@ void peer_connection_failed(struct lightningd *ld, const u8 *msg)
 	}
 }
 
+static struct channel *channel_by_channel_id(struct peer *peer,
+					     const struct channel_id *channel_id)
+{
+	struct channel *channel;
+
+	list_for_each(&peer->channels, channel, list) {
+		struct channel_id cid;
+
+		if (!channel->funding_txid)
+			continue;
+
+		derive_channel_id(&cid,
+				  channel->funding_txid,
+				  channel->funding_outnum);
+		if (structeq(&cid, channel_id))
+			return channel;
+	}
+	return NULL;
+}
+
+/* We only get here IF we weren't trying to connect to it. */
 void peer_sent_nongossip(struct lightningd *ld,
 			 const struct pubkey *id,
 			 const struct wireaddr *addr,
@@ -474,28 +487,35 @@ void peer_sent_nongossip(struct lightningd *ld,
 	else
 		channel_id = &extracted_channel_id;
 
-	/* FIXME: match state too; we can have multiple onchain
-	 * (ie. dead) channels for the same peer. */
 	peer = peer_by_id(ld, id);
-	if (peer) {
-		error = towire_errorfmt(ld, channel_id,
-					"Unexpected message %i in state %s",
-					fromwire_peektype(in_msg),
-					channel_state_name(peer2channel(peer)));
-		goto send_error;
-	}
 
 	/* Open request? */
 	if (fromwire_peektype(in_msg) == WIRE_OPEN_CHANNEL) {
+		if (peer && peer_active_channel(peer)) {
+			error = towire_errorfmt(in_msg, channel_id,
+						"Multiple channels unsupported");
+			goto send_error;
+		}
 		peer_accept_channel(ld, id, addr, cs, gossip_index,
 				    gfeatures, lfeatures,
 				    peer_fd, gossip_fd, in_msg);
 		return;
 	}
 
+	/* If they are talking about a specific channel id, we may have an
+	 * error for them. */
+	if (peer && channel_id) {
+		struct channel *channel;
+		channel = channel_by_channel_id(peer, channel_id);
+		if (channel && channel->error) {
+			error = channel->error;
+			goto send_error;
+		}
+	}
+
 	/* Weird request. */
 	error = towire_errorfmt(ld, channel_id,
-				"Unexpected message %i for unknown peer",
+				"Unexpected message %i for peer",
 				fromwire_peektype(in_msg));
 
 send_error:

tests/test_lightningd.py
@@ -3382,6 +3382,30 @@ class LightningDTests(BaseLightningDTests):
             oneconfig = l1.rpc.listconfigs(c)
             assert(oneconfig[c] == configs[c])
 
+    def test_multiple_channels(self):
+        l1 = self.node_factory.get_node()
+        l2 = self.node_factory.get_node()
+
+        for i in range(3):
+            # FIXME: we shouldn't disconnect on close?
+            ret = l1.rpc.connect(l2.info['id'], 'localhost', l2.info['port'])
+            assert ret['id'] == l2.info['id']
+
+            l1.daemon.wait_for_log('WIRE_GOSSIPCTL_HAND_BACK_PEER')
+            l2.daemon.wait_for_log('WIRE_GOSSIPCTL_HAND_BACK_PEER')
+            chanid = self.fund_channel(l1, l2, 10**6)
+
+            l1.rpc.close(l2.info['id'])
+            l1.daemon.wait_for_log(' to CLOSINGD_COMPLETE')
+            l2.daemon.wait_for_log(' to CLOSINGD_COMPLETE')
+
+        channels = l1.rpc.listpeers()['peers'][0]['channels']
+        assert len(channels) == 3
+        # Most in state ONCHAIND_MUTUAL, last is CLOSINGD_COMPLETE
+        for i in range(len(channels)-1):
+            assert channels[i]['state'] == 'ONCHAIND_MUTUAL'
+        assert channels[-1]['state'] == 'CLOSINGD_COMPLETE'
+
     def test_cli(self):
         l1 = self.node_factory.get_node()
 

commit d50ca141eec3fadbfd4aa9da9e5e725f557ce6d1

lightningd: re-allow closing negotiation when CLOSINGD_COMPLETE
d822ba1ee accidentally removed this case, which is important: if the
other side didn't get our final matching closing_signed, it will
reconnect and try again.  We consider the channel no longer "active"
and thus ignore it, and get upset when it send the
`channel_reestablish` message.

We could just consider CLOSINGD_COMPLETE to be active, but then we'd
have to wait for the closing transaction to be mined before we'd allow
another connection.

We can't special case it when the peer reconnects, because there
could be (in theory) multiple channels for that peer in CLOSINGD_COMPLETE,
and we don't know which one to reestablish.

So, we need to catch this when they send the reestablish, and hand
that msg to closingd to do negotiation again.  We already have code
to note that we're in CLOSINGD_COMPLETE and thus ignore any result
it gives us.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

closingd/closing.c
@@ -81,7 +81,8 @@ static void do_reconnect(struct crypto_state *cs,
 			 u64 gossip_index,
 			 const struct channel_id *channel_id,
 			 const u64 next_index[NUM_SIDES],
-			 u64 revocations_received)
+			 u64 revocations_received,
+			 const u8 *channel_reestablish)
 {
 	u8 *msg;
 	struct channel_id their_channel_id;
@@ -104,20 +105,25 @@ static void do_reconnect(struct crypto_state *cs,
 	if (!sync_crypto_write(cs, PEER_FD, take(msg)))
 		peer_failed_connection_lost();
 
-	/* Wait for them to say something interesting */
-	while ((msg = read_peer_msg(tmpctx, cs, gossip_index, channel_id,
-				    sync_crypto_write_arg,
-				    status_fail_io,
-				    NULL)) == NULL)
+	/* They might have already send reestablish, which triggered us */
+	while (!channel_reestablish) {
 		clean_tmpctx();
 
-	if (!fromwire_channel_reestablish(msg, &their_channel_id,
+		/* Wait for them to say something interesting */
+		channel_reestablish
+			= read_peer_msg(tmpctx, cs, gossip_index, channel_id,
+					sync_crypto_write_arg,
+					status_fail_io,
+					NULL);
+	}
+
+	if (!fromwire_channel_reestablish(channel_reestablish, &their_channel_id,
 					  &next_local_commitment_number,
 					  &next_remote_revocation_number)) {
 		peer_failed(cs, gossip_index, channel_id,
 			    "bad reestablish msg: %s %s",
-			    wire_type_name(fromwire_peektype(msg)),
-			    tal_hex(tmpctx, msg));
+			    wire_type_name(fromwire_peektype(channel_reestablish)),
+			    tal_hex(tmpctx, channel_reestablish));
 	}
 	status_trace("Got reestablish commit=%"PRIu64" revoke=%"PRIu64,
 		     next_local_commitment_number,
@@ -443,6 +449,7 @@ int main(int argc, char *argv[])
 	u64 gossip_index;
 	enum side whose_turn;
 	bool deprecated_api;
+	u8 *channel_reestablish;
 
 	subdaemon_setup(argc, argv);
 
@@ -466,7 +473,8 @@ int main(int argc, char *argv[])
 				   &next_index[LOCAL],
 				   &next_index[REMOTE],
 				   &revocations_received,
-				   &deprecated_api))
+				   &deprecated_api,
+				   &channel_reestablish))
 		master_badmsg(WIRE_CLOSING_INIT, msg);
 
 	status_trace("satoshi_out = %"PRIu64"/%"PRIu64,
@@ -483,7 +491,8 @@ int main(int argc, char *argv[])
 
 	if (reconnected)
 		do_reconnect(&cs, gossip_index, &channel_id,
-			     next_index, revocations_received);
+			     next_index, revocations_received,
+			     channel_reestablish);
 
 	peer_billboard(true, "Negotiating closing fee between %"PRIu64
 		       " and %"PRIu64" satoshi (ideal %"PRIu64")",

lightningd/channel_control.c
@@ -114,7 +114,8 @@ static void peer_start_closingd_after_shutdown(struct channel *channel,
 	}
 
 	/* This sets channel->owner, closes down channeld. */
-	peer_start_closingd(channel, &cs, gossip_index, fds[0], fds[1], false);
+	peer_start_closingd(channel, &cs, gossip_index, fds[0], fds[1],
+			    false, NULL);
 	channel_set_state(channel, CHANNELD_SHUTTING_DOWN, CLOSINGD_SIGEXCHANGE);
 }
 

lightningd/closing_control.c
@@ -121,10 +121,11 @@ static unsigned closing_msg(struct subd *sd, const u8 *msg, const int *fds UNUSE
 }
 
 void peer_start_closingd(struct channel *channel,
-			 struct crypto_state *cs,
+			 const struct crypto_state *cs,
 			 u64 gossip_index,
 			 int peer_fd, int gossip_fd,
-			 bool reconnected)
+			 bool reconnected,
+			 const u8 *channel_reestablish)
 {
 	u8 *initmsg;
 	u64 minfee, startfee, feelimit;
@@ -204,7 +205,8 @@ void peer_start_closingd(struct channel *channel,
 				      channel->next_index[LOCAL],
 				      channel->next_index[REMOTE],
 				      num_revocations,
-				      deprecated_apis);
+				      deprecated_apis,
+				      channel_reestablish);
 
 	/* We don't expect a response: it will give us feedback on
 	 * signatures sent and received, then closing_complete. */

lightningd/closing_control.h
@@ -7,9 +7,10 @@ struct channel_id;
 struct crypto_state;
 
 void peer_start_closingd(struct channel *channel,
-			 struct crypto_state *cs,
+			 const struct crypto_state *cs,
 			 u64 gossip_index,
 			 int peer_fd, int gossip_fd,
-			 bool reconnected);
+			 bool reconnected,
+			 const u8 *channel_reestablish);
 
 #endif /* LIGHTNING_LIGHTNINGD_CLOSING_CONTROL_H */

lightningd/peer_control.c
@@ -502,7 +502,7 @@ void peer_connected(struct lightningd *ld, const u8 *msg,
 			channel->peer->addr = addr;
 			peer_start_closingd(channel, &cs, gossip_index,
 					    peer_fd, gossip_fd,
-					    true);
+					    true, NULL);
 			goto connected;
 		}
 		abort();
@@ -589,6 +589,17 @@ void peer_sent_nongossip(struct lightningd *ld,
 		if (channel && channel->error) {
 			error = channel->error;
 			goto send_error;
+		}
+
+		/* Reestablish for a now-closed channel?  They might have
+		 * missed final update, so do the closing negotiation dance
+		 * again. */
+		if (fromwire_peektype(in_msg) == WIRE_CHANNEL_REESTABLISH
+		    && channel
+		    && channel->state == CLOSINGD_COMPLETE) {
+			peer_start_closingd(channel, cs, gossip_index,
+					    peer_fd, gossip_fd, true, in_msg);
+			return;
 		}
 	}
 

wallet/test/run-wallet.c
@@ -333,10 +333,11 @@ bool peer_start_channeld(struct channel *channel UNNEEDED,
 { fprintf(stderr, "peer_start_channeld called!\n"); abort(); }
 /* Generated stub for peer_start_closingd */
 void peer_start_closingd(struct channel *channel UNNEEDED,
-			 struct crypto_state *cs UNNEEDED,
+			 const struct crypto_state *cs UNNEEDED,
 			 u64 gossip_index UNNEEDED,
 			 int peer_fd UNNEEDED, int gossip_fd UNNEEDED,
-			 bool reconnected UNNEEDED)
+			 bool reconnected UNNEEDED,
+			 const u8 *channel_reestablish UNNEEDED)
 { fprintf(stderr, "peer_start_closingd called!\n"); abort(); }
 /* Generated stub for sanitize_error */
 char *sanitize_error(const tal_t *ctx UNNEEDED, const u8 *errmsg UNNEEDED,