8e83ad3e6cad6a986b9d3b0dc660c6912f7daff0
Rusty Russell <rusty@rustcorp.com.au>
2018-05-10 23:30:05 +0930

    gossipd: use a real update in local_add_channel.
    We generate one now, so let's use it.  That lets us simplify the
    code, too.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- gossipd/gossip.c
+++ gossipd/gossip.c
@@ -656,7 +656,7 @@ static u8 *handle_gossip_msg(struct daemon *daemon, const u8 *msg)
 		break;
 
 	case WIRE_CHANNEL_UPDATE:
-		err = handle_channel_update(rstate, msg);
+		err = handle_channel_update(rstate, msg, true);
 		if (err)
 			return err;
 		break;
@@ -1488,7 +1488,7 @@ static void gossip_send_keepalive_update(struct routing_state *rstate,
 	status_trace("Sending keepalive channel_update for %s",
 		     type_to_string(tmpctx, struct short_channel_id, &scid));
 
-	err = handle_channel_update(rstate, update);
+	err = handle_channel_update(rstate, update, true);
 	if (err)
 		status_failed(STATUS_FAIL_INTERNAL_ERROR,
 			      "rejected keepalive channel_update: %s",
@@ -2384,7 +2384,7 @@ static struct io_plan *handle_disable_channel(struct io_conn *conn,
 			      strerror(errno));
 	}
 
-	err = handle_channel_update(daemon->rstate, msg);
+	err = handle_channel_update(daemon->rstate, msg, true);
 	if (err)
 		status_failed(STATUS_FAIL_INTERNAL_ERROR,
 			      "rejected disabling channel_update: %s",
