e81efd2e578a150e158b0744a6b3498da4fdc3c8
nicolas.dorier <nicolas.dorier@gmail.com>
2018-05-23 12:26:18 +0900

    Make sure lightningd is the foreground job is socat is enabled

--- tools/docker-entrypoint.sh
+++ tools/docker-entrypoint.sh
@@ -69,14 +69,16 @@ if [[ $REPLACEDNETWORK ]]; then
 fi
 
 if [ "$EXPOSE_TCP" == "true" ]; then
+    set -m
     lightningd &
-
     echo "C-Lightning starting"
     while read -r i; do if [ "$i" = "lightning-rpc" ]; then break; fi; done \
     < <(inotifywait  -e create,open --format '%f' --quiet "$LIGHTNINGD_DATA" --monitor)
     echo "C-Lightning started"
 
-    socat "TCP4-listen:$LIGHTNINGD_PORT,fork,reuseaddr" "UNIX-CONNECT:$LIGHTNINGD_DATA/lightning-rpc"
+    socat "TCP4-listen:$LIGHTNINGD_PORT,fork,reuseaddr" "UNIX-CONNECT:$LIGHTNINGD_DATA/lightning-rpc" &
+
+    fg %-
 else
     lightningd
 fi
