2008327af45ab8218d45fc22f5a27b255faf3b66
Christian Decker <decker.christian@gmail.com>
2018-06-11 19:50:37 +0200

    opts: Add the max_fee_multiplier to specify acceptable fee ranges
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- lightningd/options.c
+++ lightningd/options.c
@@ -462,6 +462,13 @@ static void dev_register_opts(struct lightningd *ld)
 	opt_register_arg("--dev-bitcoind-poll", opt_set_u32, opt_show_u32,
 			 &ld->topology->poll_seconds,
 			 "Time between polling for new transactions");
+	opt_register_arg("--dev-max-fee-multiplier", opt_set_u32, opt_show_u32,
+			 &ld->config.max_fee_multiplier,
+			 "Allow the fee proposed by the remote end to be up to "
+			 "multiplier times higher than our own. Small values "
+			 "will cause channels to be closed more often due to "
+			 "fee fluctuations, large values may result in large "
+			 "fees.");
 	opt_register_arg("--dev-override-fee-rates", opt_set_fee_rates, NULL,
 			 ld->topology,
 			 "Force a specific rates (immediate/normal/slow) in satoshis per kw regardless of estimated fees");
@@ -515,6 +522,9 @@ static const struct config testnet_config = {
 
 	/* Rescan 5 hours of blocks on testnet, it's reorg happy */
 	.rescan = 30,
+
+	/* Fees may be in the range our_fee - 10*our_fee */
+	.max_fee_multiplier = 5,
 };
 
 /* aka. "Dude, where's my coins?" */
@@ -568,6 +578,9 @@ static const struct config mainnet_config = {
 
 	/* Rescan 2.5 hours of blocks on startup, it's not so reorg happy */
 	.rescan = 15,
+
+	/* Fees may be in the range our_fee - 10*our_fee */
+	.max_fee_multiplier = 5,
 };
 
 static void check_config(struct lightningd *ld)
