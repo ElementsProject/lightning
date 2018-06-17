e7764531777acf4b0a302d60bb3e42fe4d45d64b
Rusty Russell <rusty@rustcorp.com.au>
2018-06-17 19:43:44 +0930

    lightningd: fix up BOLT references.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/test/run-commit_tx.c
+++ lightningd/test/run-commit_tx.c
@@ -45,9 +45,9 @@ static struct secret secret_from_hex(const char *hex)
 	len = strlen(hex);
 	/* BOLT #3:
 	 *
-	 * private keys are displayed as 32 bytes plus a trailing 1 (bitcoin's
-	 * convention for "compressed" private keys, i.e. keys for which the
-	 * public key is compressed)
+	 * - Private keys are displayed as 32 bytes plus a trailing 1
+	 *   (Bitcoin's convention for "compressed" private keys, i.e. keys
+	 *   for which the public key is compressed).
 	 */
 	if (len == 66 && strends(hex, "01"))
 		len -= 2;
@@ -469,17 +469,16 @@ int main(void)
 	 * # Appendix C: Commitment and HTLC Transaction Test Vectors
 	 *
 	 * In the following:
-	 * - we consider *local* transactions, which implies that all payments
-         *    to *local* are delayed
-	 * - we assume that *local* is the funder
-	 * - private keys are displayed as 32 bytes plus a trailing 1
-         *    (bitcoin's convention for "compressed" private keys, i.e. keys
-         *    for which the public key is compressed)
+	 *  - *local* transactions are considered, which implies that all
+	 *    payments to *local* are delayed.
+	 *  - It's assumed that *local* is the funder.
+	 *  - Private keys are displayed as 32 bytes plus a trailing 1
+	 *    (Bitcoin's convention for "compressed" private keys, i.e. keys for
+	 *    which the public key is compressed).
+	 *  - Transaction signatures are all deterministic, using RFC6979 (using
+	 *    HMAC-SHA256).
 	 *
-	 * - transaction signatures are all deterministic, using
-         *    RFC6979 (using HMAC-SHA256)
-	 *
-	 * We start by defining common basic parameters for each test vector:
+	 * To start, common basic parameters for each test vector are defined:
 	 * the HTLCs are not used for the first "simple commitment tx with no
 	 * HTLCs" test.
 	 *
@@ -503,10 +502,10 @@ int main(void)
 
 	/* BOLT #3:
 	 *
-	 * <!-- We derive the test vector values as per Key Derivation, though
-	 * it's not required for this test.  They're included here for
-	 * completeness and in case someone wants to reproduce the test
-	 * vectors themselves:
+	 * <!-- The test vector values are derived, as per Key Derivation,
+	 *      though it's not required for this test. They're included here
+	 *      for completeness and in case someone wants to reproduce the
+	 *      test vectors themselves:
          *
          * INTERNAL: remote_funding_privkey: 1552dfba4f6cf29a62a0af13c8d6981d36d0ef8d61ba10fb0fe90da7634d7e130101
          * INTERNAL: local_payment_basepoint_secret: 111111111111111111111111111111111111111111111111111111111111111101
@@ -519,9 +518,9 @@ int main(void)
          * # From local_delayed_payment_basepoint_secret
          * INTERNAL: local_delayed_payment_basepoint: 023c72addb4fdf09af94f0c94d7fe92a386a7e70cf8a1d85916386bb2535c7b1b1
          * INTERNAL: local_per_commitment_point: 025f7117a78150fe2ef97db7cfc83bd57b2e2c0d0dd25eaf467a4a1c2a45ce1486
-         * INTERNAL: remote_secretkey: 8deba327a7cc6d638ab0eb025770400a6184afcba6713c210d8d10e199ff2fda01
+         * INTERNAL: remote_privkey: 8deba327a7cc6d638ab0eb025770400a6184afcba6713c210d8d10e199ff2fda01
          * # From local_delayed_payment_basepoint_secret, local_per_commitment_point and local_delayed_payment_basepoint
-         * INTERNAL: local_delayed_secretkey: adf3464ce9c2f230fd2582fda4c6965e4993ca5524e8c9580e3df0cf226981ad01
+         * INTERNAL: local_delayed_privkey: adf3464ce9c2f230fd2582fda4c6965e4993ca5524e8c9580e3df0cf226981ad01
 	 */
 	local_funding_privkey.secret = secret_from_hex("30ff4956bbdd3222d44cc5e8a1261dab1e07957bdac5ae88fe3261ef321f374901");
 	x_remote_funding_privkey.secret = secret_from_hex("1552dfba4f6cf29a62a0af13c8d6981d36d0ef8d61ba10fb0fe90da7634d7e1301");
@@ -750,7 +749,7 @@ int main(void)
 
 	/* BOLT #3:
 	 *
-	 *    name: commitment tx with all 5 HTLCs untrimmed (minimum feerate)
+	 *    name: commitment tx with all five HTLCs untrimmed (minimum feerate)
 	 *    to_local_msat: 6988000000
 	 *    to_remote_msat: 3000000000
 	 *    local_feerate_per_kw: 0
