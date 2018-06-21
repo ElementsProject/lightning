2ab3fd79725df35be86f69587818b923143eefa1
Corné Plooy <corne@bitonic.nl>
2018-06-21 16:37:26 +0200

    tests: in the test app scripts, copy stdin to a file, so its contents can be investigated.

--- tests/app_scripts/fail.py
+++ tests/app_scripts/fail.py
@@ -7,5 +7,13 @@ APP_FORWARDED = 0
 APP_NOT_FORWARDED = 1
 APP_UNKNOWN = 2
 
+#copy stdin to a file
+with open('app-script-input', 'wb') as f:
+    while True:
+        data = sys.stdin.buffer.read()
+        if not data:
+            break
+        f.write(data)
+
 sys.stdout.buffer.write(struct.pack('B', APP_NOT_FORWARDED))
 
