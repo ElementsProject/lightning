bf4dc0991065b82f85b6c6b3bb1acc2055fc31f1
Rusty Russell <rusty@rustcorp.com.au>
2018-06-28 11:04:45 +0930

    devtools/decodemsg: decode node_announcement.alias
    $ ./devtools/decodemsg 01014bfa4585cb36194ce7c308e8d3d9032ff4e42ed4764a4c6d0a9a58da0a4e57e97fbd463577a5fd14347c60cc7bef2b40bd644337153564320b310b4d155cab1300005b3315de0266e4598d1d3c415f572a8488830b60f7e744ed9235eb0b1ba93283b315c035180266e4e3838ae383b3e382bbe383b3e382b92031e69daf000000000000000000000000004d010102030404d202000000000000000000000000000000002607039216a8b803f3acd758aa260704e00533f3e8f2aedaa8969b3d0fa03a96e857bbb28064dca5e147e934244b9ba50230032607
    
    Before:
        WIRE_NODE_ANNOUNCEMENT:
        signature=304402204bfa4585cb36194ce7c308e8d3d9032ff4e42ed4764a4c6d0a9a58da0a4e57e902207fbd463577a5fd14347c60cc7bef2b40bd644337153564320b310b4d155cab13
        features=[]
        timestamp=1530074590
        node_id=0266e4598d1d3c415f572a8488830b60f7e744ed9235eb0b1ba93283b315c03518
        rgb_color=[\x02f\xe4]
        alias=[\xe3\x83\x8a\xe3\x83\xb3\xe3\x82\xbb\xe3\x83\xb3\xe3\x82\xb9 1\xe6\x9d\xaf\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00]
        addresses=[\x01\x01\x02\x03\x04\x04\xd2\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00&\x07\x03\x92\x16\xa8\xb8\x03\xf3\xac\xd7X\xaa&\x07\x04\xe0\x053\xf3\xe8\xf2\xae\xda\xa8\x96\x9b=\x0f\xa0:\x96\xe8W\xbb\xb2\x80d\xdc\xa5\xe1G\xe94$K\x9b\xa5\x020\x03&\x07]
    
    After:
        WIRE_NODE_ANNOUNCEMENT:
        signature=304402204bfa4585cb36194ce7c308e8d3d9032ff4e42ed4764a4c6d0a9a58da0a4e57e902207fbd463577a5fd14347c60cc7bef2b40bd644337153564320b310b4d155cab13
        features=[]
        timestamp=1530074590
        node_id=0266e4598d1d3c415f572a8488830b60f7e744ed9235eb0b1ba93283b315c03518
        rgb_color=[0266e4]
        alias=[ナンセンス 1杯 e3838ae383b3e382bbe383b3e382b92031e69daf000000000000000000000000 ]
        addresses=[010102030404d202000000000000000000000000000000002607039216a8b803f3acd758aa260704e00533f3e8f2aedaa8969b3d0fa03a96e857bbb28064dca5e147e934244b9ba50230032607]
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- devtools/print_wire.c
+++ devtools/print_wire.c
@@ -1,5 +1,8 @@
+#include <ccan/mem/mem.h>
+#include <ccan/utf8/utf8.h>
 #include <common/type_to_string.h>
 #include <devtools/print_wire.h>
+#include <errno.h>
 #include <inttypes.h>
 #include <stdio.h>
 
@@ -23,19 +26,72 @@ void printwire_u64(const char *fieldname, const u64 *v)
 	printf("%"PRIu64"\n", *v);
 }
 
-void printwire_u8_array(const char *fieldname, const u8 **cursor, size_t *plen, size_t len)
+/* Returns false if we ran out of data. */
+static bool print_hexstring(const u8 **cursor, size_t *plen, size_t len)
 {
-	printf("[");
 	while (len) {
 		u8 v = fromwire_u8(cursor, plen);
 		if (!*cursor)
-			return;
-		if (isprint(v))
-			printf("%c", v);
-		else
-			printf("\\x%02x", v);
+			return false;
+		printf("%02x", v);
 		len--;
 	}
+	return true;
+}
+
+static void printwire_alias(const u8 **cursor, size_t *plen, size_t len)
+{
+	struct utf8_state utf8 = UTF8_STATE_INIT;
+	const char *p = (const char *)*cursor;
+	bool char_done = true;
+
+	printf("[");
+	for (size_t i = 0; i < len; i++) {
+		if (!p[i]) {
+			if (!memeqzero(p+i, len-i)) {
+				printf(" **INVALID PADDING** ");
+				goto hexdump;
+			}
+			break;
+		}
+		if (utf8_decode(&utf8, p[i])) {
+			if (errno != 0) {
+				printf(" **INVALID UTF-8** ");
+				goto hexdump;
+			}
+			char_done = true;
+		} else {
+			/* Don't allow unprintable characters */
+			if (utf8.total_len == 1 && !cisprint(utf8.c)) {
+				printf(" **UNPRINTABLE CHARACTER** ");
+				goto hexdump;
+			}
+			char_done = false;
+		}
+	}
+	if (!char_done) {
+		printf(" **INCOMPLETE UTF-8** ");
+		goto hexdump;
+	}
+	printf("%.*s ", (int)len, p);
+
+hexdump:
+	if (!print_hexstring(cursor, plen, len))
+		return;
+	printf(" ]\n");
+	return;
+}
+
+void printwire_u8_array(const char *fieldname, const u8 **cursor, size_t *plen, size_t len)
+{
+	if (streq(fieldname, "node_announcement.alias")) {
+		printwire_alias(cursor, plen, len);
+		return;
+	}
+
+	printf("[");
+	if (!print_hexstring(cursor, plen, len))
+		return;
 	printf("]\n");
 }
 
