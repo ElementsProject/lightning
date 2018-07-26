a806a5218c4ae1e87b9f4a0d408ab5a8419daf89
Rusty Russell <rusty@rustcorp.com.au>
2018-07-27 06:50:37 +0930

    devtools/decodemsg: add --onion option for decoding onion errors.
    This requires a tweak to generate-wire.py too, since it always called the
    top-level routine 'print_message'.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- devtools/decodemsg.c
+++ devtools/decodemsg.c
@@ -1,28 +1,36 @@
 #include <ccan/err/err.h>
+#include <ccan/opt/opt.h>
 #include <common/decode_short_channel_ids.h>
 #include <common/utils.h>
+#include <devtools/gen_print_onion_wire.h>
 #include <devtools/gen_print_wire.h>
 #include <stdio.h>
-
-static void usage(void)
-{
-	fprintf(stderr, "Usage: decodemsg <msg-in-hex>\n");
-	exit(1);
-}
 
 int main(int argc, char *argv[])
 {
 	const u8 *m;
+	bool onion = false;
 	setup_locale();
 
-	if (argc != 2)
-		usage();
+	opt_register_noarg("--onion", opt_set_bool, &onion,
+			   "Decode an error message instead of a peer message");
+	opt_register_noarg("--help|-h", opt_usage_and_exit,
+			   "<hexmsg>"
+			   "Decode a lightning spec wire message from hex.",
+			   "Print this message.");
 
-	/* Last arg is hex string */
+	opt_parse(&argc, argv, opt_log_stderr_exit);
+	if (argc != 2)
+		errx(1, "Need a hex message");
+
+	/* Arg is hex string */
 	m = tal_hexdata(NULL, argv[1], strlen(argv[1]));
 	if (!m)
 		errx(1, "'%s' is not valid hex", argv[1]);
 
-	print_message(m);
+	if (onion)
+		printonion_type_message(m);
+	else
+		printwire_type_message(m);
 	return 0;
 }
