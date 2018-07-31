fb0b5cc9763217515c790b0a3dd2e77ac0078dd4
William Casarin <jb55@jb55.com>
2018-07-31 08:03:01 -0700

    lightningd: update lightningd status in chaintopology
    Set syncing status when adding new blocks. Right now this happens at the same
    time the "Adding block ..." log happens.
    
    Set status back to ready when syncing is finished in updates_complete.
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- lightningd/chaintopology.c
+++ lightningd/chaintopology.c
@@ -353,6 +353,9 @@ static void updates_complete(struct chain_topology *topo)
 		topo->prev_tip = topo->tip;
 	}
 
+	/* Let lightningd know that we're done syncing */
+	*topo->lightningd_status = LIGHTNINGD_STATUS_READY;
+
 	/* Try again soon. */
 	next_topology_timer(topo);
 }
@@ -422,6 +425,10 @@ static struct block *new_block(struct chain_topology *topo,
 	log_debug(topo->log, "Adding block %u: %s",
 		  height,
 		  type_to_string(tmpctx, struct bitcoin_blkid, &b->blkid));
+
+	/* Update lightningd status to let them know we're still adding blocks */
+	*topo->lightningd_status = LIGHTNINGD_STATUS_SYNCING;
+
 	assert(!block_map_get(&topo->block_map, &b->blkid));
 	b->next = NULL;
 	b->prev = NULL;
