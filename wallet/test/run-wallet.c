fa55e2cab0afb0d7ae5768b74a5b6bc62d161240
Mark Beckwith <wythe@intrig.com>
2018-08-15 09:40:37 -0500

    param: upgraded json_tok_loglevel
    Signed-off-by: Mark Beckwith <wythe@intrig.com>

--- wallet/test/run-wallet.c
+++ wallet/test/run-wallet.c
@@ -227,8 +227,9 @@ bool json_tok_channel_id(const char *buffer UNNEEDED, const jsmntok_t *tok UNNEE
 			 struct channel_id *cid UNNEEDED)
 { fprintf(stderr, "json_tok_channel_id called!\n"); abort(); }
 /* Generated stub for json_tok_loglevel */
-bool json_tok_loglevel(const char *buffer UNNEEDED, const jsmntok_t *tok UNNEEDED,
-		       enum log_level *level UNNEEDED)
+bool json_tok_loglevel(struct command *cmd UNNEEDED, const char *name UNNEEDED,
+		       const char *buffer UNNEEDED, const jsmntok_t *tok UNNEEDED,
+		       enum log_level **level UNNEEDED)
 { fprintf(stderr, "json_tok_loglevel called!\n"); abort(); }
 /* Generated stub for json_tok_number */
 bool json_tok_number(struct command *cmd UNNEEDED, const char *name UNNEEDED,
