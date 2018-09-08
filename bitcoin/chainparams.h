37630dc35bbefdd2f22c05b7341f929e4ab994df
Christian Decker <decker.christian@gmail.com>
2018-09-08 13:37:29 +0200

    chainparams: Always retrieve chainparams by the chain_hash

--- bitcoin/chainparams.h
+++ bitcoin/chainparams.h
@@ -29,14 +29,6 @@ struct chainparams {
 const struct chainparams *chainparams_for_network(const char *network_name);
 
 /**
- * chainparams_by_index - Helper to get a network by its numeric index
- *
- * We may not want to pass the network name through to subdaemons, so
- * we allows lookup by index.
- */
-const struct chainparams *chainparams_by_index(const int index);
-
-/**
  * chainparams_by_bip173 - Helper to get a network by its bip173 name
  *
  * This lets us decode BOLT11 addresses.
