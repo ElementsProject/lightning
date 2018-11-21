3e1138951b083b3a8eac7d8a03ba5db40ef2cc6e
Christian Decker <decker.christian@gmail.com>
2018-11-21 21:54:01 +0100

    plugin: Add pointer to jsonrpc so we can add new methods
    Signed-off-by: Christian Decker <@cdecker>

--- lightningd/test/run-find_my_abspath.c
+++ lightningd/test/run-find_my_abspath.c
@@ -135,7 +135,8 @@ void plugins_config(struct plugins *plugins UNNEEDED)
 void plugins_init(struct plugins *plugins UNNEEDED)
 { fprintf(stderr, "plugins_init called!\n"); abort(); }
 /* Generated stub for plugins_new */
-struct plugins *plugins_new(const tal_t *ctx UNNEEDED, struct log_book *log_book UNNEEDED)
+struct plugins *plugins_new(const tal_t *ctx UNNEEDED, struct log_book *log_book UNNEEDED,
+			    struct jsonrpc *rpc UNNEEDED)
 { fprintf(stderr, "plugins_new called!\n"); abort(); }
 /* Generated stub for register_opts */
 void register_opts(struct lightningd *ld UNNEEDED)
