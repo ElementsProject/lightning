a0fd119a5648d9f6d0544fb7924a3e6a506853da
Rusty Russell <rusty@rustcorp.com.au>
2018-11-22 12:47:29 +1030

    openingd: wire up dev_memleak.
    This is a bit different from the other cases: we need to iterate through
    the peers and ask all the ones in openingd.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ lightningd/memdump.h
@@ -0,0 +1,11 @@
+#ifndef LIGHTNING_LIGHTNINGD_MEMDUMP_H
+#define LIGHTNING_LIGHTNINGD_MEMDUMP_H
+#include "config.h"
+#include <ccan/short_types/short_types.h>
+#include <stdbool.h>
+
+struct command;
+struct subd;
+
+void opening_memleak_done(struct command *cmd, struct subd *leaker);
+#endif /* LIGHTNING_LIGHTNINGD_MEMDUMP_H */
