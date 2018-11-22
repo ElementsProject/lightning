6c0a4db8e905caf1c74e35b8100e5a5ef285fff0
Christian Decker <decker.christian@gmail.com>
2018-11-22 11:37:08 +0100

    jsonrpc: Split the jsonrpc object creation from starting to listen
    This is needed in order to be able to add methods while initializing
    the plugins, but before actually moving to the config dir and starting
    to listen.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- lightningd/test/run-find_my_abspath.c
+++ lightningd/test/run-find_my_abspath.c
@@ -90,6 +90,12 @@ void htlcs_notify_new_block(struct lightningd *ld UNNEEDED, u32 height UNNEEDED)
 /* Generated stub for json_escape */
 struct json_escaped *json_escape(const tal_t *ctx UNNEEDED, const char *str TAKES UNNEEDED)
 { fprintf(stderr, "json_escape called!\n"); abort(); }
+/* Generated stub for jsonrpc_listen */
+void jsonrpc_listen(struct jsonrpc *rpc UNNEEDED, struct lightningd *ld UNNEEDED)
+{ fprintf(stderr, "jsonrpc_listen called!\n"); abort(); }
+/* Generated stub for jsonrpc_new */
+struct jsonrpc *jsonrpc_new(const tal_t *ctx UNNEEDED, struct lightningd *ld UNNEEDED)
+{ fprintf(stderr, "jsonrpc_new called!\n"); abort(); }
 /* Generated stub for load_channels_from_wallet */
 void load_channels_from_wallet(struct lightningd *ld UNNEEDED)
 { fprintf(stderr, "load_channels_from_wallet called!\n"); abort(); }
@@ -137,9 +143,6 @@ void register_opts(struct lightningd *ld UNNEEDED)
 /* Generated stub for setup_color_and_alias */
 void setup_color_and_alias(struct lightningd *ld UNNEEDED)
 { fprintf(stderr, "setup_color_and_alias called!\n"); abort(); }
-/* Generated stub for setup_jsonrpc */
-struct jsonrpc *setup_jsonrpc(struct lightningd *ld UNNEEDED)
-{ fprintf(stderr, "setup_jsonrpc called!\n"); abort(); }
 /* Generated stub for setup_topology */
 void setup_topology(struct chain_topology *topology UNNEEDED, struct timers *timers UNNEEDED,
 		    u32 min_blockheight UNNEEDED, u32 max_blockheight UNNEEDED)
