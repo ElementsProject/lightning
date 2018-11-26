b08cab461a59b604d1912dfd25917ba32c8d3cfe
Christian Decker <decker.christian@gmail.com>
2018-11-26 19:54:06 +0100

    plugin: Map results back to the incoming JSON-RPC request
    The final step in the JSON-RPC passthrough: map the result we got from
    the plugin back to the original request we got from the client.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- lightningd/plugin.c
+++ lightningd/plugin.c
@@ -8,6 +8,7 @@
 #include <ccan/tal/str/str.h>
 #include <errno.h>
 #include <lightningd/json.h>
+#include <lightningd/jsonrpc_errors.h>
 #include <lightningd/lightningd.h>
 #include <unistd.h>
 
@@ -407,10 +408,31 @@ static void plugin_rpcmethod_destroy(struct json_command *cmd,
 static void plugin_rpcmethod_cb(const struct plugin_request *req,
 				struct plugin_rpc_request *rpc_req)
 {
-	// Parse
-	// Extract results or error
-	// Construct reply
-	// Return result with appropriate return code.
+	struct json_stream *response;
+	const jsmntok_t *res;
+	assert(req->resulttok || req->errortok);
+
+	if (req->errortok) {
+		res = req->errortok;
+		command_fail(rpc_req->cmd, PLUGIN_ERROR, "%.*s",
+			     res->end - res->start, req->response + res->start);
+		return;
+	}
+
+	res = req->resulttok;
+	response = json_stream_success(rpc_req->cmd);
+
+	/* If this is a raw string result we'd be cropping the quotes from the
+	 * result, so add them back */
+	if (req->resulttok->type == JSMN_STRING)
+		json_add_member(response, NULL, "\"%.*s\"",
+				res->end - res->start,
+				req->response + res->start);
+	else
+		json_add_member(response, NULL, "%.*s", res->end - res->start,
+				req->response + res->start);
+
+	command_success(rpc_req->cmd, response);
 }
 
 static void plugin_rpcmethod_dispatch(struct command *cmd, const char *buffer,
