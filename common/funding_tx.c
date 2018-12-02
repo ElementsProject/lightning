23540fe956a8eae9a5138d8286f7f9b91af3bb7c
Rusty Russell <rusty@rustcorp.com.au>
2018-12-03 09:32:11 +1030

    common: make funding_tx and withdraw_tx share UTXO code.
    They both do the same thing: convert utxos into tx inputs.  Share code.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/funding_tx.c
+++ common/funding_tx.c
@@ -3,7 +3,6 @@
 #include <bitcoin/script.h>
 #include <bitcoin/tx.h>
 #include <ccan/ptrint/ptrint.h>
-#include <common/key_derive.h>
 #include <common/permute_tx.h>
 #include <common/utxo.h>
 
@@ -21,23 +20,10 @@ struct bitcoin_tx *funding_tx(const tal_t *ctx,
 			      const struct pubkey *changekey,
 			      const struct ext_key *bip32_base)
 {
-	struct bitcoin_tx *tx = bitcoin_tx(ctx, tal_count(utxomap),
-					   change_satoshis ? 2 : 1);
 	u8 *wscript;
-	size_t i;
+	struct bitcoin_tx *tx;
 
-	for (i = 0; i < tal_count(utxomap); i++) {
-		tx->input[i].txid = utxomap[i]->txid;
-		tx->input[i].index = utxomap[i]->outnum;
-		tx->input[i].amount = tal_dup(tx, u64, &utxomap[i]->amount);
-		if (utxomap[i]->is_p2sh && bip32_base) {
-			struct pubkey key;
-
-			bip32_pubkey(bip32_base, &key, utxomap[i]->keyindex);
-			tx->input[i].script
-				= bitcoin_scriptsig_p2sh_p2wpkh(tx, &key);
-		}
-	}
+	tx = tx_spending_utxos(ctx, utxomap, bip32_base, change_satoshis != 0);
 
 	tx->output[0].amount = funding_satoshis;
 	wscript = bitcoin_redeem_2of2(tx, local_fundingkey, remote_fundingkey);
--- common/utxo.h
+++ common/utxo.h
@@ -8,6 +8,8 @@
 #include <ccan/tal/tal.h>
 #include <stdbool.h>
 
+struct ext_key;
+
 /* Information needed for their_unilateral/to-us outputs */
 struct unilateral_close_info {
 	u64 channel_id;
@@ -36,4 +38,11 @@ struct utxo {
 
 void towire_utxo(u8 **pptr, const struct utxo *utxo);
 struct utxo *fromwire_utxo(const tal_t *ctx, const u8 **ptr, size_t *max);
+
+/* Create a tx, and populate inputs from utxos */
+struct bitcoin_tx *tx_spending_utxos(const tal_t *ctx,
+				     const struct utxo **utxos,
+				     const struct ext_key *bip32_base,
+				     bool add_change_output);
+
 #endif /* LIGHTNING_COMMON_UTXO_H */
