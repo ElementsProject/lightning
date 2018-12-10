6aa511fa7ad9f5b75357a17a2b7c6f846260e4b7
Rusty Russell <rusty@rustcorp.com.au>
2018-12-10 13:40:16 +1030

    channeld: only enable option_data_loss_protect if EXPERIMENTAL_FEATURES.
    We have an incompatibility with lnd it seems: I've lost channels on
    reconnect with 'sync error'.  Since I never got this code to be reliable,
    disable it for next release since I suspect it's our fault :(
    
    And reenable the check which didn't work, for others to untangle.
    
    I couldn't get option_data_loss_protect to be reliable, and I disabled
    the check.  This was a mistake, I should have either spent even more
    time trying to get to the bottom of this (especially, writing test
    vectors for the spec and testing against other implementations).
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- channeld/channeld.c
+++ channeld/channeld.c
@@ -1986,7 +1986,6 @@ static void check_current_dataloss_fields(struct peer *peer,
 			    type_to_string(tmpctx, struct secret,
 					   &old_commit_secret));
 
-#if 0 /* FIXME: This isn't reliable! */
 	/* FIXME: We don't keep really old per_commit numbers, so we can't
 	 * check this 'needs retransmit' case! */
 	if (next_remote_revocation_number == peer->next_index[REMOTE]) {
@@ -2010,7 +2009,6 @@ static void check_current_dataloss_fields(struct peer *peer,
 					    remote_current_per_commitment_point),
 			     next_remote_revocation_number,
 			     peer->next_index[REMOTE]);
-#endif
 
 	status_trace("option_data_loss_protect: fields are correct");
 }
