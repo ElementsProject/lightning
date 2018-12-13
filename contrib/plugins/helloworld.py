f4194dfc5bd433a2701c388641f55f1869f3770c
Christian Decker <decker.christian@gmail.com>
2018-12-13 17:35:01 +0100

    pytest: Add a test for the event subscription and notification
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- contrib/plugins/helloworld.py
+++ contrib/plugins/helloworld.py
@@ -24,5 +24,10 @@ def init(options, configuration, plugin):
     plugin.log("Plugin helloworld.py initialized")
 
 
+@plugin.subscribe("connect")
+def on_connect(id, address, plugin):
+    plugin.log("Received connect event for peer {}".format(id))
+
+
 plugin.add_option('greeting', 'Hello', 'The greeting I should use.')
 plugin.run()
