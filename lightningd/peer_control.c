5d6f7f14ba8c72753c3e64086fa752504f1693e4
Rusty Russell <rusty@rustcorp.com.au>
2019-01-02 11:19:49 +1030

    lightningd: add comment on why we have "useless" initialization.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/peer_control.c
+++ lightningd/peer_control.c
@@ -901,6 +901,7 @@ static struct command_result *json_close(struct command *cmd,
 {
 	const jsmntok_t *idtok;
 	struct peer *peer;
+	/* FIXME: gcc 7.3.0 thinks this might not be initialized. */
 	struct channel *channel = NULL;
 	unsigned int *timeout;
 	bool *force;
