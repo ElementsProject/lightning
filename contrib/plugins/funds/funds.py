43cb5b02f2590352ccbeebb25c3e44562bb7df50
Rene Pickhardt <rpickhardt@gmail.com>
2019-01-03 14:46:19 +0100

    included feedback from @cdecker and included a way to change the standard unit via an argument of lightningd

--- contrib/plugins/funds/funds.py
+++ contrib/plugins/funds/funds.py
@@ -10,6 +10,12 @@ Activate the plugin with:
 Call the plugin with: 
 `lightning-cli funds`
 
+The standard unit to depict the funds is set to satoshis. 
+The unit can be changed by and arguments after `lightning-cli funds` 
+for each call. It is also possible to change the standard unit when 
+starting lightningd just pass `--funds_display_unit={unit}` where
+unit can be s for satoshi, b for bits, m for milliBitcoin and B for BTC.
+
 
 Author: Rene Pickhardt (https://ln.rene-pickhardt.de)
 Development of the plugin was sponsored by fulmo.org
@@ -24,48 +30,52 @@ from os.path import join
 rpc_interface = None
 plugin = Plugin(autopatch=True)
 
+unit_aliases = {
+    "bitcoin": "BTC",
+    "btc": "BTC",
+    "satoshi": "sat",
+    "satoshis": "sat",
+    "bit": "bit",
+    "bits": "bit",
+    "milli": "mBTC",
+    "mbtc": "mBTC",
+    "millibtc": "mBTC",
+    "B": "BTC",
+    "s": "sat",
+    "m": "mBTC",
+    "b": "bit",
+}
+
+unit_divisor = {
+    "sat": 1,
+    "bit": 100,
+    "mBTC": 100*1000,
+    "BTC": 100*1000*1000,
+}
+
 
 @plugin.method("funds")
-def funds(plugin, unit="s"):
+def funds(unit=None, plugin=None):
     """Lists the total funds the lightning node owns off- and onchain in {unit}.
 
-{unit} can take the following values:
-s, satoshi, satoshis to depict satoshis
-b, bit, bits to depict bits
-m, milli, btc to depict milliBitcoin
-B, bitcoin, btc to depict Bitcoins
+    {unit} can take the following values:
+    s, satoshi, satoshis to depict satoshis
+    b, bit, bits to depict bits
+    m, milli, btc to depict milliBitcoin
+    B, bitcoin, btc to depict Bitcoins
 
-When not using Satoshis (default) the comma values are rounded off."""
+    When not using Satoshis (default) the comma values are rounded off."""
 
-    switcher = {
-        "bitcoin": "BTC",
-        "btc": "BTC",
-        "satoshi": "sat",
-        "satoshis": "sat",
-        "bit": "bit",
-        "bits": "bit",
-        "milli": "mBTC",
-        "mbtc": "mBTC",
-        "millibtc": "mBTC",
-        "B": "BTC",
-        "s": "sat",
-        "m": "mBTC",
-        "b": "bit",
-    }
+    plugin.log("call with unit: {}".format(unit), level="debug")
+    if unit is None:
+        unit = plugin.get_option("funds_display_unit")
 
     if unit != "B":
-        unit = switcher.get(unit.lower(), "sat")
+        unit = unit_aliases.get(unit.lower(), "sat")
     else:
         unit = "BTC"
 
-    switcher = {
-        "sat": 1,
-        "bit": 100,
-        "mBTC": 100*1000,
-        "BTC": 100*1000*1000,
-    }
-
-    div = switcher.get(unit, 1)
+    div = unit_divisor.get(unit, 1)
 
     funds = rpc_interface.listfunds()
 
@@ -84,17 +94,18 @@ When not using Satoshis (default) the comma values are rounded off."""
 @plugin.method("init")
 def init(options, configuration, plugin):
     global rpc_interface
-    plugin.log("start initialization of the funds plugin")
-    print(configuration)
+    plugin.log("start initialization of the funds plugin", level="debug")
     basedir = configuration['lightning-dir']
     rpc_filename = configuration['rpc-file']
     path = join(basedir, rpc_filename)
     plugin.log("rpc interface located at {}".format(path))
     rpc_interface = LightningRpc(path)
-
     plugin.log("Funds Plugin successfully initialezed")
+    plugin.log("standard unit is set to {}".format(
+        plugin.get_option("funds_display_unit")), level="debug")
 
 
-plugin.add_option('funds', 'funds',
-                  'Lists the total funds the lightning node owns off- and onchina in {units}')
+# set the standard display unit to satoshis
+plugin.add_option('funds_display_unit', 's',
+                  'pass the unit which should be used by default for the simple funds overview plugin')
 plugin.run()
