e72d04fb7260896a8b520924e071fc4ed2793d72
Christian Decker <decker.christian@gmail.com>
2019-01-11 22:39:18 +0100

    pytest: Add tests for htlc_accepted_hook
    Two tests: one for failures and one for in-path resolution.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

---
+++ tests/plugins/fail_htlcs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from lightning import Plugin
+
+plugin = Plugin()
+
+
+@plugin.hook("htlc_accepted")
+def on_htlc_accepted(htlc, onion, plugin):
+    plugin.log("Failing htlc on purpose")
+    plugin.log("onion: %r" % (onion))
+    return {"result": "fail", "failure_code": 16399}
+
+
+plugin.run()
