ElementsProject/lightning

commit 0ba547ee101efe7c5b14a7fbcf41b5088efc2504

gossipd: handle overflowing query properly (avoid slow 100% CPU reports)
Don't do this:
  (gdb) bt
  #0  0x00007f37ae667c40 in ?? () from /lib/x86_64-linux-gnu/libz.so.1
  #1  0x00007f37ae668b38 in ?? () from /lib/x86_64-linux-gnu/libz.so.1
  #2  0x00007f37ae669907 in deflate () from /lib/x86_64-linux-gnu/libz.so.1
  #3  0x00007f37ae674c65 in compress2 () from /lib/x86_64-linux-gnu/libz.so.1
  #4  0x000000000040cfe3 in zencode_scids (ctx=0xc1f118, scids=0x2599bc49 "\a\325{", len=176320) at gossipd/gossipd.c:218
  #5  0x000000000040d0b3 in encode_short_channel_ids_end (encoded=0x7fff8f98d9f0, max_bytes=65490) at gossipd/gossipd.c:236
  #6  0x000000000040dd28 in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290511, number_of_blocks=8) at gossipd/gossipd.c:576
  #7  0x000000000040ddc6 in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290511, number_of_blocks=16) at gossipd/gossipd.c:595
  #8  0x000000000040ddee in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290495, number_of_blocks=32) at gossipd/gossipd.c:596
  #9  0x000000000040ddc6 in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290495, number_of_blocks=64) at gossipd/gossipd.c:595
  #10 0x000000000040ddee in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290431, number_of_blocks=128) at gossipd/gossipd.c:596
  #11 0x000000000040ddc6 in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290431, number_of_blocks=256) at gossipd/gossipd.c:595
  #12 0x000000000040ddc6 in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290431, number_of_blocks=512) at gossipd/gossipd.c:595
  #13 0x000000000040ddc6 in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17290431, number_of_blocks=1024) at gossipd/gossipd.c:595
  #14 0x000000000040ddee in queue_channel_ranges (peer=0x3868fc8, first_blocknum=17289408, number_of_blocks=2047) at gossipd/gossipd.c:596

gossipd/gossipd.c
@@ -659,9 +659,14 @@ static void reply_channel_range(struct peer *peer,
 /*~ When we need to send an array of channels, it might go over our 64k packet
  * size.  If it doesn't, we recurse, splitting in two, etc.  Each message
  * indicates what blocks it contains, so the recipient knows when we're
- * finished. */
+ * finished.
+ *
+ * tail_blocks is the empty blocks at the end, in case they asked for all
+ * blocks to 4 billion.
+ */
 static void queue_channel_ranges(struct peer *peer,
-				 u32 first_blocknum, u32 number_of_blocks)
+				 u32 first_blocknum, u32 number_of_blocks,
+				 u32 tail_blocks)
 {
 	struct routing_state *rstate = peer->daemon->rstate;
 	u8 *encoded = encode_short_channel_ids_start(tmpctx);
@@ -704,7 +709,8 @@ static void queue_channel_ranges(struct peer *peer,
 
 	/* If we can encode that, fine: send it */
 	if (encode_short_channel_ids_end(&encoded, max_encoded_bytes)) {
-		reply_channel_range(peer, first_blocknum, number_of_blocks,
+		reply_channel_range(peer, first_blocknum,
+				    number_of_blocks + tail_blocks,
 				    encoded);
 		return;
 	}
@@ -717,22 +723,26 @@ static void queue_channel_ranges(struct peer *peer,
 			      first_blocknum);
 		return;
 	}
-	status_debug("queue_channel_ranges full: splitting %u+%u and %u+%u",
+	status_debug("queue_channel_ranges full: splitting %u+%u and %u+%u(+%u)",
 		     first_blocknum,
 		     number_of_blocks / 2,
 		     first_blocknum + number_of_blocks / 2,
-		     number_of_blocks - number_of_blocks / 2);
-	queue_channel_ranges(peer, first_blocknum, number_of_blocks / 2);
+		     number_of_blocks - number_of_blocks / 2,
+		     tail_blocks);
+	queue_channel_ranges(peer, first_blocknum, number_of_blocks / 2, 0);
 	queue_channel_ranges(peer, first_blocknum + number_of_blocks / 2,
-			     number_of_blocks - number_of_blocks / 2);
+			     number_of_blocks - number_of_blocks / 2,
+			     tail_blocks);
 }
 
 /*~ The peer can ask for all channels is a series of blocks.  We reply with one
  * or more messages containing the short_channel_ids. */
 static u8 *handle_query_channel_range(struct peer *peer, const u8 *msg)
 {
+	struct routing_state *rstate = peer->daemon->rstate;
 	struct bitcoin_blkid chain_hash;
-	u32 first_blocknum, number_of_blocks;
+	u32 first_blocknum, number_of_blocks, tail_blocks;
+	struct short_channel_id last_scid;
 
 	if (!fromwire_query_channel_range(msg, &chain_hash,
 					  &first_blocknum, &number_of_blocks)) {
@@ -751,14 +761,25 @@ static u8 *handle_query_channel_range(struct peer *peer, const u8 *msg)
 		return NULL;
 	}
 
-	/* This checks for 32-bit overflow! */
-	if (first_blocknum + number_of_blocks < first_blocknum) {
-		return towire_errorfmt(peer, NULL,
-				       "query_channel_range overflow %u+%u",
-				       first_blocknum, number_of_blocks);
-	}
+	/* If they ask for number_of_blocks UINTMAX, and we have to divide
+	 * and conquer, we'll do a lot of unnecessary work.  Cap it at the
+	 * last value we have, then send an empty reply. */
+	if (uintmap_last(&rstate->chanmap, &last_scid.u64)) {
+		u32 last_block = short_channel_id_blocknum(&last_scid);
 
-	queue_channel_ranges(peer, first_blocknum, number_of_blocks);
+		/* u64 here avoids overflow on number_of_blocks
+		   UINTMAX for example */
+		if ((u64)first_blocknum + number_of_blocks > last_block) {
+			tail_blocks = first_blocknum + number_of_blocks
+				- last_block - 1;
+			number_of_blocks -= tail_blocks;
+		} else
+			tail_blocks = 0;
+	} else
+		tail_blocks = 0;
+
+	queue_channel_ranges(peer, first_blocknum, number_of_blocks,
+			     tail_blocks);
 	return NULL;
 }
 
@@ -2349,6 +2370,13 @@ static struct io_plan *query_channel_range(struct io_conn *conn,
 
 	if (peer->query_channel_blocks) {
 		status_broken("query_channel_range: previous query active");
+		goto fail;
+	}
+
+	/* Check for overflow on 32-bit machines! */
+	if (BITMAP_NWORDS(number_of_blocks) < number_of_blocks / BITMAP_WORD_BITS) {
+		status_broken("query_channel_range: huge number_of_blocks (%u) not supported",
+			number_of_blocks);
 		goto fail;
 	}
 

tests/test_gossip.py
@@ -558,7 +558,6 @@ def test_routing_gossip(node_factory, bitcoind):
 
 
 @unittest.skipIf(not DEVELOPER, "needs DEVELOPER=1")
-@pytest.mark.xfail(strict=True)
 def test_gossip_query_channel_range(node_factory, bitcoind):
     l1, l2, l3, l4 = node_factory.line_graph(4, opts={'log-level': 'io'},
                                              fundchannel=False)
@@ -672,8 +671,8 @@ def test_gossip_query_channel_range(node_factory, bitcoind):
                                          first=0,
                                          num=1000000)
 
-    # Turns out it sends huge number of empty replies here.
-    l1.daemon.wait_for_logs([r'\[IN\] 0108'] * 21)
+    # Turns out it sends: 0+53, 53+26, 79+13, 92+7, 99+3, 102+2, 104+1, 105+999895
+    l1.daemon.wait_for_logs([r'\[IN\] 0108'] * 8)
 
     # It should definitely have split
     assert ret['final_first_block'] != 0 or ret['final_num_blocks'] != 1000000
@@ -683,11 +682,11 @@ def test_gossip_query_channel_range(node_factory, bitcoind):
     assert ret['short_channel_ids'][1] == scid23
     l2.daemon.wait_for_log('queue_channel_ranges full: splitting')
 
-    # Test overflow case doesn't split forever; should only get 32 for this.
+    # Test overflow case doesn't split forever; should still only get 8 for this
     ret = l1.rpc.dev_query_channel_range(id=l2.info['id'],
                                          first=1,
                                          num=429496000)
-    l1.daemon.wait_for_logs([r'\[IN\] 0108'] * 32)
+    l1.daemon.wait_for_logs([r'\[IN\] 0108'] * 8)
 
     # And no more!
     time.sleep(1)

commit 35a73b0d87f476cdd35b90d233292902075c692b

short_channel_id: make mk_short_channel_id return a failure.
We had a bug 0ba547ee101efe7c5b14a7fbcf41b5088efc2504 caused by
short_channel_id overflow.  If we'd caught this, we'd have terminated
the peer instead of crashing, so add appropriate checks.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

bitcoin/short_channel_id.c
@@ -22,12 +22,17 @@
 */
 
 
-void mk_short_channel_id(struct short_channel_id *scid,
-			 u32 blocknum, u32 txnum, u16 outnum)
+bool mk_short_channel_id(struct short_channel_id *scid,
+			 u64 blocknum, u64 txnum, u64 outnum)
 {
+	if ((blocknum & 0xFFFFFF) != blocknum
+	    || (txnum & 0xFFFFFF) != txnum
+	    || (outnum & 0xFFFF) != outnum)
+		return false;
 	scid->u64 = (((u64)blocknum & 0xFFFFFF) << 40 |
 		     ((u64)txnum & 0xFFFFFF) << 16 |
 		     (outnum & 0xFFFF));
+	return true;
 }
 
 bool short_channel_id_from_str(const char *str, size_t strlen,
@@ -50,8 +55,8 @@ bool short_channel_id_from_str(const char *str, size_t strlen,
 #else
 	matches = sscanf(buf, "%ux%ux%hu", &blocknum, &txnum, &outnum);
 #endif
-	mk_short_channel_id(dst, blocknum, txnum, outnum);
-	return matches == 3;
+	return matches == 3
+		&& mk_short_channel_id(dst, blocknum, txnum, outnum);
 }
 
 char *short_channel_id_to_str(const tal_t *ctx, const struct short_channel_id *scid)

bitcoin/short_channel_id.h
@@ -47,8 +47,9 @@ static inline u16 short_channel_id_outnum(const struct short_channel_id *scid)
 	return scid->u64 & 0xFFFF;
 }
 
-void mk_short_channel_id(struct short_channel_id *scid,
-			 u32 blocknum, u32 txnum, u16 outnum);
+/* Returns false if blocknum, txnum or outnum require too many bits */
+bool WARN_UNUSED_RESULT mk_short_channel_id(struct short_channel_id *scid,
+					    u64 blocknum, u64 txnum, u64 outnum);
 
 bool WARN_UNUSED_RESULT short_channel_id_from_str(const char *str, size_t strlen,
 						  struct short_channel_id *dst);

gossipd/gossipd.c
@@ -664,13 +664,14 @@ static void reply_channel_range(struct peer *peer,
  * tail_blocks is the empty blocks at the end, in case they asked for all
  * blocks to 4 billion.
  */
-static void queue_channel_ranges(struct peer *peer,
+static bool queue_channel_ranges(struct peer *peer,
 				 u32 first_blocknum, u32 number_of_blocks,
 				 u32 tail_blocks)
 {
 	struct routing_state *rstate = peer->daemon->rstate;
 	u8 *encoded = encode_short_channel_ids_start(tmpctx);
 	struct short_channel_id scid;
+	bool scid_ok;
 
 	/* BOLT #7:
 	 *
@@ -688,10 +689,12 @@ static void queue_channel_ranges(struct peer *peer,
 
 	/* Avoid underflow: we don't use block 0 anyway */
 	if (first_blocknum == 0)
-		mk_short_channel_id(&scid, 1, 0, 0);
+		scid_ok = mk_short_channel_id(&scid, 1, 0, 0);
 	else
-		mk_short_channel_id(&scid, first_blocknum, 0, 0);
+		scid_ok = mk_short_channel_id(&scid, first_blocknum, 0, 0);
 	scid.u64--;
+	if (!scid_ok)
+		return false;
 
 	/* We keep a `uintmap` of `short_channel_id` to `struct chan *`.
 	 * Unlike a htable, it's efficient to iterate through, but it only
@@ -712,7 +715,7 @@ static void queue_channel_ranges(struct peer *peer,
 		reply_channel_range(peer, first_blocknum,
 				    number_of_blocks + tail_blocks,
 				    encoded);
-		return;
+		return true;
 	}
 
 	/* It wouldn't all fit: divide in half */
@@ -721,7 +724,7 @@ static void queue_channel_ranges(struct peer *peer,
 		/* We always assume we can send 1 blocks worth */
 		status_broken("Could not fit scids for single block %u",
 			      first_blocknum);
-		return;
+		return false;
 	}
 	status_debug("queue_channel_ranges full: splitting %u+%u and %u+%u(+%u)",
 		     first_blocknum,
@@ -729,10 +732,10 @@ static void queue_channel_ranges(struct peer *peer,
 		     first_blocknum + number_of_blocks / 2,
 		     number_of_blocks - number_of_blocks / 2,
 		     tail_blocks);
-	queue_channel_ranges(peer, first_blocknum, number_of_blocks / 2, 0);
-	queue_channel_ranges(peer, first_blocknum + number_of_blocks / 2,
-			     number_of_blocks - number_of_blocks / 2,
-			     tail_blocks);
+	return queue_channel_ranges(peer, first_blocknum, number_of_blocks / 2, 0)
+		&& queue_channel_ranges(peer, first_blocknum + number_of_blocks / 2,
+					number_of_blocks - number_of_blocks / 2,
+					tail_blocks);
 }
 
 /*~ The peer can ask for all channels is a series of blocks.  We reply with one
@@ -778,8 +781,12 @@ static u8 *handle_query_channel_range(struct peer *peer, const u8 *msg)
 	} else
 		tail_blocks = 0;
 
-	queue_channel_ranges(peer, first_blocknum, number_of_blocks,
-			     tail_blocks);
+	if (!queue_channel_ranges(peer, first_blocknum, number_of_blocks,
+				  tail_blocks))
+		return towire_errorfmt(peer, NULL,
+				       "Invalid query_channel_range %u+%u",
+				       first_blocknum, number_of_blocks + tail_blocks);
+
 	return NULL;
 }
 

lightningd/peer_control.c
@@ -537,9 +537,15 @@ static enum watch_result funding_lockin_cb(struct lightningd *ld,
 
 		loc = wallet_transaction_locate(tmpctx, ld->wallet, txid);
 		channel->scid = tal(channel, struct short_channel_id);
-		mk_short_channel_id(channel->scid,
-				    loc->blkheight, loc->index,
-				    channel->funding_outnum);
+		if (!mk_short_channel_id(channel->scid,
+					 loc->blkheight, loc->index,
+					 channel->funding_outnum)) {
+			channel_fail_permanent(channel, "Invalid funding scid %u:%u:%u",
+					       loc->blkheight, loc->index,
+					       channel->funding_outnum);
+			return DELETE_WATCH;
+		}
+
 		/* We've added scid, update */
 		wallet_channel_save(ld->wallet, channel);
 	}

wallet/wallet.c
@@ -2166,8 +2166,11 @@ wallet_outpoint_spend(struct wallet *w, const tal_t *ctx, const u32 blockheight,
 		assert(res == SQLITE_ROW);
 
 		scid = tal(ctx, struct short_channel_id);
-		mk_short_channel_id(scid, sqlite3_column_int(stmt, 0),
-				    sqlite3_column_int(stmt, 1), outnum);
+		if (!mk_short_channel_id(scid, sqlite3_column_int(stmt, 0),
+					 sqlite3_column_int(stmt, 1), outnum))
+			fatal("wallet_outpoint_spend: invalid scid %u:%u:%u",
+			      sqlite3_column_int(stmt, 0),
+			      sqlite3_column_int(stmt, 1), outnum);
 		db_stmt_done(stmt);
 		return scid;
 	}