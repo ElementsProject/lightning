5596bce670eae705d8a975c8fbd182c0f8da42ec
Christian Decker <decker.christian@gmail.com>
2019-02-19 20:50:38 +0100

    pylightning: Add some more documentation for the python lib
    It was looking a bit minimal on pypi :-)
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- contrib/pylightning/setup.py
+++ contrib/pylightning/setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='pylightning',
       version='0.0.6',
       description='Client library for lightningd',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/ElementsProject/lightning',
       author='Christian Decker',
       author_email='decker.christian@gmail.com',
