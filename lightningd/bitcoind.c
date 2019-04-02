d650bd69b0d1950ef650b98de753d19511e5d78a
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-04-01 22:24:35 -0400

    log improvement

--- lightningd/bitcoind.c
+++ lightningd/bitcoind.c
@@ -117,8 +117,12 @@ static char *bcli_args(const tal_t *ctx, struct bitcoin_cli *bcli)
 	char *ret = tal_strdup(ctx, bcli->args[0]);
 
 	for (i = 1; bcli->args[i]; i++) {
-            if (!strstarts(bcli->args[i], "-rpcuser") && !strstarts(bcli->args[i], "-rpcpassword")) {
-                ret = tal_strcat(ctx, take(ret), " ");
+            ret = tal_strcat(ctx, take(ret), " ");
+            if (strstarts(bcli->args[i], "-rpcpassword") {
+                    ret = tal_strcat(ctx, take(ret), "-rpcpassword=...");
+            } else if (strstarts(bcli->args[i], "-rpcuser") {
+                    ret = tal_strcat(ctx, take(ret), "-rpcuser=...");
+            } else {
                 ret = tal_strcat(ctx, take(ret), bcli->args[i]);
             }
 	}
