7483e1bf17f2aa2d3410d2c712dd892dbc5aabc5
Michael Schmoock <mschmoock@cocus.com>
2019-05-21 20:45:40 +0200

    pytest: mbtc unit and representations

--- contrib/pylightning/tests/test_units.py
+++ contrib/pylightning/tests/test_units.py
@@ -1,4 +1,36 @@
+from decimal import Decimal
 from lightning import Millisatoshi
+
+
+def test_units():
+    amount = Millisatoshi(42)
+    assert amount.millisatoshis == 42
+    amount = Millisatoshi('42msat')
+    assert amount.millisatoshis == 42
+    amount = Millisatoshi('42sat')
+    assert amount.millisatoshis == 42 * 1000
+    amount = Millisatoshi('42mbtc')
+    assert amount.millisatoshis == 42 * 1000 * 10**5
+    amount = Millisatoshi('42btc')
+    assert amount.millisatoshis == 42 * 1000 * 10**8
+    # also with decimals
+    amount = Millisatoshi('0.042sat')
+    assert amount.millisatoshis == 42
+    amount = Millisatoshi('0.42mbtc')
+    assert amount.millisatoshis == 42 * 1000 * 10**3
+    amount = Millisatoshi('0.42btc')
+    assert amount.millisatoshis == 42 * 1000 * 10**6
+    amount = Millisatoshi('.42btc')
+    assert amount.millisatoshis == 42 * 1000 * 10**6
+
+
+def test_misc():
+    amount = Millisatoshi('42mbtc')
+    assert amount.to_mbtc() == Decimal(42)
+    assert amount.to_mbtc_str() == '42.00000mbtc'
+    amount = Millisatoshi('0.42mbtc')
+    assert amount.to_mbtc() == Decimal('0.42')
+    assert amount.to_mbtc_str() == '0.42000mbtc'
 
 
 def test_to_approx_str():
