ad24d94c7bcc5de83e637ba2e890f6ef6d9a6118
Rusty Russell <rusty@rustcorp.com.au>
2019-05-31 17:00:32 +0930

    lightningd: make 'spendable_msatoshi' more realistic.
    Take into account the fee we'd have to pay if we're the funder, and
    also drop to 0 if the amount is less than the smallest HTLC the peer
    will accept.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wallet/test/run-wallet.c
+++ wallet/test/run-wallet.c
@@ -112,6 +112,13 @@ u32 get_block_height(const struct chain_topology *topo UNNEEDED)
 /* Generated stub for get_chainparams */
 const struct chainparams *get_chainparams(const struct lightningd *ld UNNEEDED)
 { fprintf(stderr, "get_chainparams called!\n"); abort(); }
+/* Generated stub for htlc_is_trimmed */
+bool htlc_is_trimmed(enum side htlc_owner UNNEEDED,
+		     struct amount_msat htlc_amount UNNEEDED,
+		     u32 feerate_per_kw UNNEEDED,
+		     struct amount_sat dust_limit UNNEEDED,
+		     enum side side UNNEEDED)
+{ fprintf(stderr, "htlc_is_trimmed called!\n"); abort(); }
 /* Generated stub for invoices_create */
 bool invoices_create(struct invoices *invoices UNNEEDED,
 		     struct invoice *pinvoice UNNEEDED,
