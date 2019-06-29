3d595eb60ab97f78550f461b59de5faab011dc08
Christian Decker <decker.christian@gmail.com>
2019-06-29 18:25:45 +0200

    pytest: Strengthen the htlc_accepted tests
    We were having a few issues with malformed data in the past, so this time we
    really check that stuff.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- tests/plugins/hold_htlcs.py
+++ tests/plugins/hold_htlcs.py
@@ -8,16 +8,26 @@ settled/forwarded/
 
 
 from lightning import Plugin
+import json
+import os
+import tempfile
 import time
-
 
 plugin = Plugin()
 
 
 @plugin.hook("htlc_accepted")
 def on_htlc_accepted(htlc, onion, plugin):
+    # Stash the onion so the test can check it
+    fname = os.path.join(tempfile.mkdtemp(), "onion.json")
+    with open(fname, 'w') as f:
+        f.write(json.dumps(onion))
+
     plugin.log("Holding onto an incoming htlc for 10 seconds")
+
     time.sleep(10)
+
+    print("Onion written to {}".format(fname))
 
     # Give the tester something to look for
     plugin.log("htlc_accepted hook called")
