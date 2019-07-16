01e292a3ffdecdf9043c131a49d5aba2f0a13d5b
lisa neigut <niftynei@gmail.com>
2019-07-15 19:20:45 -0500

    Travis: use inline mako install
    Needed for next patch.

--- .travis/build.sh
+++ .travis/build.sh
@@ -26,6 +26,7 @@ if [ ! -f dependencies/bin/bitcoind ]; then
 fi
 
 pyenv global 3.7
+pip3 install --user --quiet mako
 pip3 install --user --quiet -r tests/requirements.txt
 pip3 install --quiet \
      pytest-test-groups==1.0.3
