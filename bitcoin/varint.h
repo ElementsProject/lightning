eed19e6adae0298480bc6aee9ae47d3cfe2d2872
Christian Decker <decker.christian@gmail.com>
2019-07-18 14:07:07 +0200

    sphinx: Switch to big-endian number encoding
    See https://github.com/lightningnetwork/lightning-rfc/pull/619 and
    https://github.com/lightningnetwork/lightning-rfc/pull/619 for discussion.
    
    Signed-off-by: Christian Decker <decker.christian@gmail.com>

--- bitcoin/varint.h
+++ bitcoin/varint.h
@@ -14,4 +14,13 @@ size_t varint_put(u8 buf[VARINT_MAX_LEN], varint_t v);
 
 /* Returns bytes used: 0 if max_len too small. */
 size_t varint_get(const u8 *p, size_t max_len, varint_t *val);
+
+
+/* Big-endian variant of varint_put, used in lightning */
+size_t bigsize_put(u8 buf[VARINT_MAX_LEN], varint_t v);
+
+/* Big-endian variant of varint_get, used in lightning */
+size_t bigsize_get(const u8 *p, size_t max, varint_t *val);
+
+
 #endif /* LIGHTNING_BITCOIN_VARINT_H */
