64fe2f9b835e6344628d2091a1efe11635734cf1
darosior <darosior@protonmail.com>
2019-07-21 13:39:15 +0200

    Plugins: Add a notification for invoice payment
    Similarly to the 'invoice_payment' hook

--- lightningd/notification.h
+++ lightningd/notification.h
@@ -1,6 +1,8 @@
 #ifndef LIGHTNING_LIGHTNINGD_NOTIFICATION_H
 #define LIGHTNING_LIGHTNINGD_NOTIFICATION_H
 #include "config.h"
+#include <ccan/json_escape/json_escape.h>
+#include <common/amount.h>
 #include <lightningd/jsonrpc.h>
 #include <lightningd/lightningd.h>
 #include <lightningd/log.h>
@@ -13,5 +15,8 @@ void notify_connect(struct lightningd *ld, struct node_id *nodeid,
 void notify_disconnect(struct lightningd *ld, struct node_id *nodeid);
 
 void notify_warning(struct lightningd *ld, struct log_entry *l);
+
+void notify_invoice_payment(struct lightningd *ld, struct amount_msat amount,
+			    struct preimage preimage, const struct json_escape *label);
 
 #endif /* LIGHTNING_LIGHTNINGD_NOTIFICATION_H */
