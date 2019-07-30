ElementsProject/lightning

commit 9e5b9a31c9517922052e4464aa7adc661bfe977a

FreeBSD: fix build.
This makes it build for me on FreeBSD 11:
1. $(MAKE) has to passed through into update-mocks.
2. FreeBSD sed doesn't turn \n into a newline on RHS.
3. Bash and mako dependencies were missing from INSTALL.md

Fixes: #2850
Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

Makefile
@@ -367,7 +367,7 @@ coverage: coverage/coverage.info
 # We make libwallycore.la a dependency, so that it gets built normally, without ncc.
 # Ncc can't handle the libwally source code (yet).
 ncc: external/libwally-core/src/libwallycore.la
-	make CC="ncc -ncgcc -ncld -ncfabs" AR=nccar LD=nccld
+	$(MAKE) CC="ncc -ncgcc -ncld -ncfabs" AR=nccar LD=nccld
 
 # Ignore test/ directories.
 TAGS: FORCE

tools/test/Makefile
@@ -31,7 +31,7 @@ tools/test/gen_test.h:
 
 tools/test/gen_test.c:
 	$(BOLT_GEN) --page impl ${@:.c=.h} test_type < tools/test/test_cases > $@
-	@tools/update-mocks.sh "$@"
+	@MAKE=$(MAKE) tools/update-mocks.sh "$@"
 
 tools/test/gen_print.h: wire/gen_onion_wire.h
 	$(BOLT_GEN) -P --page header $@ test_type < tools/test/test_cases > $@

tools/mockup.sh
@@ -20,7 +20,8 @@ for SYMBOL; do
     # has notleak_ as a declaration, and then an inline).
     WHERE=$(grep -nH "^[a-zA-Z0-9_ (),]* [*]*$SYMBOL(" ./*/*.h | head -n1)
     if [ x"$WHERE" != x ]; then
-	STUB='\n{ fprintf(stderr, "'$SYMBOL' called!\\n"); abort(); }'
+	STUB='\
+{ fprintf(stderr, "'$SYMBOL' called!\\n"); abort(); }'
     else
 	echo "/* Could not find declaration for $SYMBOL */"
 	continue

tools/update-mocks.sh
@@ -18,7 +18,7 @@ if [ -n "$START" ]; then
     head -n "$START" "${BASE}.old" > "$FILE"
     tail -n +"$END" "${BASE}.old" >> "$FILE"
     # Try to make binary.
-    if ! make "${FILE/%.c/}" 2> "${BASE}.err" >/dev/null; then
+    if ! $MAKE "${FILE/%.c/}" 2> "${BASE}.err" >/dev/null; then
 	tools/mockup.sh < "${BASE}.err" >> "${BASE}.stubs"
 	# If there are no link errors, maybe compile fail for other reason?
 	if ! grep -F -q 'Generated stub for' "${BASE}.stubs"; then

commit 13891e42c3f976ddd2eff1ef207ec9c805244de3

Makefile: fix `update-mocks`
Broken by 9e5b9a31c9517922052e4464aa7adc661bfe977a: it needs $MAKE set.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

Makefile
@@ -451,7 +451,7 @@ clean: wire-clean
 	find . -name '*.nccout' -delete
 
 update-mocks/%: %
-	@tools/update-mocks.sh "$*"
+	@MAKE=$(MAKE) tools/update-mocks.sh "$*"
 
 unittest/%: %
 	$(VG) $(VG_TEST_ARGS) $* > /dev/null