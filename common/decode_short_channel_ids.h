e7728c0eea48d877120773d071b3c9b309d3d64f
Rusty Russell <rusty@rustcorp.com.au>
2019-08-10 01:36:01 +0930

    common: implement decoding of query-flags for query_short_channel_ids.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/decode_short_channel_ids.h
+++ common/decode_short_channel_ids.h
@@ -3,6 +3,9 @@
 #include "config.h"
 #include <ccan/short_types/short_types.h>
 #include <ccan/tal/tal.h>
+#include <common/bigsize.h>
+
+struct tlv_query_short_channel_ids_tlvs_query_flags;
 
 /* BOLT #7:
  *
@@ -16,4 +19,31 @@ enum scid_encode_types {
 };
 
 struct short_channel_id *decode_short_ids(const tal_t *ctx, const u8 *encoded);
+
+/* BOLT-61a1365a45cc8b463ddbbe3429d350f8eac787dd #7:
+ *
+ * `encoded_query_flags` is an array of bitfields, one varint per bitfield,
+ * one bitfield for each `short_channel_id`. Bits have the following meaning:
+ *
+ * | Bit Position  | Meaning                                  |
+ * | ------------- | ---------------------------------------- |
+ * | 0             | Sender wants `channel_announcement`      |
+ * | 1             | Sender wants `channel_update` for node 1 |
+ * | 2             | Sender wants `channel_update` for node 2 |
+ * | 3             | Sender wants `node_announcement` for node 1 |
+ * | 4             | Sender wants `node_announcement` for node 2 |
+ */
+enum scid_query_flag {
+	SCID_QF_ANNOUNCE = 0x1,
+	SCID_QF_UPDATE1 = 0x2,
+	SCID_QF_UPDATE2 = 0x4,
+	SCID_QF_NODE1 = 0x8,
+	SCID_QF_NODE2 = 0x10,
+};
+
+#if EXPERIMENTAL_FEATURES
+bigsize_t *decode_scid_query_flags(const tal_t *ctx,
+				   const struct tlv_query_short_channel_ids_tlvs_query_flags *qf);
+#endif /* EXPERIMENTAL_FEATURES */
+
 #endif /* LIGHTNING_COMMON_DECODE_SHORT_CHANNEL_IDS_H */
