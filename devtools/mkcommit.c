b4eecba6b4668eff4811c3aeeb82a38167a052a5
lisa neigut <niftynei@gmail.com>
2019-08-08 22:36:57 -0500

    fixup mkcommit's reverse ordered chainparams

--- devtools/mkcommit.c
+++ devtools/mkcommit.c
@@ -368,7 +368,7 @@ int main(int argc, char *argv[])
 	if (!per_commit_point(&localseed, &local_per_commit_point, commitnum))
 		errx(1, "Bad deriving local per-commitment-point");
 
-	local_txs = channel_txs(chainparams, NULL, &htlcmap, &wscripts, channel,
+	local_txs = channel_txs(NULL, chainparams, &htlcmap, &wscripts, channel,
 				&local_per_commit_point, commitnum, LOCAL);
 
 	printf("## local_commitment\n"
@@ -469,7 +469,7 @@ int main(int argc, char *argv[])
 	/* Create the remote commitment tx */
 	if (!per_commit_point(&remoteseed, &remote_per_commit_point, commitnum))
 		errx(1, "Bad deriving remote per-commitment-point");
-	remote_txs = channel_txs(chainparams, NULL, &htlcmap, &wscripts, channel,
+	remote_txs = channel_txs(NULL, chainparams, &htlcmap, &wscripts, channel,
 				 &remote_per_commit_point, commitnum, REMOTE);
 	remote_txs[0]->input_amounts[0]
 		= tal_dup(remote_txs[0], struct amount_sat, &funding_amount);
