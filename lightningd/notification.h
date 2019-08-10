25d79c5c3fdfc5f9c6ede46401082a96c3d85766
trueptolemy <trueptolemy@foxmail.com>
2019-08-11 05:32:13 +0800

    notification: Add registeration interface

--- lightningd/notification.h
+++ lightningd/notification.h
@@ -3,6 +3,7 @@
 #include "config.h"
 #include <bitcoin/short_channel_id.h>
 #include <bitcoin/tx.h>
+#include <ccan/autodata/autodata.h>
 #include <ccan/json_escape/json_escape.h>
 #include <ccan/time/time.h>
 #include <common/amount.h>
@@ -16,6 +17,22 @@
 #include <wire/gen_onion_wire.h>
 
 bool notifications_have_topic(const char *topic);
+
+struct notification {
+	const char *topic;
+	/* the serialization interface */
+	void *serialize;
+};
+
+AUTODATA_TYPE(notifications, struct notification);
+
+/* FIXME: Find a way to avoid back-to-back declaration and definition */
+#define REGISTER_NOTIFICATION(topic, serialize)                                  \
+	struct notification topic##_notification_gen = {                         \
+		stringify(topic),                                                \
+		serialize,                                                       \
+	};                                                                       \
+	AUTODATA(notifications, &topic##_notification_gen);
 
 void notify_connect(struct lightningd *ld, struct node_id *nodeid,
 		    struct wireaddr_internal *addr);
