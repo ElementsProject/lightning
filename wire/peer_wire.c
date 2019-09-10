1e50e27becc0612fe15fd2610f7a5bc06c2379bb
lisa neigut <niftynei@gmail.com>
2019-09-09 22:45:23 -0500

    df: add wires for dual funding messages
    plus tests, with some of the corruption tests taken out, since it
    crashes. will be re-enabled in the next commit.

--- wire/peer_wire.c
+++ wire/peer_wire.c
@@ -31,6 +31,14 @@ static bool unknown_type(enum wire_type t)
 	case WIRE_QUERY_CHANNEL_RANGE:
 	case WIRE_REPLY_CHANNEL_RANGE:
 	case WIRE_GOSSIP_TIMESTAMP_FILTER:
+#if EXPERIMENTAL_FEATURES
+	case WIRE_OPEN_CHANNEL2:
+	case WIRE_ACCEPT_CHANNEL2:
+	case WIRE_FUNDING_COMPOSE:
+	case WIRE_ACCEPTER_SIGS:
+	case WIRE_INIT_RBF:
+	case WIRE_ACK_RBF:
+#endif /* EXPERIMENTAL_FEATURES */
 		return false;
 	}
 	return true;
@@ -68,6 +76,14 @@ bool is_msg_for_gossipd(const u8 *cursor)
 	case WIRE_CHANNEL_REESTABLISH:
 	case WIRE_ANNOUNCEMENT_SIGNATURES:
 	case WIRE_GOSSIP_TIMESTAMP_FILTER:
+#if EXPERIMENTAL_FEATURES
+	case WIRE_OPEN_CHANNEL2:
+	case WIRE_ACCEPT_CHANNEL2:
+	case WIRE_FUNDING_COMPOSE:
+	case WIRE_ACCEPTER_SIGS:
+	case WIRE_INIT_RBF:
+	case WIRE_ACK_RBF:
+#endif /* EXPERIMENTAL_FEATURES */
 		break;
 	}
 	return false;
