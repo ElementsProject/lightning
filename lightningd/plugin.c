14247283b2934d0dc352f09e3dc4c389097008d6
Christian Decker <decker.christian@gmail.com>
2019-09-17 13:03:11 +0200

    plugin: Tell the plugin which network we run on
    The fundchannel plugin needs to know how to build a transaction, so we need to
    tell it which chainparams to use. Also adds `chainparams` as a global, since
    that seems to be the way to do things in plugins.

--- lightningd/plugin.c
+++ lightningd/plugin.c
@@ -1049,6 +1049,9 @@ plugin_populate_init_request(struct plugin *plugin, struct jsonrpc_request *req)
 	json_add_string(req->stream, "lightning-dir", ld->config_dir);
 	json_add_string(req->stream, "rpc-file", ld->rpc_filename);
 	json_add_bool(req->stream, "startup", plugin->plugins->startup);
+	json_add_string(
+	    req->stream, "network",
+	    plugin->plugins->ld->topology->bitcoind->chainparams->network_name);
 	json_object_end(req->stream);
 }
 
