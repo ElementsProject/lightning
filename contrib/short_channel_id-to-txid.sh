015df1d52a706f4d000daa3be2c9655b05f4cf5b
lambrosini <luca.ambrosini@supsi.ch>
2019-09-27 17:52:37 +0200

    Add compatibility with the new shortchannelid syntax with x as separator

--- contrib/short_channel_id-to-txid.sh
+++ contrib/short_channel_id-to-txid.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-if [ "$#" != 1 ]; then
-    echo Usage: "$0" "short:channel:id" >&2
+if [ "$#" != 1 ]
+then
+    echo Usage: "$0" "shortchannelid (e.g. 532046x1702x0 or 532046:1702:0)" >&2
     echo Uses bitcoin-cli to extract the actual txid >&2
-    exit 1
+    exit -1
 fi
 
-BLOCK=$(echo "$1" | cut -d: -f1)
+# Try to segment using both x and : as delimiters (compatibility with the old shortchannelid standard)
+BLOCK=$(echo "$1" | cut -dx -f1)
+TXNUM=$(echo "$1" | cut -dx -f2)
+
+if [ "$BLOCK" == "$1" -a "$TXNUM" == "$1" ]
+then
+    BLOCK=$(echo "$1" | cut -d: -f1)
+    TXNUM=$(echo "$1" | cut -d: -f2)
+fi
 TXNUM=$(echo "$1" | cut -d: -f2)
 
 bitcoin-cli getblock "$(bitcoin-cli getblockhash "$BLOCK")" true | grep '^    "' | head -n "$((TXNUM + 1))" | tail -n 1 | tr -dc '0-9a-f\n'
