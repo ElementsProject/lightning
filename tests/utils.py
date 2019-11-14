bb06bec891673c40551fe780da38e99233137eaa
Rusty Russell <rusty@rustcorp.com.au>
2019-11-14 14:04:29 +1030

    features: set OPT_VAR_ONION (bit 9) iff EXPERIMENTAL_FEATURES
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/utils.py
+++ tests/utils.py
@@ -4,3 +4,13 @@ from pyln.testing.utils import env, only_one, wait_for, write_config, TailablePr
 
 EXPERIMENTAL_FEATURES = env("EXPERIMENTAL_FEATURES", "0") == "1"
 COMPAT = env("COMPAT", "1") == "1"
+
+
+def expected_features():
+    """Return the expected features hexstring for this configuration"""
+    if EXPERIMENTAL_FEATURES:
+        # features 1, 3, 7, 9, 11 and 13 (0x2aa2).
+        return "2aa2"
+    else:
+        # features 1, 3, 7, 11 and 13 (0x28a2).
+        return "28a2"
