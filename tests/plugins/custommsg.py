b0529843acb38f9e18cf31756be736f3c505aa24
Christian Decker <decker.christian@gmail.com>
2019-12-05 00:16:11 +0100

    pytest: Add a plugin for custommsgs and check that they get the msgs
    This completes the custommsg epic, finally we are back where we began all that
    time ago (about 4 hours really...): in a plugin that implements some custom
    logic.

---
+++ tests/plugins/custommsg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from pyln.client import Plugin
+
+plugin = Plugin()
+
+
+@plugin.hook('custommsg')
+def on_custommsg(peer_id, message, plugin, **kwargs):
+    plugin.log("Got a custom message {msg} from peer {peer_id}".format(
+        msg=message,
+        peer_id=peer_id
+    ))
+
+
+plugin.run()
