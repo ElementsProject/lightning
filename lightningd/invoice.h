3c6e33ab8b4e74b01f58f0c9d050aaf229e5bdf9
Rusty Russell <rusty@rustcorp.com.au>
2019-12-12 10:25:45 +1030

    lightningd: split invoice check into separate function.
    We now return the same error for various "does not match this
    invoice", so it makes sense to encapsulate these checks.  We'll also
    want to expose this for multi-part payments.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/invoice.h
+++ lightningd/invoice.h
@@ -8,6 +8,24 @@ struct htlc_in;
 struct lightningd;
 struct sha256;
 
+
+/**
+ * invoice_check_payment - check if this payment would be valid
+ * @ctx: tal context to allocate return off
+ * @ld: lightningd
+ * @payment_hash: hash of preimage they want.
+ * @msat: amount they offer to pay.
+ * @payment_secret: they payment secret they sent, if any.
+ *
+ * Returns NULL if there's a problem, otherwise returns the invoice details.
+ */
+const struct invoice_details *
+invoice_check_payment(const tal_t *ctx,
+		      struct lightningd *ld,
+		      const struct sha256 *payment_hash,
+		      const struct amount_msat msat,
+		      const struct secret *payment_secret);
+
 /**
  * invoice_try_pay - process payment for this payment_hash, amount msat.
  * @ld: lightningd
