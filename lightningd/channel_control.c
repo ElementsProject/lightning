cb63f6111fe5d010942310a70f81673f67f726e5
lisa neigut <niftynei@gmail.com>
2019-12-11 18:46:08 -0600

    channel control: don't allow peer to cancel channel
    restrict fundchannel_cancel usage to only the opener side
    
    Changelog-Changed: Only the opener of a fundchannel can cancel the channel open with fundchannel_cancel

--- lightningd/channel_control.c
+++ lightningd/channel_control.c
@@ -726,6 +726,11 @@ struct command_result *cancel_channel_before_broadcast(struct command *cmd,
 					    buffer + cidtok->start);
 	}
 
+	if (cancel_channel->funder == REMOTE)
+		return command_fail(cmd, LIGHTNINGD,
+				    "Cannot cancel channel that was "
+				    "initiated by peer");
+
 	/* Check if we broadcast the transaction. (We store the transaction type into DB
 	 * before broadcast). */
 	enum wallet_tx_type type;
