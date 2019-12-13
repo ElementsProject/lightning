b2c4d5e9528b3291fa8448086445a4cbab92c0c2
Vasil Dimov <vd@FreeBSD.org>
2019-12-13 21:26:15 +0100

    build: fix developer mode compilation on FreeBSD
    Recent FreeBSD versions use LLVM's lld as a linker, not the GNU ld.
    Their behavior slightly differs, so adapt the build system to handle
    either one.
    
    * The LLVM's linker prints "undefined symbol:" instead of
      "undefined reference to". Tweak tools/mockup.sh to also look for that
      message.
    
    * The LLVM's linker may only print the first dozen errors (omitting
      the rest to avoid flooding the screen). tools/update-mocks.sh relies
      on getting all errors as it extracts the missing symbols' names from
      the error output and creates mocks for them. Detect if errors were
      omitted and re-run, telling the linker to not omit any. The GNU linker
      does not support -error-limit=0, so unfortunately we can't just run
      with that option unconditionally from the first attempt.
    
    * Nit: FreeBSD's sed(1) prints "t" for "\t" instead of a horizontal tab.
      Use a verbatim tab in the command, instead of "\t" which works on
      both.
    
    Changelog-Fixed: Developer mode compilation on FreeBSD.

--- tools/mockup.sh
+++ tools/mockup.sh
@@ -6,8 +6,13 @@ if [ $# -eq 0 ]; then
     set -- $(while read -r LINE; do
 	case "$LINE" in
 	    *undefined\ reference\ to*)
+		# file.cc:(.text+0x10): undefined reference to `foo()'
 		LINE=${LINE#*undefined reference to \`}
 		echo "${LINE%\'*}"
+		;;
+	    *undefined\ symbol:*)
+		# ld: error: undefined symbol: foo()
+		echo "${LINE#*undefined symbol: }"
 		;;
 	    *)
 		continue
