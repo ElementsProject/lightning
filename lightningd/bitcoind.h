466c18bc8c511cc281f917022c9b01119ea2d2bd
darosior <darosior@protonmail.com>
2019-12-14 21:35:54 +0100

    lightningd/bitcoind: remove unused 'get_output' function
    Along with its callbacks.

--- lightningd/bitcoind.h
+++ lightningd/bitcoind.h
@@ -173,21 +173,6 @@ void bitcoind_getrawblock_(struct bitcoind *bitcoind,
 						  struct bitcoin_block *), \
 			      (arg))
 
-void bitcoind_getoutput_(struct bitcoind *bitcoind,
-			 unsigned int blocknum, unsigned int txnum,
-			 unsigned int outnum,
-			 void (*cb)(struct bitcoind *bitcoind,
-				    const struct bitcoin_tx_output *output,
-				    void *arg),
-			 void *arg);
-#define bitcoind_getoutput(bitcoind_, blocknum, txnum, outnum, cb, arg)	\
-	bitcoind_getoutput_((bitcoind_), (blocknum), (txnum), (outnum),	\
-			    typesafe_cb_preargs(void, void *,		\
-						(cb), (arg),		\
-						struct bitcoind *,	\
-						const struct bitcoin_tx_output*), \
-			    (arg))
-
 void bitcoind_gettxout(struct bitcoind *bitcoind,
 		       const struct bitcoin_txid *txid, const u32 outnum,
 		       void (*cb)(struct bitcoind *bitcoind,
