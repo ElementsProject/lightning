5a70470b6517313dc518927e59cab46115e4c64f
lisa neigut <niftynei@gmail.com>
2020-01-16 18:05:55 -0600

    df: add test for double spending a dual funding input

--- tests/test_opening.py
+++ tests/test_opening.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from fixtures import *  # noqa: F401,F403
 from flaky import flaky  # noqa: F401
 from lightning import RpcError
-from utils import EXPERIMENTAL_FEATURES, only_one, wait_for, sync_blockheight
+from utils import EXPERIMENTAL_FEATURES, only_one, sync_blockheight
 
 import os
 import pytest
@@ -50,26 +50,23 @@ def test_two_sided_open(node_factory, bitcoind):
 
 
 @unittest.skipIf(not EXPERIMENTAL_FEATURES, "dual funding is experimental")
-def test_burn_on_cancel(node_factory, bitcoind):
-    # If our peer cancels us, we should burn the tx
+def test_double_spends(node_factory, bitcoind):
+    # We re-use inputs if the tx hasn't been broadcast within a few hours/blocks
+    # In the case that this happens, we should gracefully shutdown the
+    # channels associated with the double-spent utxo
     plugin_path = os.path.join(os.getcwd(), 'tests/plugins/funder.py')
 
-    l1 = node_factory.get_node(may_reconnect=True)
-    l2 = node_factory.get_node(options={'plugin': plugin_path},
-                               may_reconnect=True)
+    l1 = node_factory.get_node()
+    l2 = node_factory.get_node(options={'plugin': plugin_path})
+    l3 = node_factory.get_node()
 
     l1.fundwallet(200000000)
     l2.fundwallet(200000000)
+    l3.fundwallet(200000000)
 
     amount = 200000
     l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
     funding_addr = l1.rpc.fundchannel_start(l2.info['id'], amount)['funding_address']
-
-    # Fund external wallet
-    addr = l1.rpc.newaddr()['bech32']
-    l1.bitcoin.rpc.sendtoaddress(addr, 0.1)
-    bitcoind.generate_block(1)
-    wait_for(lambda: len(l1.rpc.listfunds()['outputs']) == 1)
 
     prep = l1.rpc.txprepare([{funding_addr: amount}], zero_out_change=True)
     decode = bitcoind.rpc.decoderawtransaction(prep['unsigned_tx'])
@@ -89,19 +86,25 @@ def test_burn_on_cancel(node_factory, bitcoind):
     # Funds should be committed to this channel open
     chan = only_one(only_one(l2.rpc.listpeers()['peers'])['channels'])
     assert chan['msatoshi_to_us'] == amount * 1000
-    assert len(l2.rpc.listfunds()['outputs']) == 0
+    funds = l2.rpc.listfunds()
+    assert len(funds['outputs']) == 0
+    assert not only_one(funds['reserved_outputs'])['reservation_expired']
 
-    # Have l1 cancel
-    l1.rpc.fundchannel_cancel(l2.info['id'])
-    assert len(l1.rpc.listpeers()['peers']) == 0
+    # l1 doesn't broadcast, let's advance 18 blocks (UTXO_RESERVATION_BLOCKS)
+    l1.bitcoin.generate_block(18)
+    sync_blockheight(bitcoind, [l2])
 
-    l2.daemon.wait_for_log(r'1 utxo burned in tx')
-    only_one(l2.rpc.listfunds()['outputs'])
-    assert len(l2.rpc.listpeers()['peers']) == 0
+    funds = l2.rpc.listfunds()
+    assert len(funds['outputs']) == 0
+    assert only_one(funds['reserved_outputs'])['reservation_expired']
 
-    l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
-    assert len(only_one(l1.rpc.listpeers()['peers'])['channels']) == 0
-    assert len(only_one(l2.rpc.listpeers()['peers'])['channels']) == 0
+    # try to fundchannel from l3 <-> l2 now
+    l3.rpc.connect(l2.info['id'], 'localhost', l2.port)
+    l3.rpc.fundchannel(l2.info['id'], amount)
+
+    # since we haven't updated our lookup for available utxos yet,
+    # we get a warning!!
+    l2.daemon.wait_for_log(r'Attempting to fund channel for 20000sat when max was set to 0sat')
 
     # Clean up prepped tx, otherwise we leak on quit
     l1.rpc.txdiscard(txid)
