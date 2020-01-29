39835fe92ba8eac7854586dd5c0f335f28c09245
Christian Decker <decker.christian@gmail.com>
2020-01-29 16:03:01 +0100

    pyln-proto: Add invoice utilities
    We are about to disect a couple of invoices for features, so let's add a class
    that can encode and decode invoices from bolt11 strings. This is pretty much
    the lnaddr.py file created by @rustyrussell with some minor changes. I'm
    planning to clean this up further which is why I'm only exporting the
    `Invoice` class for now.

--- contrib/pyln-proto/pyln/proto/__init__.py
+++ contrib/pyln-proto/pyln/proto/__init__.py
@@ -1,1 +1,14 @@
+from .invoice import Invoice
+from .onion import OnionPayload, TlvPayload, LegacyOnionPayload
+from .wire import LightningConnection, LightningServerSocket
+
 __version__ = '0.0.1'
+
+__all__ = [
+    "Invoice",
+    "LightningServerSocket",
+    "LightningConnection",
+    "OnionPayload",
+    "LegacyOnionPayload",
+    "TlvPayload",
+]
