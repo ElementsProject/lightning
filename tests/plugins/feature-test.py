966ac9509856707c05b50c418e9ac22aefdfa356
Christian Decker <decker.christian@gmail.com>
2020-01-29 22:33:10 +0100

    pytest: Add a test for plugin featurebits

---
+++ tests/plugins/feature-test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from pyln.client import Plugin
+
+# Register a different set feature of feature bits for each location so we can
+# later check that they are being passed correctly.
+plugin = Plugin(
+    init_features=1 << 101,
+    node_features=1 << 103,
+    invoice_features=1 << 105,
+)
+
+
+plugin.run()
