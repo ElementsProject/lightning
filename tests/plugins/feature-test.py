10c4c0a84af038d98d13150cade7c4a430da995a
Christian Decker <decker.christian@gmail.com>
2020-02-03 13:04:16 +0100

    plugin: Allow custom features only if the plugin is not dynamic
    This is in order to avoid having to update featurebits as plugins get
    activated and deactivated.

--- tests/plugins/feature-test.py
+++ tests/plugins/feature-test.py
@@ -5,6 +5,7 @@ from pyln.client import Plugin
 # Register a different set feature of feature bits for each location so we can
 # later check that they are being passed correctly.
 plugin = Plugin(
+    dynamic=False,
     init_features=1 << 101,
     node_features=1 << 103,
     invoice_features=1 << 105,
