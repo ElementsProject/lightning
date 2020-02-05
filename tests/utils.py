fa2e6c47b135201f0e9a2de6e7cc89f9c5b1738f
lisa neigut <niftynei@gmail.com>
2020-02-05 14:10:06 -0600

    fixup tests: update version to include dual-funding number
    dual funding is currently 19

--- tests/utils.py
+++ tests/utils.py
@@ -4,17 +4,26 @@ from pyln.testing.utils import env, only_one, wait_for, write_config, TailablePr
 
 EXPERIMENTAL_FEATURES = env("EXPERIMENTAL_FEATURES", "0") == "1"
 COMPAT = env("COMPAT", "1") == "1"
+# features 1, 3, 7, 9, 11, 13, 15 and 17 (0x02aaa2).
+BASE_FEATURES = "02aaa2"
+# dual funding is currently 19; will update in the future
+# features 1, 3, 7, 9, 11, 13, 15, 17, 19 (0x0aaaa2).
+BASE_PLUS_DF = "0aaaa2"
 
 
 def expected_peer_features():
     """Return the expected peer features hexstring for this configuration"""
-    # features 1, 3, 7, 9, 11, 13, 15 and 17 (0x02aaa2).
-    return "02aaa2"
+    if not EXPERIMENTAL_FEATURES:
+        return BASE_FEATURES
+
+    return BASE_PLUS_DF
 
 
 # With the addition of the keysend plugin, we now send a different set of
 # features for the 'node' and the 'peer' feature sets
 def expected_node_features():
     """Return the expected node features hexstring for this configuration"""
-    # features 1, 3, 7, 9, 11, 13, 15, 17 and 55 (0x8000000002aaa2).
-    return "8000000002aaa2"
+    if not EXPERIMENTAL_FEATURES:
+        return "80000000" + BASE_FEATURES
+
+    return "80000000" + BASE_PLUS_DF
