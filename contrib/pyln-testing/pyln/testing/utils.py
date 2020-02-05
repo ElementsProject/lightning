48c4d5886a971771b5661e49d726e7db5d68a1fb
Ken Sedgwick <ken@bonsai.com>
2020-02-05 12:56:41 -0800

    Rebased cpp-hsmd-3 onto master after prior PRs merged.

--- contrib/pyln-testing/pyln/testing/utils.py
+++ contrib/pyln-testing/pyln/testing/utils.py
@@ -71,7 +71,7 @@ TEST_DEBUG = env("TEST_DEBUG", "0") == "1"
 SLOW_MACHINE = env("SLOW_MACHINE", "0") == "1"
 DEPRECATED_APIS = env("DEPRECATED_APIS", "0") == "1"
 TIMEOUT = int(env("TIMEOUT", 180 if SLOW_MACHINE else 60))
-ALT_SUBDAEMON = env("ALT_SUBDAEMON", "")
+SUBDAEMON = env("SUBDAEMON", "")
 
 def wait_for(success, timeout=TIMEOUT):
     start_time = time.time()
@@ -494,8 +494,8 @@ class LightningD(TailableProc):
             'bitcoin-datadir': lightning_dir,
         }
 
-        if ALT_SUBDAEMON:
-            opts['alt-subdaemon'] = ALT_SUBDAEMON
+        if SUBDAEMON:
+            opts['subdaemon'] = SUBDAEMON
 
         for k, v in opts.items():
             self.opts[k] = v
--- contrib/remote_hsmd/NOTES.md
+++ contrib/remote_hsmd/NOTES.md
@@ -1,5 +1,25 @@
 
 
-    ALT_SUBDAEMON='lightning_hsmd:remote_hsmd' \
-    make VALGRIND=0 pytest \
-    |& tee log
+Run all of the integration tests:
+```
+SUBDAEMON='hsmd:remote_hsmd' \
+make \
+DEVELOPER=1 \
+VALGRIND=0 \
+pytest \
+|& tee log
+```
+
+Run a single integration test:
+```
+PYTHONPATH=`pwd`/hsmd:`pwd`/contrib/pylightning:`pwd`/contrib/pyln-testing:`pwd`/contrib/pyln-client:$PYTHONPATH \
+TEST_DEBUG=1 \
+DEVELOPER=1 \
+VALGRIND=0 \
+SLOW_MACHINE=1 \
+SUBDAEMON='hsmd:remote_hsmd' \
+pytest \
+tests/test_gossip.py::test_gossip_notices_close \
+-v --timeout=550 --timeout_method=thread -x -s \
+|& tee log
+```
