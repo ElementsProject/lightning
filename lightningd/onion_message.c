676bcc8bf3226fbb35d5b0d365372b89faba48a3
Rusty Russell <rusty@rustcorp.com.au>
2020-03-06 16:31:24 +1030

    plugins: expose onion_message hook (EXPERIMENTAL_FEATURES)
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/onion_message.c
+++ lightningd/onion_message.c
@@ -1,11 +1,62 @@
 #include <channeld/gen_channel_wire.h>
+#include <common/json_helpers.h>
 #include <lightningd/channel.h>
 #include <lightningd/onion_message.h>
 #include <lightningd/lightningd.h>
 #include <lightningd/peer_control.h>
+#include <lightningd/plugin_hook.h>
 #include <lightningd/subd.h>
 
 #if EXPERIMENTAL_FEATURES
+struct onion_message_hook_payload {
+	/* plaintext is NULL if we couldn't decrypt. */
+	const u8 *plaintext;
+	struct secret ss;
+	u8 *e2e_payload;
+	/* These are *optional* */
+	struct short_channel_id *next_scid;
+	struct node_id *next_node;
+	u8 *reply_onion;
+};
+
+static void
+onion_message_serialize(struct onion_message_hook_payload *payload,
+			   struct json_stream *stream)
+{
+	json_object_start(stream, "onion_message");
+	if (payload->plaintext) {
+		json_add_hex_talarr(stream, "plaintext", payload->plaintext);
+	} else {
+		json_add_hex_talarr(stream, "payload", payload->e2e_payload);
+	}
+	json_add_secret(stream, "shared_secret", &payload->ss);
+	if (payload->next_scid)
+		json_add_short_channel_id(stream, "next_short_channel_id",
+					  payload->next_scid);
+	if (payload->next_node)
+		json_add_node_id(stream, "next_node_id", payload->next_node);
+	if (payload->reply_onion)
+		json_add_hex_talarr(stream, "reply_onion", payload->reply_onion);
+	json_object_end(stream);
+}
+
+static void
+onion_message_hook_cb(struct onion_message_hook_payload *payload,
+			 const char *buffer,
+			 const jsmntok_t *toks)
+{
+	/* The core infra checks the "result"; anything other than continue
+	 * just stops. */
+	tal_free(payload);
+}
+
+REGISTER_PLUGIN_HOOK(onion_message,
+		     PLUGIN_HOOK_CHAIN,
+		     onion_message_hook_cb,
+		     struct onion_message_hook_payload *,
+		     onion_message_serialize,
+		     struct onion_message_hook_payload *);
+
 /* Returns false if we can't tell it */
 static bool make_peer_send(struct lightningd *ld,
 			   struct channel *dst, const u8 *msg TAKES)
@@ -39,30 +90,34 @@ static bool make_peer_send(struct lightningd *ld,
 
 void handle_onionmsg_to_us(struct channel *channel, const u8 *msg)
 {
-	u8 *e2e_payload, *plaintext;
-	struct short_channel_id *next_scid;
-	struct node_id *next_node;
-	u8 *next_onion;
-	struct secret ss;
+	struct lightningd *ld = channel->peer->ld;
+	struct onion_message_hook_payload *payload;
 
-	if (!fromwire_got_onionmsg_to_us(msg, msg, &next_scid, &next_node,
-					 &next_onion, &ss, &e2e_payload)) {
+	payload = tal(ld, struct onion_message_hook_payload);
+	if (!fromwire_got_onionmsg_to_us(payload, msg,
+					 &payload->next_scid,
+					 &payload->next_node,
+					 &payload->reply_onion,
+					 &payload->ss,
+					 &payload->e2e_payload)) {
 		channel_internal_error(channel, "bad got_onionmsg_tous: %s",
 				       tal_hex(tmpctx, msg));
 		return;
 	}
 
-	/* FIXME: Wire up onion message handling! */
-	plaintext = final_e2e_payload(msg, e2e_payload, &ss);
-	if (!plaintext) {
+	payload->plaintext = final_e2e_payload(payload,
+					       payload->e2e_payload,
+					       &payload->ss);
+	if (!payload->plaintext) {
 		log_info(channel->log, "Received invalid onion message %s%s",
-			 tal_hex(tmpctx, e2e_payload),
-			 next_onion ? " (with next_onion)": "");
+			 tal_hex(tmpctx, payload->e2e_payload),
+			 payload->reply_onion ? " (with reply_onion)": "");
 	} else {
 		log_info(channel->log, "Received onion message %s%s",
-			 tal_hex(tmpctx, plaintext),
-			 next_onion ? " (with next_onion)": "");
+			 tal_hex(tmpctx, payload->plaintext),
+			 payload->reply_onion ? " (with reply_onion)": "");
 	}
+	plugin_hook_call_onion_message(ld, payload, payload);
 }
 
 void handle_onionmsg_forward(struct channel *channel, const u8 *msg)
