251bae55d7ca9f11a9f6127d9a0b96fbeb01cac1
lisa neigut <niftynei@gmail.com>
2020-04-17 16:24:47 -0500

    build: Update build-release.sh to remove i386 build
    fixup! docker: Remove Dockerfile for i386 builder

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -14,7 +14,7 @@ if [ x"$1" = x"--inside-docker" ]; then
 fi
 
 # bin-Ubuntu-16.04-amd64 was superceded by the reproducible built 18.04 version.
-ALL_TARGETS="bin-Fedora-28-amd64 bin-Ubuntu-16.04-i386 zipfile"
+ALL_TARGETS="bin-Fedora-28-amd64 zipfile"
 
 FORCE_VERSION=
 FORCE_UNCLEAN=false
@@ -88,10 +88,6 @@ for target in $TARGETS; do
 	Ubuntu-16.04-amd64)
 	    DOCKERFILE=contrib/Dockerfile.builder
 	    TAG=ubuntu-amd64
-	    ;;
-	Ubuntu-16.04-i386)
-	    DOCKERFILE=contrib/Dockerfile.builder.i386
-	    TAG=ubuntu-i386
 	    ;;
 	*)
 	    echo "No Dockerfile for $platform" >&2
