904a69d10836b75125d9f092268acf60b2a0b9f4
Christian Decker <decker.christian@gmail.com>
2020-04-30 18:40:49 +0200

    pyln-proto: Avoid circular dependency in setup.py
    It seems that loading the version from the source files triggers imports that
    may not yet have been installed.

--- contrib/pyln-proto/setup.py
+++ contrib/pyln-proto/setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from pyln import proto
 import io
 
 
@@ -10,7 +9,7 @@ with io.open('requirements.txt', encoding='utf-8') as f:
     requirements = [r for r in f.read().split('\n') if len(r)]
 
 setup(name='pyln-proto',
-      version=proto.__version__,
+      version='0.0.2',
       description='Pure python implementation of the Lightning Network protocol',
       long_description=long_description,
       long_description_content_type='text/markdown',
