1b6db460fc8dbdeb5d758a20f6177ffae3e7323d
Rusty Russell <rusty@rustcorp.com.au>
2020-06-24 20:51:36 +0930

    pyln.spec.bolt*: unify setups.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ contrib/pyln-spec/boltsetup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import io
+
+with io.open('requirements.txt', encoding='utf-8') as f:
+    requirements = [r for r in f.read().split('\n') if len(r)]
+
+
+def bolt_setup(boltnum: int, version: str, desc: str):
+    setup(name='pyln-bolt{}'.format(boltnum),
+          version=version,
+          description=desc,
+          url='http://github.com/ElementsProject/lightning',
+          author='Rusty Russell',
+          author_email='rusty@rustcorp.com.au',
+          license='MIT',
+          packages=['pyln.spec.bolt{}'.format(boltnum)],
+          package_data={'pyln.proto.message': ['py.typed']},
+          scripts=[],
+          zip_safe=True,
+          install_requires=requirements)
