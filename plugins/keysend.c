f6ce0391b4e0c376c7c1890a1a9b3f69fb48deb6
Vasil Dimov <vd@FreeBSD.org>
2020-06-25 16:14:55 +0200

    build: fix compilation on i386
    ```
    plugins/keysend.c:136:47: error: format specifies type 'unsigned long' but the argument has type 'time_t' (aka 'int') [-Werror,-Wformat]
            ki->label = tal_fmt(ki, "keysend-%lu.%09lu", now.ts.tv_sec, now.ts.tv_nsec);
                                             ~~~         ^~~~~~~~~~~~~
                                             %d
    ```
    
    Changelog-None

--- plugins/keysend.c
+++ plugins/keysend.c
@@ -134,7 +134,7 @@ static struct command_result *htlc_accepted_call(struct command *cmd,
 
 	ki = tal(cmd, struct keysend_in);
 	memcpy(&ki->payment_preimage, preimage_field->value, 32);
-	ki->label = tal_fmt(ki, "keysend-%lu.%09lu", now.ts.tv_sec, now.ts.tv_nsec);
+	ki->label = tal_fmt(ki, "keysend-%lu.%09lu", (unsigned long)now.ts.tv_sec, now.ts.tv_nsec);
 	ki->payload = tal_steal(ki, payload);
 	ki->preimage_field = preimage_field;
 
