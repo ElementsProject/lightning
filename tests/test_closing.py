0afdc4d9412079c69058dd232e3195b31565dbf7
Rusty Russell <rusty@rustcorp.com.au>
2020-06-30 20:22:08 +0930

    tests: fix flake in test_penalty_htlc_tx_fulfill
    If the daemon already knows about the channel before it was stopped,
    it won't get this message from gossipd.  That's OK, since we explicitly
    test for the channel being active two lines down.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/test_closing.py
+++ tests/test_closing.py
@@ -782,7 +782,6 @@ def test_penalty_htlc_tx_fulfill(node_factory, bitcoind):
 
     # push some money from l3->l2, so that the commit counter advances
     l2.rpc.connect(l3.info['id'], 'localhost', l3.port)
-    l2.daemon.wait_for_log('now ACTIVE')
     inv = l3.rpc.invoice(10**4, '1', 'push')
     # Make sure gossipd in l2 knows it's active
     wait_for(lambda: [c['active'] for c in l2.rpc.listchannels(l2.get_channel_scid(l3))['channels']] == [True, True])
