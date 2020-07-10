5818040e36a583a4397e2699642f0168791f856e
niftynei <niftynei@gmail.com>
2020-07-10 16:12:03 -0500

    elements: update output amounts for elements test
    The amount we expect back, post change, is different for an elements tx

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -710,8 +710,13 @@ def test_sign_and_send_psbt(node_factory, bitcoind, chainparams):
         # do proper acccounting for it.
         {'type': 'chain_mvt', 'credit': 0, 'debit': 4000000000, 'tag': 'withdrawal'},
         {'type': 'chain_mvt', 'credit': 0, 'debit': 0, 'tag': 'chain_fees'},
-        {'type': 'chain_mvt', 'credit': 988285000, 'debit': 0, 'tag': 'deposit'},
     ]
+
+    if chainparams['elements']:
+        wallet_coin_mvts.append({'type': 'chain_mvt', 'credit': 984625000, 'debit': 0, 'tag': 'deposit'})
+    else:
+        wallet_coin_mvts.append({'type': 'chain_mvt', 'credit': 988285000, 'debit': 0, 'tag': 'deposit'})
+
     check_coin_moves(l1, 'wallet', wallet_coin_mvts, chainparams)
 
 
