4f9fd4661624de7c701c2aeb7bca22e25b674f85
Rusty Russell <rusty@rustcorp.com.au>
2020-07-15 15:00:49 +0930

    wallet: add wallet_find_utxo().
    This is a new fundamental routine to obtain UTXOs from the database.
    
    It's not the most efficient approach, as it returns a single UTXO at a
    time, but it can consolidate all our UTXO handling (becoming more
    complex by the reservation timeout logic).
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wallet/wallet.h
+++ wallet/wallet.h
@@ -375,6 +375,29 @@ struct utxo **wallet_get_unconfirmed_closeinfo_utxos(const tal_t *ctx,
 						     struct wallet *w);
 
 /**
+ * wallet_find_utxo - Select an available UTXO (does not reserve it!).
+ * @ctx: tal context
+ * @w: wallet
+ * @current_blockheight: current chain length.
+ * @amount_we_are_short: optional amount.
+ * @feerate_per_kw: feerate we are using.
+ * @maxheight: zero (if caller doesn't care) or maximum blockheight to accept.
+ * @excludes: UTXOs not to consider.
+ *
+ * If @amount_we_are_short is not NULL, we try to get something very close
+ * (i.e. when we add this input, we will add => @amount_we_are_short, but
+ * less than @amount_we_are_short + dustlimit).
+ *
+ * Otherwise we give a random UTXO.
+ */
+struct utxo *wallet_find_utxo(const tal_t *ctx, struct wallet *w,
+			      unsigned current_blockheight,
+			      struct amount_sat *amount_we_are_short,
+			      unsigned feerate_per_kw,
+			      u32 maxheight,
+			      const struct utxo **excludes);
+
+/**
  * wallet_add_onchaind_utxo - Add a UTXO with spending info from onchaind.
  *
  * Usually we add UTXOs by looking at transactions, but onchaind tells
