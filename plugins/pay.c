5107727fa98afdc2a9e244f09ed8dd53b4c53bad
Christian Decker <decker.christian@gmail.com>
2020-07-18 10:10:51 +0200

    plugin: Fix misspelled COMPAT_V090 compile guards

--- plugins/pay.c
+++ plugins/pay.c
@@ -1271,7 +1271,7 @@ static struct pay_status *add_pay_status(struct pay_command *pc,
 	return ps;
 }
 
-#ifndef COMPAT_090
+#ifndef COMPAT_V090
 UNUSED
 #endif
 static struct command_result *json_pay(struct command *cmd,
@@ -1842,10 +1842,10 @@ static void init(struct plugin *p,
 
 struct payment_modifier *paymod_mods[] = {
 	&local_channel_hints_pay_mod,
-	&directpay_pay_mod,
-	&shadowroute_pay_mod,
 	&exemptfee_pay_mod,
+	&directpay_pay_mod,
 	&presplit_pay_mod,
+	&shadowroute_pay_mod,
 	&routehints_pay_mod,
 	&waitblockheight_pay_mod,
 	&retry_pay_mod,
@@ -1973,7 +1973,7 @@ static struct command_result *json_paymod(struct command *cmd,
 }
 
 static const struct plugin_command commands[] = {
-#ifdef COMPAT_v090
+#ifdef COMPAT_V090
 	{
 		"legacypay",
 		"payment",
