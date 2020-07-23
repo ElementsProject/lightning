aedc8ec5cc549eddac1544de0bf780191fea00a4
niftynei <niftynei@gmail.com>
2020-07-22 21:11:26 -0500

    channel_id: add v2 channel_id derivtion helper
    v2 of channel open uses the channel revocation basepoints to calculate
    the channel_id, instead of the funding_txid + outnum
    
    Moving away from the funding_txid opens the way for splicing + rbf

--- common/channel_id.c
+++ common/channel_id.c
@@ -1,3 +1,4 @@
+#include <bitcoin/pubkey.h>
 #include <bitcoin/tx.h>
 #include <common/channel_id.h>
 #include <common/type_to_string.h>
@@ -10,6 +11,29 @@ void derive_channel_id(struct channel_id *channel_id,
 	memcpy(channel_id, txid, sizeof(*channel_id));
 	channel_id->id[sizeof(*channel_id)-2] ^= txout >> 8;
 	channel_id->id[sizeof(*channel_id)-1] ^= txout;
+}
+
+void derive_channel_id_v2(struct channel_id *channel_id,
+			  const struct pubkey *basepoint_1,
+			  const struct pubkey *basepoint_2)
+{
+	u8 der_keys[PUBKEY_CMPR_LEN * 2];
+	struct sha256 sha;
+	int offset_1, offset_2;
+
+	/* basepoint_1 is first? */
+	if (pubkey_idx(basepoint_1, basepoint_2) == 0) {
+		offset_1 = 0;
+		offset_2 = PUBKEY_CMPR_LEN;
+	} else {
+		offset_1 = PUBKEY_CMPR_LEN;
+		offset_2 = 0;
+	}
+	pubkey_to_der(der_keys + offset_1, basepoint_1);
+	pubkey_to_der(der_keys + offset_2, basepoint_2);
+	sha256(&sha, der_keys, sizeof(der_keys));
+	BUILD_ASSERT(sizeof(*channel_id) == sizeof(sha));
+	memcpy(channel_id, &sha, sizeof(*channel_id));
 }
 
 void towire_channel_id(u8 **pptr, const struct channel_id *channel_id)
