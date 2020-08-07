cf25ca11b860cbfe0c375218d9ef67af8339213e
Rusty Russell <rusty@rustcorp.com.au>
2020-08-07 11:00:47 +0930

    sendpsbt: annotate transactions in wallet.
    This is what txsend does, only we have a psbt so we have
    to change the db interface to take a wally_tx.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wallet/db.h
+++ wallet/db.h
@@ -118,7 +118,7 @@ void db_bind_short_channel_id_arr(struct db_stmt *stmt, int col,
 void db_bind_signature(struct db_stmt *stmt, int col,
 		       const secp256k1_ecdsa_signature *sig);
 void db_bind_timeabs(struct db_stmt *stmt, int col, struct timeabs t);
-void db_bind_tx(struct db_stmt *stmt, int col, const struct bitcoin_tx *tx);
+void db_bind_tx(struct db_stmt *stmt, int col, const struct wally_tx *tx);
 void db_bind_psbt(struct db_stmt *stmt, int col, const struct wally_psbt *psbt);
 void db_bind_amount_msat(struct db_stmt *stmt, int pos,
 			 const struct amount_msat *msat);
