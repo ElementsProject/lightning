a55a400c38af4f871dc8fd1894941d34e8da60e8
ZmnSCPxj jxPCSnmZ <zmnscpxj@protonmail.com>
2020-08-21 17:31:33 +0800

    plugins/spender/multifundchannel.c: Move multifundchannel to the spenderp plugin.

---
+++ plugins/spender/multifundchannel.h
@@ -0,0 +1,10 @@
+#ifndef LIGHTNING_PLUGINS_SPENDER_MULTIFUNDCHANNEL_H
+#define LIGHTNING_PLUGINS_SPENDER_MULTIFUNDCHANNEL_H
+#include "config.h"
+
+#include <plugins/libplugin.h>
+
+extern const struct plugin_command multifundchannel_commands[];
+extern const size_t num_multifundchannel_commands;
+
+#endif /* LIGHTNING_PLUGINS_SPENDER_MULTIFUNDCHANNEL_H */
