e006d19dbcde4575c346b95fc35fd0d3afe023bb
Michael Schmoock <michael@schmoock.net>
2020-08-21 12:38:51 +0200

    openchannel: test new hook chainable mechanics

---
+++ tests/plugins/openchannel_hook_accept.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Plugin to test openchannel_hook
+
+Will simply accept any channel. Useful fot testing chained hook.
+"""
+
+from pyln.client import Plugin
+
+plugin = Plugin()
+
+
+@plugin.hook('openchannel')
+def on_openchannel(openchannel, plugin, **kwargs):
+    msg = "accept on principle"
+    plugin.log(msg)
+    return {'result': 'continue'}
+
+
+plugin.run()
