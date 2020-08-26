6815a1d9a285e7f6299dec6b7bb07af5396703ff
Rusty Russell <rusty@rustcorp.com.au>
2020-08-26 10:10:48 +0930

    common/route: routing helpers built on gossmap and dijksra.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ common/route.h
@@ -0,0 +1,49 @@
+/* Routing helpers for use with dijkstra */
+#ifndef LIGHTNING_COMMON_ROUTE_H
+#define LIGHTNING_COMMON_ROUTE_H
+#include "config.h"
+#include <common/amount.h>
+
+struct dijkstra;
+struct gossmap;
+
+struct route {
+	int dir;
+	struct gossmap_chan *c;
+};
+
+/* Can c carrt amount in dir? */
+bool route_can_carry(const struct gossmap *map,
+		     const struct gossmap_chan *c,
+		     int dir,
+		     struct amount_msat amount,
+		     void *arg);
+
+/* Same, but ignore disabled flags on channel */
+bool route_can_carry_even_disabled(const struct gossmap *map,
+				   const struct gossmap_chan *c,
+				   int dir,
+				   struct amount_msat amount,
+				   void *unused);
+
+/* Shortest path, with lower amount tiebreak */
+bool route_path_shorter(u32 old_distance, u32 new_distance,
+			struct amount_msat old_cost,
+			struct amount_msat new_cost,
+			struct amount_msat old_risk,
+			struct amount_msat new_risk,
+			void *unused);
+
+/* Cheapest path, with shorter path tiebreak */
+bool route_path_cheaper(u32 old_distance, u32 new_distance,
+			struct amount_msat old_cost,
+			struct amount_msat new_cost,
+			struct amount_msat old_risk,
+			struct amount_msat new_risk,
+			void *unused);
+
+/* Extract route tal_arr from completed dijkstra */
+struct route **route_from_dijkstra(const struct gossmap *map,
+				   const struct dijkstra *dij,
+				   const struct gossmap_node *cur);
+#endif /* LIGHTNING_COMMON_ROUTE_H */
