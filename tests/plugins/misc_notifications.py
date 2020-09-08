ddc759c49eec8e7de0bd7065bbb4fd1b91e585a4
Michael Schmoock <michael@schmoock.net>
2020-09-08 18:16:04 +0200

    test: plugin channel_state_changed notification

--- tests/plugins/misc_notifications.py
+++ tests/plugins/misc_notifications.py
@@ -15,11 +15,16 @@ def init(plugin, options, configuration):
 
 
 @plugin.subscribe("channel_opened")
-def channel_opened(plugin, channel_opened):
+def channel_opened(plugin, channel_opened, **kwargs):
     plugin.log("A channel was opened to us by {}, with an amount"
                " of {} and the following funding transaction id: {}"
                .format(channel_opened["id"], channel_opened["amount"],
                        channel_opened["funding_txid"]))
+
+
+@plugin.subscribe("channel_state_changed")
+def channel_state_changed(plugin, channel_state_changed, **kwargs):
+    plugin.log("channel_state_changed {}".format(channel_state_changed))
 
 
 plugin.run()
