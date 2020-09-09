c9c9d96949054b94c1915bedff503a1195fa87bc
niftynei <niftynei@gmail.com>
2020-09-09 19:40:28 +0930

    peer-control: send set of messages to start, not just one
    The accepter has to send 2 messages over to channeld to send at start --
    their commitment_signatures and tx_signatures

--- lightningd/channel_control.c
+++ lightningd/channel_control.c
@@ -415,7 +415,7 @@ static unsigned channel_msg(struct subd *sd, const u8 *msg, const int *fds)
 
 void peer_start_channeld(struct channel *channel,
 			 struct per_peer_state *pps,
-			 const u8 *funding_signed,
+			 const u8 **fwd_msgs,
 			 bool reconnected)
 {
 	u8 *initmsg;
@@ -556,7 +556,8 @@ void peer_start_channeld(struct channel *channel,
 				      channel->shutdown_scriptpubkey[REMOTE] != NULL,
 				      channel->shutdown_scriptpubkey[LOCAL],
 				      channel->channel_flags,
-				      funding_signed,
+				      /* FIXME: pass set of msgs */
+				      fwd_msgs ? fwd_msgs[0] : NULL,
 				      reached_announce_depth,
 				      &last_remote_per_commit_secret,
 				      channel->peer->their_features,
