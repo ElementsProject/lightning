8907062c9fec8784dd1f0602d9027f11d90d9d62
Christian Decker <decker.christian@gmail.com>
2020-09-10 09:29:51 +0200

    pyln: Bump proto version to match pyln-bolt*
    Since pyln-bolt* specify the 0.8.4 version which we didn't upload, and the
    requirements.txt specify ==0.8.4, we need to backfill that version, even if we
    could just bump it directly to 0.9.1.

--- contrib/pyln-proto/pyln/proto/__init__.py
+++ contrib/pyln-proto/pyln/proto/__init__.py
@@ -3,7 +3,7 @@ from .invoice import Invoice
 from .onion import OnionPayload, TlvPayload, LegacyOnionPayload
 from .wire import LightningConnection, LightningServerSocket
 
-__version__ = '0.8.3'
+__version__ = '0.8.4'
 
 __all__ = [
     "Invoice",
