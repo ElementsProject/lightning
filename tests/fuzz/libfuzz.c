22e34311de3ae2676ad35e19b6d076834467736f
Antoine Poinsot <darosior@protonmail.com>
2020-09-23 00:22:16 +0200

    fuzz: add a fuzz target for common/bigsize
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- tests/fuzz/libfuzz.c
+++ tests/fuzz/libfuzz.c
@@ -1,5 +1,9 @@
 #include <tests/fuzz/libfuzz.h>
 
+#include <ccan/tal/tal.h>
+#include <common/utils.h>
+#include <string.h>
+
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 int LLVMFuzzerInitialize(int *argc, char ***argv);
 
@@ -13,4 +17,17 @@ int LLVMFuzzerInitialize(int *argc, char ***argv) {
 	init(argc, argv);
 
 	return 0;
+}
+
+const uint8_t **get_chunks(const void *ctx, const uint8_t *data,
+			  size_t data_size, size_t chunk_size)
+{
+	size_t n_chunks = data_size / chunk_size;
+	const uint8_t **chunks = tal_arr(ctx, const uint8_t *, n_chunks);
+
+	for (size_t i = 0; i < n_chunks; i++)
+		chunks[i] = tal_dup_arr(chunks, const uint8_t,
+					data + i * chunk_size, chunk_size, 0);
+
+	return chunks;
 }
