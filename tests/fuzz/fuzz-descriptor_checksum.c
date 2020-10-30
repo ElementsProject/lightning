4f2ae48c5401f289c7defca7a16df67c13b9c679
Antoine Poinsot <darosior@protonmail.com>
2020-10-30 15:29:07 +0100

    fuzz: add a fuzz target for common/descriptor_checksum
    A small one just to check that we don't crash nor go out of bounds!
    
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

---
+++ tests/fuzz/fuzz-descriptor_checksum.c
@@ -0,0 +1,20 @@
+#include <tests/fuzz/libfuzz.h>
+
+#include <ccan/tal/tal.h>
+#include <common/descriptor_checksum.h>
+
+void init(int *argc, char ***argv)
+{
+}
+
+void run(const uint8_t *data, size_t size)
+{
+	char *string;
+	struct descriptor_checksum checksum;
+
+	/* We should not crash nor overflow the checksum buffer. */
+
+	string = to_string(NULL, data, size);
+	descriptor_checksum(string, tal_count(string), &checksum);
+	tal_free(string);
+}
