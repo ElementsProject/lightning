7bfb5f10c7e874437bc5cce7832355cb64c2ec68
Michael Schmoock <michael@schmoock.net>
2020-11-30 17:33:48 +0100

    pyln: failing test msat from float str
    We were not able to create pyln Millisatoshi from floats, e.g.:
     - "0.01btc"
     - "0.1sat"
     - ...
    
    This adds a test that makes sure this won't happen again.

--- contrib/pyln-client/tests/test_units.py
+++ contrib/pyln-client/tests/test_units.py
@@ -1,4 +1,5 @@
 from pyln.client import Millisatoshi
+import pytest  # type: ignore
 
 
 def test_to_approx_str():
@@ -34,3 +35,29 @@ def test_to_approx_str():
     assert amount.to_approx_str() == "12btc"
     amount = Millisatoshi('1200000000sat')
     assert amount.to_approx_str(1) == "12btc"  # note: no rounding
+
+
+def test_floats():
+    # test parsing amounts from floating number strings
+    amount = Millisatoshi("0.01btc")
+    assert amount.to_satoshi() == 10**6
+    amount = Millisatoshi("1.01btc")
+    assert amount.to_satoshi() == 10**8 + 10**6
+    amount = Millisatoshi("0.1sat")
+    assert int(amount) == 100
+    amount = Millisatoshi("0.01sat")
+    assert int(amount) == 10
+    amount = Millisatoshi("1.1sat")
+    assert int(amount) == 1100
+
+    # test floating point arithmetic
+    amount = Millisatoshi("1000msat") * 0.1
+    assert int(amount) == 100
+
+    # sub millisatoshi are not a concept yet
+    with pytest.raises(ValueError, match='Millisatoshi must be a whole number'):
+        amount = Millisatoshi("0.000000000001btc")
+    with pytest.raises(ValueError, match='Millisatoshi must be a whole number'):
+        amount = Millisatoshi("0.0001sat")
+    with pytest.raises(ValueError, match='Millisatoshi must be a whole number'):
+        amount = Millisatoshi("0.1msat")
