47d9ff88df3d58c7258ad2daa7aab0ec331ea6ee
niftynei <niftynei@gmail.com>
2020-12-09 13:06:30 -0600

    bcli: only print error if exitcode is non-zero

--- plugins/bcli.c
+++ plugins/bcli.c
@@ -601,8 +601,10 @@ static struct command_result *process_sendrawtransaction(struct bitcoin_cli *bcl
 	response = jsonrpc_stream_success(bcli->cmd);
 	json_add_bool(response, "success", *bcli->exitstatus == 0);
 	json_add_string(response, "errmsg",
-			bcli->exitstatus ? tal_strndup(bcli->cmd, bcli->output,
-						       bcli->output_bytes-1) : "");
+			*bcli->exitstatus ?
+			tal_strndup(bcli->cmd,
+				    bcli->output, bcli->output_bytes-1)
+			: "");
 
 	return command_finished(bcli->cmd, response);
 }
