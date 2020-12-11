b9fea7972f34eca9dc59fa7b69f513eac600939e
Christian Decker <decker.christian@gmail.com>
2020-12-11 19:21:23 +0100

    tests: Update mocks after breaking them via interleaved merges

--- common/test/run-sphinx-xor_cipher_stream.c
+++ common/test/run-sphinx-xor_cipher_stream.c
@@ -74,6 +74,9 @@ u64 fromwire_u64(const u8 **cursor UNNEEDED, size_t *max UNNEEDED)
 /* Generated stub for fromwire_u8 */
 u8 fromwire_u8(const u8 **cursor UNNEEDED, size_t *max UNNEEDED)
 { fprintf(stderr, "fromwire_u8 called!\n"); abort(); }
+/* Generated stub for fromwire_u8_array */
+void fromwire_u8_array(const u8 **cursor UNNEEDED, size_t *max UNNEEDED, u8 *arr UNNEEDED, size_t num UNNEEDED)
+{ fprintf(stderr, "fromwire_u8_array called!\n"); abort(); }
 /* Generated stub for hmac_done */
 void hmac_done(crypto_auth_hmacsha256_state *state UNNEEDED,
 	       struct hmac *hmac UNNEEDED)
