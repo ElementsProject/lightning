32d66b2740deed55d8bc459da81b6dacac986b39
Rusty Russell <rusty@rustcorp.com.au>
2020-12-14 11:51:19 +1030

    bolt11: expose routine to parse bolt11 string except signature.
    This means we can hand around unsigned bolt11s.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/bolt11.h
+++ common/bolt11.h
@@ -94,6 +94,16 @@ struct bolt11 *bolt11_decode(const tal_t *ctx, const char *str,
 			     const struct chainparams *must_be_chain,
 			     char **fail);
 
+/* Extracts signature but does not check it. */
+struct bolt11 *bolt11_decode_nosig(const tal_t *ctx, const char *str,
+				   const struct feature_set *our_features,
+				   const char *description,
+				   const struct chainparams *must_be_chain,
+				   struct sha256 *hash,
+				   u5 **sig,
+				   bool *have_n,
+				   char **fail);
+
 /* Initialize an empty bolt11 struct with optional amount */
 struct bolt11 *new_bolt11(const tal_t *ctx,
 			  const struct amount_msat *msat TAKES);
