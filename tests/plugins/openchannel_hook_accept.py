803d9701d3a6a27cc57594fe0dcf81e2444f2028
niftynei <niftynei@gmail.com>
2020-12-14 17:33:31 -0600

    df-tests: test_openchannel_hook_chaining

--- tests/plugins/openchannel_hook_accept.py
+++ tests/plugins/openchannel_hook_accept.py
@@ -5,8 +5,12 @@ Will simply accept any channel. Useful fot testing chained hook.
 """
 
 from pyln.client import Plugin
+from pyln.testing.utils import env
 
 plugin = Plugin()
+
+
+EXPERIMENTAL_FEATURES = env("EXPERIMENTAL_FEATURES", "0") == "1"
 
 
 @plugin.hook('openchannel')
@@ -14,6 +18,14 @@ def on_openchannel(openchannel, plugin, **kwargs):
     msg = "accept on principle"
     plugin.log(msg)
     return {'result': 'continue'}
+
+
+if EXPERIMENTAL_FEATURES:
+    @plugin.hook('openchannel2')
+    def on_openchannel2(openchannel2, plugin, **kwargs):
+        msg = "accept on principle"
+        plugin.log(msg)
+        return {'result': 'continue'}
 
 
 plugin.run()
