4d9ce939eb02f2ae0d055975455c1a78ad84f2a8
niftynei <niftynei@gmail.com>
2020-12-14 18:47:30 -0600

    df-tests: test_channel_state_changed_unilateral

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -820,10 +820,23 @@ def test_channel_state_changed_unilateral(node_factory, bitcoind):
         return event
 
     event2 = wait_for_event(l2)
+    if l2.config('experimental-dual-fund'):
+        assert(event2['peer_id'] == l1_id)
+        assert(event2['channel_id'] == cid)
+        assert(event2['short_channel_id'] is None)
+        assert(event2['old_state'] == "DUALOPEND_OPEN_INIT")
+        assert(event2['new_state'] == "DUALOPEND_AWAITING_LOCKIN")
+        assert(event2['cause'] == "remote")
+        assert(event2['message'] == "Sigs exchanged, waiting for lock-in")
+        event2 = wait_for_event(l2)
+
     assert(event2['peer_id'] == l1_id)  # we only test these IDs the first time
     assert(event2['channel_id'] == cid)
     assert(event2['short_channel_id'] == scid)
-    assert(event2['old_state'] == "CHANNELD_AWAITING_LOCKIN")
+    if l2.config('experimental-dual-fund'):
+        assert(event2['old_state'] == "DUALOPEND_AWAITING_LOCKIN")
+    else:
+        assert(event2['old_state'] == "CHANNELD_AWAITING_LOCKIN")
     assert(event2['new_state'] == "CHANNELD_NORMAL")
     assert(event2['cause'] == "remote")
     assert(event2['message'] == "Lockin complete")
