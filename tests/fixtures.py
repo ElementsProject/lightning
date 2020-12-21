336e972bf80028d2561f60b9685f888e73f0ae7f
Christian Decker <decker.christian@gmail.com>
2020-12-21 15:06:07 +0100

    pytest: Really only check DB statements if the env-var is "1"

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -19,7 +19,7 @@ class LightningNode(utils.LightningNode):
 
         # If we opted into checking the DB statements we will attach the dblog
         # plugin before starting the node
-        check_dblog = os.environ.get("TEST_CHECK_DBSTMTS", None) is not None
+        check_dblog = os.environ.get("TEST_CHECK_DBSTMTS", None) == "1"
         db = os.environ.get("TEST_DB_PROVIDER", "sqlite3")
         if db == 'sqlite3' and check_dblog:
             dblog = os.path.join(os.path.dirname(__file__), 'plugins', 'dblog.py')
