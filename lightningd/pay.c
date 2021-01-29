7b86013c1be0df7b40e97f56933d666996fab0aa
Rusty Russell <rusty@rustcorp.com.au>
2021-01-29 10:30:09 +1030

    listsendpays: remove deprecated "null" amount_msat.
    Changelog-Removed: `listsendpays` will no longer add `amount_msat` `null` (deprecated 0.9.1).
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/pay.c
+++ lightningd/pay.c
@@ -129,9 +129,6 @@ void json_add_payment_fields(struct json_stream *response,
 	if (amount_msat_greater(t->msatoshi, AMOUNT_MSAT(0)))
 		json_add_amount_msat_compat(response, t->msatoshi, "msatoshi",
 					    "amount_msat");
-	else if (deprecated_apis)
-		json_add_null(response, "amount_msat");
-
 
 	json_add_amount_msat_compat(response, t->msatoshi_sent,
 				    "msatoshi_sent", "amount_sent_msat");
