24336e0601112c215bfaaf3f4e192d3d25975d0a
Rusty Russell <rusty@rustcorp.com.au>
2021-02-02 23:18:01 +1030

    common: infrastructure to construct warning messages.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/wire_error.h
+++ common/wire_error.h
@@ -32,6 +32,30 @@ u8 *towire_errorfmtv(const tal_t *ctx,
 		     const char *fmt,
 		     va_list ap);
 
+/**
+ * towire_warningfmt - helper to turn string into WIRE_WARNING.
+ *
+ * @ctx: context to allocate from
+ * @channel: specific channel to complain about, or NULL for all.
+ * @fmt: format for warning.
+ */
+u8 *towire_warningfmt(const tal_t *ctx,
+		      const struct channel_id *channel,
+		      const char *fmt, ...) PRINTF_FMT(3,4);
+
+/**
+ * towire_warningfmtv - helper to turn string into WIRE_WARNING.
+ *
+ * @ctx: context to allocate from
+ * @channel: specific channel to complain about, or NULL for all.
+ * @fmt: format for warning.
+ * @ap: accumulated varargs.
+ */
+u8 *towire_warningfmtv(const tal_t *ctx,
+		       const struct channel_id *channel,
+		       const char *fmt,
+		       va_list ap);
+
 /* BOLT #1:
  *
  * The channel is referred to by `channel_id`, unless `channel_id` is 0
