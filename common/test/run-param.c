9e757030c1d28c30dddd1509622b8a6a10b016a7
Christian Decker <decker.christian@gmail.com>
2021-06-18 17:21:41 +0200

    jsonrpc: Add parsers for routehint-arrays
    We'll start passing routehints manually to keysend to reach non-public
    nodes as well.

--- common/test/run-param.c
+++ common/test/run-param.c
@@ -49,6 +49,10 @@ bool fromwire_tlv(const u8 **cursor UNNEEDED, size_t *max UNNEEDED,
 bool json_to_channel_id(const char *buffer UNNEEDED, const jsmntok_t *tok UNNEEDED,
 			struct channel_id *cid UNNEEDED)
 { fprintf(stderr, "json_to_channel_id called!\n"); abort(); }
+/* Generated stub for json_to_msat */
+bool json_to_msat(const char *buffer UNNEEDED, const jsmntok_t *tok UNNEEDED,
+		  struct amount_msat *msat UNNEEDED)
+{ fprintf(stderr, "json_to_msat called!\n"); abort(); }
 /* Generated stub for json_to_node_id */
 bool json_to_node_id(const char *buffer UNNEEDED, const jsmntok_t *tok UNNEEDED,
 			       struct node_id *id UNNEEDED)
