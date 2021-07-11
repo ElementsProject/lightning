48a46a3ae1a483c6215f3a0e6a2972c474c05918
Rusty Russell <rusty@rustcorp.com.au>
2021-07-11 16:29:42 +0930

    overflows: helpers for integer assignment overflows.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/overflows.h
+++ common/overflows.h
@@ -1,6 +1,7 @@
 #ifndef LIGHTNING_COMMON_OVERFLOWS_H
 #define LIGHTNING_COMMON_OVERFLOWS_H
 #include "config.h"
+#include <ccan/short_types/short_types.h>
 
 static inline bool add_overflows_size_t(uint64_t a, uint64_t b)
 {
@@ -20,5 +21,23 @@ static inline bool mul_overflows_u64(uint64_t a, uint64_t b)
 		return false;
 	ret = a * b;
 	return (ret / a != b);
+}
+
+static inline bool assign_overflow_u8(u8 *dst, uint64_t v)
+{
+	*dst = v;
+	return *dst == v;
+}
+
+static inline bool assign_overflow_u16(u16 *dst, uint64_t v)
+{
+	*dst = v;
+	return *dst == v;
+}
+
+static inline bool assign_overflow_u32(u32 *dst, uint64_t v)
+{
+	*dst = v;
+	return *dst == v;
 }
 #endif /* LIGHTNING_COMMON_OVERFLOWS_H */
