c82f2e3aed5d89bcb8db271064ed9dda0aac41a0
niftynei <niftynei@gmail.com>
2021-08-18 21:51:56 -0500

    tests: use descriptor wallets by default

--- contrib/pyln-testing/pyln/testing/utils.py
+++ contrib/pyln-testing/pyln/testing/utils.py
@@ -390,7 +390,7 @@ class BitcoinD(TailableProc):
 
         logging.info("BitcoinD started")
         try:
-            self.rpc.createwallet("lightningd-tests")
+            self.rpc.createwallet("lightningd-tests", False, False, "", False, True)
         except JSONRPCError:
             self.rpc.loadwallet("lightningd-tests")
 
