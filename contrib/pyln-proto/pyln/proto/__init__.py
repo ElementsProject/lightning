478c43cd9cbd37141173d1570db7b66d5402c35f
Christian Decker <decker.christian@gmail.com>
2021-09-23 14:48:07 +0200

    pyln: Derive version from git for pyln-proto

--- contrib/pyln-proto/pyln/proto/__init__.py
+++ contrib/pyln-proto/pyln/proto/__init__.py
@@ -3,8 +3,7 @@ from .primitives import ShortChannelId, PublicKey
 from .invoice import Invoice
 from .onion import OnionPayload, TlvPayload, LegacyOnionPayload
 from .wire import LightningConnection, LightningServerSocket
-
-__version__ = '0.10.1'
+from .__version__ import __version__
 
 __all__ = [
     "Invoice",
@@ -16,4 +15,5 @@ __all__ = [
     "bech32_decode",
     "ShortChannelId",
     "PublicKey",
+    "__version__",
 ]
