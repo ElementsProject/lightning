b051c34007f1bec7db2e8ab9dc72b35ad2c3005b
Robert Dickinson <robert.lee.dickinson@gmail.com>
2021-09-27 09:42:56 -0400

    Refactor argument helper functions

--- common/graphql_args.c
+++ common/graphql_args.c
@@ -2,57 +2,63 @@
 #include <common/json_helpers.h>
 #include <external/jsmn/jsmn.h>
 
-bool arg_node_id(const char *name, struct graphql_field *field, void *ctx, struct node_id **id) {
+/* Helper: Get a field argument by name, or NULL */
+static struct graphql_argument *find_arg(struct graphql_field *field, const char *argname) {
 	struct graphql_argument *arg;
-	*id = NULL;
-	if (!field->args || !field->args->first)
-		return false;
+	if (!field || !field->args)
+		return NULL;
 	for (arg = field->args->first; arg; arg = arg->next) {
 		if (arg->name && arg->name->token_type == 'a' &&
 		    arg->name->token_string &&
-		    streq(arg->name->token_string, name) &&
-		    arg->val && arg->val->str_val && arg->val->str_val->val &&
-		    arg->val->str_val->val->token_type == 's' &&
-		    arg->val->str_val->val->token_string) {
-			struct graphql_token *t = arg->val->str_val->val;
+		    streq(arg->name->token_string, argname))
+			return arg;
+	}
+	return NULL;
+}
 
-			/* A hackish way of being able to reuse json_to_node_id */
-			jsmntok_t tmptok;
-			tmptok.type = JSMN_STRING;
-			tmptok.start = 0;
-			tmptok.end = strlen(t->token_string);
-			tmptok.size = 0;
+/* Helper: Get a string argument's value, or NULL */
+static const char *get_string(struct graphql_argument *arg) {
+	if (arg && arg->val && arg->val->str_val && arg->val->str_val->val &&
+	    arg->val->str_val->val->token_type == 's' &&
+	    arg->val->str_val->val->token_string)
+		return arg->val->str_val->val->token_string;
+	return NULL;
+}
 
-			*id = tal(ctx, struct node_id);
-			if (json_to_node_id(t->token_string, &tmptok, *id))
-				return true;
-			*id = tal_free(*id);
+/* Get a node_id argument */
+bool arg_node_id(const char *name, struct graphql_field *field, void *ctx, struct node_id **id) {
+	const char *str;
+	*id = NULL;
+	if ((str = get_string(find_arg(field, name)))) {
+		/* A hackish way of being able to reuse json_to_node_id */
+		jsmntok_t tmptok;
+		tmptok.type = JSMN_STRING;
+		tmptok.start = 0;
+		tmptok.end = strlen(str);
+		tmptok.size = 0;
+
+		*id = tal(ctx, struct node_id);
+		if (json_to_node_id(str, &tmptok, *id))
 			return true;
-		}
+
+		*id = tal_free(*id);
+		return true;
 	}
 	return false;
 }
 
+/* Get a log_level argument */
 bool arg_loglevel(const char *name, struct graphql_field *field, void *ctx, enum log_level **ll) {
-	struct graphql_argument *arg;
+	const char *str;
 	*ll = NULL;
-	if (!field->args || !field->args->first)
-		return false;
-	for (arg = field->args->first; arg; arg = arg->next) {
-		if (arg->name && arg->name->token_type == 'a' &&
-		    arg->name->token_string &&
-		    streq(arg->name->token_string, name) &&
-		    arg->val && arg->val->str_val && arg->val->str_val->val &&
-		    arg->val->str_val->val->token_type == 's' &&
-		    arg->val->str_val->val->token_string) {
-			struct graphql_token *t = arg->val->str_val->val;
+	if ((str = get_string(find_arg(field, name)))) {
 
-			*ll = tal(ctx, enum log_level);
-			if (log_level_parse(t->token_string, strlen(t->token_string), *ll))
-				return true;
-			*ll = tal_free(*ll);
+		*ll = tal(ctx, enum log_level);
+		if (log_level_parse(str, strlen(str), *ll))
 			return true;
-		}
+
+		*ll = tal_free(*ll);
+		return true;
 	}
 	return false;
 }
