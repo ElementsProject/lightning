d43a6bb3a742fd1322589a4d74e563de9173b542
Robert Dickinson <robert.lee.dickinson@gmail.com>
2021-09-27 15:22:19 -0400

    Rename header definition

--- common/graphql_args.h
+++ common/graphql_args.h
@@ -1,5 +1,5 @@
-#ifndef LIGHTNING_COMMON_GRAPHQL_UTIL_H
-#define LIGHTNING_COMMON_GRAPHQL_UTIL_H 1
+#ifndef LIGHTNING_COMMON_GRAPHQL_ARGS_H
+#define LIGHTNING_COMMON_GRAPHQL_ARGS_H
 #include "config.h"
 #include <ccan/graphql/graphql.h>
 #include <common/node_id.h>
@@ -8,5 +8,5 @@
 bool arg_node_id(const char *name, struct graphql_field *field, void *ctx, struct node_id **id);
 bool arg_loglevel(const char *name, struct graphql_field *field, void *ctx, enum log_level **ll);
 
-#endif
+#endif /* LIGHTNING_COMMON_GRAPHQL_ARGS_H */
 
