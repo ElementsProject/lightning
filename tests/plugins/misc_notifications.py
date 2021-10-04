474a7837e594e0990887b3f8916745fccaa2b65f
Simon Vrouwe <s_github@protonmail.com>
2021-10-04 11:04:44 +0300

    fixup misc_notifications.py, skip the 30s waiting

--- tests/plugins/misc_notifications.py
+++ tests/plugins/misc_notifications.py
@@ -3,6 +3,7 @@
 """
 
 from pyln.client import Plugin
+import sys
 
 plugin = Plugin()
 
@@ -27,10 +28,10 @@ def channel_state_changed(plugin, channel_state_changed, **kwargs):
 
 @plugin.subscribe("shutdown")
 def shutdown(plugin, **kwargs):
-    # Trigger a db_write. If another plugin registered the db_write hook, it
-    # should see the data before being shutdown and its hooks unregistered
+    # Trigger a db_write during shutdown, db_write plugins should see it
     plugin.log("received shutdown notification")
-    plugin.rpc.datastore(key='{}'.format(__file__), string="data written at shutdown")
+    plugin.rpc.datastore(key='{}'.format(__file__), string="data written at shutdown", mode="create-or-replace")
+    sys.exit(0) # skip the 30s waiting
 
 
 plugin.run()
