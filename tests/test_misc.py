ef503f2feaa0c183b6325ef2cb0e90b865095952
Simon Vrouwe <s_github@protonmail.com>
2021-11-10 09:41:43 +0200

    testing: remove test_stop_pending_fundchannel
    Not needed anymore, see previous commit

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -27,33 +27,6 @@ import time
 import unittest
 
 
-@pytest.mark.developer("needs --dev-disconnect")
-@pytest.mark.openchannel('v1')
-def test_stop_pending_fundchannel(node_factory, executor):
-    """Stop the daemon while waiting for an accept_channel
-
-    This used to crash the node, since we were calling unreserve_utxo while
-    freeing the daemon, but that needs a DB transaction to be open.
-
-    """
-    l1, l2 = node_factory.get_nodes(2)
-
-    l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
-
-    # We want l2 to stop replying altogether, not disconnect
-    os.kill(l2.daemon.proc.pid, signal.SIGSTOP)
-
-    # The fundchannel call will not terminate so run it in a future
-    executor.submit(l1.fundchannel, l2, 10**6)
-    l1.daemon.wait_for_log('peer_out WIRE_OPEN_CHANNEL')
-
-    l1.rpc.stop()
-
-    # Now allow l2 a clean shutdown
-    os.kill(l2.daemon.proc.pid, signal.SIGCONT)
-    l2.rpc.stop()
-
-
 def test_names(node_factory):
     # Note:
     # private keys:
