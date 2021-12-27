c1ab729b116443bbbc0a2f83149e501d2dc5a1a1
Rusty Russell <rusty@rustcorp.com.au>
2021-12-28 09:52:09 +1030

    common: is_msg_gossip_broadcast() for putting things in filter.
    We don't need the other "gossip" messages in our echo-suppression filter.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- wire/peer_wire.h
+++ wire/peer_wire.h
@@ -23,6 +23,8 @@
 bool is_unknown_msg_discardable(const u8 *cursor);
 /* Return true if it's a message for gossipd. */
 bool is_msg_for_gossipd(const u8 *cursor);
+/* Return true if it's a gossip update or announcement. */
+bool is_msg_gossip_broadcast(const u8 *cursor);
 
 /* Extract channel_id from various packets, return true if possible. */
 bool extract_channel_id(const u8 *in_pkt, struct channel_id *channel_id);
