4d0851f4d68c2168ac61aeae254cfcfa553fd84d
Christian Decker <decker.christian@gmail.com>
2021-12-30 16:46:01 +0100

    cln-rpc: Add top-level request and response enums

--- cln-rpc/src/requests.rs
+++ cln-rpc/src/requests.rs
@@ -1,3 +1,12 @@
+use serde::{Deserialize, Serialize};
+
 mod getinfo;
 
 pub use getinfo::*;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "method", content = "params")]
+#[serde(rename_all = "snake_case")]
+pub enum Request {
+    Getinfo(Getinfo),
+}
--- cln-rpc/src/responses.rs
+++ cln-rpc/src/responses.rs
@@ -6,3 +6,11 @@ use serde::{Deserialize, Serialize};
 mod getinfo;
 
 pub use getinfo::*;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "method", content = "result")]
+#[serde(rename_all = "snake_case")]
+pub enum Response {
+    Getinfo(Getinfo),
+}
+
