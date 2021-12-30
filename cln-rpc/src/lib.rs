8adffb25fa523e25d3a5f0c5543a401ab4c0e3ba
Christian Decker <decker.christian@gmail.com>
2021-12-30 16:47:34 +0100

    cln-rpc: Add a specialized codec for the RPC client side
    Can't use the one from the plugins since they are the receiving side
    of the plugin. Could be eventually joined into a generic variant.

--- cln-rpc/src/lib.rs
+++ cln-rpc/src/lib.rs
@@ -16,3 +16,37 @@ impl ClnRpc {
         todo!();
     }
 }
+
+mod codec {
+    use crate::JsonCodec;
+    use crate::{notifications::Notification, requests::Request};
+    use anyhow::Error;
+    use bytes::BytesMut;
+    use cln_plugins::codec::JsonRpc;
+    use tokio_util::codec::Decoder;
+
+    /// A codec that reads fully formed [crate::messages::JsonRpc]
+    /// messages. Internally it uses the [JsonCodec] which itself is built
+    /// on the [MultiLineCodec].
+    #[derive(Default)]
+    pub(crate) struct JsonRpcCodec {
+        inner: JsonCodec,
+    }
+
+    impl Decoder for JsonRpcCodec {
+        type Item = JsonRpc<Notification, Request>;
+        type Error = Error;
+
+        fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Error> {
+            match self.inner.decode(buf) {
+                Ok(None) => Ok(None),
+                Err(e) => Err(e),
+                Ok(Some(s)) => {
+                    let req: Self::Item = serde_json::from_value(s)?;
+                    Ok(Some(req))
+                }
+            }
+        }
+    }
+}
+
