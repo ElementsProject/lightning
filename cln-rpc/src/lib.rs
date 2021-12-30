8f6d219a38c65bcbfc857b2efe0c858d3ccb86b6
Christian Decker <decker.christian@gmail.com>
2021-12-30 16:48:54 +0100

    cln-rpc: Add read/write capabilities to the ClnRpc struct

--- cln-rpc/src/lib.rs
+++ cln-rpc/src/lib.rs
@@ -1,19 +1,78 @@
-use anyhow::{Error, Result};
+use anyhow::{Context, Error, Result};
+use cln_plugins::codec::JsonCodec;
+use cln_plugins::codec::JsonRpc;
+use futures_util::sink::SinkExt;
+use futures_util::StreamExt;
+use log::{debug, trace};
 use std::path::Path;
-mod primitives;
-mod responses;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use tokio::net::unix::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::UnixStream;
+use tokio_util::codec::{FramedRead, FramedWrite};
 
-#[macro_use]
-extern crate serde_json;
+pub mod notifications;
+pub mod primitives;
+pub mod requests;
+pub mod responses;
+
+pub use crate::{notifications::Notification, requests::Request, responses::Response};
+
 ///
-pub struct ClnRpc {}
+pub struct ClnRpc {
+    next_id: AtomicUsize,
+
+    #[allow(dead_code)]
+    read: FramedRead<OwnedReadHalf, JsonCodec>,
+    write: FramedWrite<OwnedWriteHalf, JsonCodec>,
+}
 
 impl ClnRpc {
-    pub fn new<P>(path: P) -> Result<ClnRpc>
+    pub async fn new<P>(path: P) -> Result<ClnRpc>
     where
         P: AsRef<Path>,
     {
-        todo!();
+        debug!(
+            "Connecting to socket at {}",
+            path.as_ref().to_string_lossy()
+        );
+        ClnRpc::from_stream(UnixStream::connect(path).await?)
+    }
+
+    fn from_stream(stream: UnixStream) -> Result<ClnRpc> {
+        let (read, write) = stream.into_split();
+
+        Ok(ClnRpc {
+            next_id: AtomicUsize::new(1),
+            read: FramedRead::new(read, JsonCodec::default()),
+            write: FramedWrite::new(write, JsonCodec::default()),
+        })
+    }
+
+    pub async fn call(&mut self, req: Request) -> Result<Response, Error> {
+        trace!("Sending request {:?}", req);
+
+        // Wrap the raw request in a well-formed JSON-RPC outer dict.
+        let id = self.next_id.fetch_add(1, Ordering::SeqCst);
+        let req: JsonRpc<Notification, Request> = dbg!(JsonRpc::Request(id, req));
+        let req = serde_json::to_value(req)?;
+	let req2 = req.clone();
+        self.write.send(req).await?;
+
+        let mut response = self
+            .read
+            .next()
+            .await
+            .context("no response from lightningd")?
+            .context("reading response from socket")?;
+        trace!("Read response {:?}", response);
+
+        // Annotate the response with the method from the request, so
+        // serde_json knows which variant of [`Request`] should be
+        // used.
+	response["method"] = req2["method"].clone();
+
+        serde_json::from_value(response).context("converting response into enum")
     }
 }
 
