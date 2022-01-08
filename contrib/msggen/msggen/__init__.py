6171b30512860d59ac2e02dd97f7fbdc454636a8
Christian Decker <decker.christian@gmail.com>
2022-01-08 13:37:04 +0100

    msggen: Add GrpcGenerator
    This generator class includes all the Rust conventions and syntax to
    generate structs and enums matching the messages, as well as tying the
    RPC methods into a Service. It doesn't yet generate the dispatch code
    and the conversions, but those are next.

--- contrib/msggen/msggen/__init__.py
+++ contrib/msggen/msggen/__init__.py
@@ -1,7 +1,10 @@
-from .model import parse_doc
+from .model import parse_doc, Service, Method
 from .rust import gen_rust
-
+from .grpc import GrpcGenerator
 __all__ = [
     "gen_rust",
     "parse_doc",
+    "GrpcGenerator",
+    "Service",
+    "Method",
 ]
