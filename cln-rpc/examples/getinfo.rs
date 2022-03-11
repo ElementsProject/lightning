9fbc86587a59873ff747d01ea185f3ba4de99f6b
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-03-11 14:09:26 +0100

    rust: fixed compiler warning in the example
    Changelog-None: rust: fixed compiler warning in the example
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- cln-rpc/examples/getinfo.rs
+++ cln-rpc/examples/getinfo.rs
@@ -1,13 +1,14 @@
 use anyhow::Context;
 use cln_rpc::{model::GetinfoRequest, ClnRpc, Request};
-use log::info;
 use std::env::args;
 use std::path::Path;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    // initialize the log inside the library
     env_logger::init();
+
     let rpc_path = args().nth(1).context("missing argument: socket path")?;
     let p = Path::new(&rpc_path);
 
