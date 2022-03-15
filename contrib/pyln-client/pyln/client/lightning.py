3befae4553b60d760bc6ccbb8e0caf7df3e5ea84
Christian Decker <decker.christian@gmail.com>
2022-03-15 12:38:39 +0100

    pyln: Change the default argument to `exclude` in `pay`
    We must use `None` for default arguments since otherwise they aren't
    filtered out when serializing the request. In addition default
    arguments to functions are initialized once and, if mutable, could
    persist internal changes across function calls.
    
    Changelog-None

--- contrib/pyln-client/pyln/client/lightning.py
+++ contrib/pyln-client/pyln/client/lightning.py
@@ -990,7 +990,7 @@ class LightningRpc(UnixDomainSocketRpc):
 
     def pay(self, bolt11, msatoshi=None, label=None, riskfactor=None,
             maxfeepercent=None, retry_for=None,
-            maxdelay=None, exemptfee=None, exclude=[]):
+            maxdelay=None, exemptfee=None, exclude=None):
         """
         Send payment specified by {bolt11} with {msatoshi}
         (ignored if {bolt11} has an amount), optional {label}
