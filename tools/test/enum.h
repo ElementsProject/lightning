9a511975e09eef071ee82762bc3f33bf390e7dea
Rusty Russell <rusty@rustcorp.com.au>
2022-03-23 10:01:14 +1030

    tools/generate-wire.py: have printwire routines do demarshal.
    This works better in general: let printwire_x do the work of figuring
    out how to demarshal x.  This is particularly important for TLVs, which
    require a call to tlv_x_new() first.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/test/enum.h
+++ tools/test/enum.h
@@ -2,6 +2,7 @@
 #define LIGHTNING_TOOLS_TEST_ENUM_H
 #include "config.h"
 #include <ccan/short_types/short_types.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define TEST_IFDEF 0
@@ -13,6 +14,6 @@ enum test_enum {
 
 void towire_test_enum(u8 **pptr, const enum test_enum test_enum);
 enum test_enum fromwire_test_enum(const u8 **cursor, size_t *max);
-void printwire_test_enum(const char *fieldname, const enum test_enum *test_enum);
+bool printwire_test_enum(const char *fieldname, const u8 **cursor, size_t *max);
 
 #endif /* LIGHTNING_TOOLS_TEST_ENUM_H */
