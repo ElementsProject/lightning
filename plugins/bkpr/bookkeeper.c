69154bd96c90002085addfea8336231b07178288
niftynei <niftynei@gmail.com>
2022-05-03 19:21:25 -0500

    bkpr: correctly pass in command for `jsonrpc_request_start`
    We weren't passing the command, which meant that it wasn't getting
    populated correctly.
    
    We do that now, it fixes some crashes

--- plugins/bkpr/bookkeeper.c
+++ plugins/bkpr/bookkeeper.c
@@ -1132,7 +1132,7 @@ static struct command_result *json_balance_snapshot(struct command *cmd,
 	if (tal_count(new_accts) > 0) {
 		struct out_req *req;
 
-		req = jsonrpc_request_start(cmd->plugin, NULL,
+		req = jsonrpc_request_start(cmd->plugin, cmd,
 					    "listpeers",
 					    listpeers_multi_done,
 					    log_error,
@@ -1320,7 +1320,7 @@ parse_and_log_chain_move(struct command *cmd,
 		info = tal(NULL, struct event_info);
 		info->ev = tal_steal(info, e);
 		info->acct = tal_steal(info, acct);
-		req = jsonrpc_request_start(cmd->plugin, NULL,
+		req = jsonrpc_request_start(cmd->plugin, cmd,
 					    "listpeers",
 					    listpeers_done,
 					    log_error,
