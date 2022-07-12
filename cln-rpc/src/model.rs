a3a1bd964d5757c1126edbaab33b78538812d3d1
Christian Decker <decker.christian@gmail.com>
2022-07-12 17:03:58 +0200

    doc: Update generated artifacts to match master

--- cln-rpc/src/model.rs
+++ cln-rpc/src/model.rs
@@ -2739,10 +2739,10 @@ pub mod responses {
 	    pub description: Option<String>,
 	    #[serde(alias = "bolt12", skip_serializing_if = "Option::is_none")]
 	    pub bolt12: Option<String>,
-	    #[serde(alias = "amount_msat", skip_serializing_if = "Option::is_none")]
-	    pub amount_msat: Option<Amount>,
-	    #[serde(alias = "amount_sent_msat", skip_serializing_if = "Option::is_none")]
-	    pub amount_sent_msat: Option<Amount>,
+	    #[serde(alias = "preimage", skip_serializing_if = "Option::is_none")]
+	    pub preimage: Option<String>,
+	    #[serde(alias = "number_of_parts", skip_serializing_if = "Option::is_none")]
+	    pub number_of_parts: Option<u64>,
 	    #[serde(alias = "erroronion", skip_serializing_if = "Option::is_none")]
 	    pub erroronion: Option<String>,
 	}
