f2e23d4e69d957d5eac0a4e10f595151c9a32576
niftynei <niftynei@gmail.com>
2022-07-19 14:37:26 +0930

    bkpr: first attempt at database code for accounting
    A database scheme and first attempt at drivers for the bookkeeper
    database.
    
    Also moves bookkeeper plugin into its own subdirectory

---
+++ plugins/bkpr/db.h
@@ -0,0 +1,11 @@
+#ifndef LIGHTNING_PLUGINS_BKPR_DB_H
+#define LIGHTNING_PLUGINS_BKPR_DB_H
+#include "config.h"
+#include <ccan/tal/tal.h>
+
+struct plugin;
+struct db;
+
+struct db *db_setup(const tal_t *ctx, struct plugin *p, char *db_dsn);
+
+#endif /* LIGHTNING_PLUGINS_BKPR_DB_H */
