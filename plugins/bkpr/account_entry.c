8089f246c19b6ad2b93126d932ae6fcbc0dc7695
niftynei <niftynei@gmail.com>
2022-07-19 17:04:35 +0930

    bkpr: use tags not str for tag originations

---
+++ plugins/bkpr/account_entry.c
@@ -0,0 +1,26 @@
+#include "config.h"
+
+#include <ccan/tal/str/str.h>
+#include <plugins/bkpr/account_entry.h>
+#include <stddef.h>
+
+static const char *tags[] = {
+	"journal_entry",
+};
+
+const char *account_entry_tag_str(enum account_entry_tag tag)
+{
+	return tags[tag];
+}
+
+bool account_entry_tag_find(char *str, enum account_entry_tag *tag)
+{
+	for (size_t i = 0; i < NUM_ACCOUNT_ENTRY_TAGS; i++) {
+		if (streq(str, tags[i])) {
+			*tag = (enum account_entry_tag) i;
+			return true;
+		}
+	}
+
+	return false;
+}
