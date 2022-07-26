be134341919478edfb3819d5da2ba16bbf0ceed0
Greg Sanders <gsanders87@gmail.com>
2022-07-26 13:47:52 -0400

    Strip out key that collides with PSBTv2 keytypes

--- wallet/test/run-wallet.c
+++ wallet/test/run-wallet.c
@@ -1682,7 +1682,7 @@ static bool test_channel_inflight_crud(struct lightningd *ld, const tal_t *ctx)
 
 	/* Update the PSBT for both inflights, check that are updated
 	 * correctly on save */
-	funding_psbt = psbt_from_b64(w, "cHNidP8BAD8CAAAAAf//////////////////////////////////////////AAAAAAD/////AQAAAAAAAAAAA2oBAAAAAAAACg8BAgMEBQYHCAkPAQIDBAUGBwgJCgsMDQ4PAAA=", strlen("cHNidP8BAD8CAAAAAf//////////////////////////////////////////AAAAAAD/////AQAAAAAAAAAAA2oBAAAAAAAACg8BAgMEBQYHCAkPAQIDBAUGBwgJCgsMDQ4PAAA="));
+	funding_psbt = psbt_from_b64(w, "cHNidP8BAD8CAAAAAf//////////////////////////////////////////AAAAAAD/////AQAAAAAAAAAAA2oBAAAAAAAAAAA=", strlen("cHNidP8BAD8CAAAAAf//////////////////////////////////////////AAAAAAD/////AQAAAAAAAAAAA2oBAAAAAAAAAAA="));
 	list_for_each(&chan->inflights, inflight, list)
 		inflight->funding_psbt = funding_psbt;
 	wallet_channel_save(w, chan);
