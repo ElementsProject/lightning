ff328b0058f4046b58d3edbc910bf2a4a845c867
Christian Decker <decker.christian@gmail.com>
2022-08-31 17:38:33 +0200

    pyln: Update pyln-bolt2 and pyln-bolt7 to the newest status
    Suggested-by: Vincenzo Palazzo <@vincenzopalazzo>

--- contrib/pyln-spec/bolt2/pyln/spec/bolt2/gen_version.py
+++ contrib/pyln-spec/bolt2/pyln/spec/bolt2/gen_version.py
@@ -1,3 +1,3 @@
 __base_version__ = "1.0"
-__post_version__ = "222"
-__gitversion__ = "f1c797df2966237244527c1c6343dbe9bc765342"
+__post_version__ = "246"
+__gitversion__ = "f32c6ddb5f11b431c9bb4f501cdec604172a90de"
