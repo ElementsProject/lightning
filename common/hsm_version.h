22377ecbdf1b6da33c369d6c6708201668c8d0c4
Rusty Russell <rusty@rustcorp.com.au>
2022-10-17 11:04:08 +1030

    hsmd: introduce a simple API versioning scheme.
    With the rise of external HSMs like VLS, this is no longer an
    internal-only API.  Fortunately, it doesn't change very fast so
    maintenance should not be a huge burden.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ common/hsm_version.h
@@ -0,0 +1,9 @@
+#ifndef LIGHTNING_COMMON_HSM_VERSION_H
+#define LIGHTNING_COMMON_HSM_VERSION_H
+#include "config.h"
+
+/* We give a maximum and minimum compatibility version to HSM, to allow
+ * some API adaptation. */
+#define HSM_MIN_VERSION 1
+#define HSM_MAX_VERSION 1
+#endif /* LIGHTNING_COMMON_HSM_VERSION_H */
