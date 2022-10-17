987adb97180848908ec54ff43d3b564f1a40c45a
Rusty Russell <rusty@rustcorp.com.au>
2022-10-17 11:05:01 +1030

    Makefile: check that hsm_version.h changes if wire/hsmd_wire.csv contents does
    Otherwise I know we'll miss it.  Simply check for a mention: we could well
    change things multiple times within a single release.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/hsm_version.h
+++ common/hsm_version.h
@@ -4,6 +4,14 @@
 
 /* We give a maximum and minimum compatibility version to HSM, to allow
  * some API adaptation. */
+
+/* wire/hsmd_wire.csv contents version:
+ *    409cffa355ab6cc76bd298910adca9936a68223267ddc4815ba16aeac5d0acc3
+ */
 #define HSM_MIN_VERSION 1
+
+/* wire/hsmd_wire.csv contents version:
+ *    409cffa355ab6cc76bd298910adca9936a68223267ddc4815ba16aeac5d0acc3
+ */
 #define HSM_MAX_VERSION 1
 #endif /* LIGHTNING_COMMON_HSM_VERSION_H */
