5ba948c48f3e8b75e2f72fca1faa14fbea56f308
Rusty Russell <rusty@rustcorp.com.au>
2022-10-17 11:07:05 +1030

    gossmap: move gossmap_guess_node_id to pay plugin.
    This removes a point32 dependency.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/gossmap.c
+++ common/gossmap.c
@@ -1289,18 +1289,3 @@ int gossmap_node_get_feature(const struct gossmap *map,
 	return map_feature_test(map, COMPULSORY_FEATURE(fbit),
 				n->nann_off + feature_len_off + 2, feature_len);
 }
-
-/* There are two 33-byte pubkeys possible: choose the one which appears
- * in the graph (otherwise payment will fail anyway). */
-void gossmap_guess_node_id(const struct gossmap *map,
-			   const struct point32 *point32,
-			   struct node_id *id)
-{
-	struct pubkey pk;
-	pk.pubkey = point32->pubkey;
-	node_id_from_pubkey(id, &pk);
-
-	/* If we don't find this, let's assume it's the alternate. */
-	if (!gossmap_find_node(map, id))
-		id->k[0] |= 1;
-}
