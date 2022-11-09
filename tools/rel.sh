2e4a58efac38ae2184abe263cd7a5cdcd32e6753
Rusty Russell <rusty@rustcorp.com.au>
2022-11-09 13:02:01 +1030

    check: fix warnings from shellcheck 0.8.0
    And make errors gcc-style, so emacs can jump through the automatically.
    
    ```
    In devtools/reduce-includes.sh line 21:
    	    echo -n "-$LINE"
                     ^-- SC3037 (warning): In POSIX sh, echo flags are undefined.
    
    
    In devtools/reduce-includes.sh line 25:
    	    echo -n "."
                     ^-- SC3037 (warning): In POSIX sh, echo flags are undefined.
    
    
    In tools/rel.sh line 6:
    prefix=$(printf '%s\n' "${from#$common}" | sed 's@[^/][^/]*@..@g')
                                   ^-----^ SC2295 (info): Expansions inside ${..} need to be quoted separately, otherwise they match as patterns.
    
    Did you mean: 
    prefix=$(printf '%s\n' "${from#"$common"}" | sed 's@[^/][^/]*@..@g')
    
    
    In tools/rel.sh line 7:
    printf '%s\n' "$prefix/${to#$common}"
                                ^-----^ SC2295 (info): Expansions inside ${..} need to be quoted separately, otherwise they match as patterns.
    
    Did you mean: 
    printf '%s\n' "$prefix/${to#"$common"}"
    
    For more information:
      https://www.shellcheck.net/wiki/SC3037 -- In POSIX sh, echo flags are undef...
      https://www.shellcheck.net/wiki/SC2295 -- Expansions inside ${..} need to b...
    make: *** [Makefile:553: check-shellcheck] Error 123
    ```
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/rel.sh
+++ tools/rel.sh
@@ -3,5 +3,5 @@
 from=${1}
 to=${2}
 common=$(printf '%s\n%s' "${from}" "${to}" | sed 'N;s/\(.*\).*\n\1.*$/\1/' | sed 's@/[^/]*$@/@')
-prefix=$(printf '%s\n' "${from#$common}" | sed 's@[^/][^/]*@..@g')
-printf '%s\n' "$prefix/${to#$common}"
+prefix=$(printf '%s\n' "${from#"$common"}" | sed 's@[^/][^/]*@..@g')
+printf '%s\n' "$prefix/${to#"$common"}"
