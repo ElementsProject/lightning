0a06cdd1da17d26d5e1c06e0a41a088baa25e814
Christian Decker <decker.christian@gmail.com>
2022-11-10 17:05:20 +0100

    pyln: Bump versions

--- contrib/pyln-proto/pyln/proto/__init__.py
+++ contrib/pyln-proto/pyln/proto/__init__.py
@@ -4,7 +4,7 @@ from .invoice import Invoice
 from .onion import OnionPayload, TlvPayload, LegacyOnionPayload
 from .wire import LightningConnection, LightningServerSocket
 
-__version__ = "0.12.0"
+__version__ = "22.11rc1"
 
 __all__ = [
     "Invoice",
--- contrib/pyln-testing/pyln/testing/__init__.py
+++ contrib/pyln-testing/pyln/testing/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.12.1"
+__version__ = "22.11rc1"
 
 __all__ = [
     "__version__",
