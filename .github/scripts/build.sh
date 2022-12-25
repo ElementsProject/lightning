fa66674df623874df2c80f3446595fe6996e553d
arowser <arowser@gmail.com>
2022-12-25 11:17:11 +0800

    fix arm ci

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 echo "Running in $(pwd)"
-export ARCH=${ARCH:-64}
 export BOLTDIR=bolts
 export CC=${COMPILER:-gcc}
 export COMPAT=${COMPAT:-1}
@@ -52,16 +51,16 @@ then
     export CXX="$TARGET_HOST"-g++
     export LD="$TARGET_HOST"-ld
     export STRIP="$TARGET_HOST"-strip
-    export CONFIGURATION_WRAPPER=qemu-"${TARGET_HOST%%-*}"-static
+    export CONFIGURATOR_WRAPPER=qemu-"${ARCH}"-static
 
-    wget -q https://zlib.net/zlib-1.2.12.tar.gz
-    tar xf zlib-1.2.12.tar.gz
-    cd zlib-1.2.12 || exit 1
+    wget -q https://zlib.net/fossils/zlib-1.2.13.tar.gz
+    tar xf zlib-1.2.13.tar.gz
+    cd zlib-1.2.13 || exit 1
     ./configure --prefix="$QEMU_LD_PREFIX"
     make
     sudo make install
     cd .. || exit 1
-    rm zlib-1.2.12.tar.gz && rm -rf zlib-1.2.12
+    rm zlib-1.2.13.tar.gz && rm -rf zlib-1.2.13
 
     wget -q https://www.sqlite.org/2018/sqlite-src-3260000.zip
     unzip -q sqlite-src-3260000.zip
