a40d41818e317bd1161e90d277bae6ae770978f9
Michael Schmoock <michael@schmoock.net>
2022-12-28 18:44:25 +0100

    pyln-client: make Millisatoshi comparable to int
    This often helps the msat purge and pyln msat replacement mischmasch issues.
    I changed it in a way that the `AttributeError: 'int' object has no attribute 'millisatoshis'`
    Error will still be raised whever a `Millisatoshi` is compared to
    something else then a `Millisatoshi` or `int`.
    
    Changelog-None

--- contrib/pyln-client/pyln/client/lightning.py
+++ contrib/pyln-client/pyln/client/lightning.py
@@ -163,9 +163,13 @@ class Millisatoshi:
         return self.millisatoshis
 
     def __lt__(self, other: 'Millisatoshi') -> bool:
+        if isinstance(other, int):
+            return self.millisatoshis < other
         return self.millisatoshis < other.millisatoshis
 
     def __le__(self, other: 'Millisatoshi') -> bool:
+        if isinstance(other, int):
+            return self.millisatoshis <= other
         return self.millisatoshis <= other.millisatoshis
 
     def __eq__(self, other: object) -> bool:
@@ -177,9 +181,13 @@ class Millisatoshi:
             return False
 
     def __gt__(self, other: 'Millisatoshi') -> bool:
+        if isinstance(other, int):
+            return self.millisatoshis > other
         return self.millisatoshis > other.millisatoshis
 
     def __ge__(self, other: 'Millisatoshi') -> bool:
+        if isinstance(other, int):
+            return self.millisatoshis >= other
         return self.millisatoshis >= other.millisatoshis
 
     def __add__(self, other: 'Millisatoshi') -> 'Millisatoshi':
