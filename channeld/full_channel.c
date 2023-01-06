e10dcd24fea596942ece00a22a0e7ad7d9127194
Matt Whitlock <c-lightning@mattwhitlock.name>
2023-01-05 21:34:56 -0500

    channeld: remove dead HTLCs from htable and free them (eventually)
    The channel->htlcs map was exhibiting unbounded growth, as elements were
    never removed from it. This was causing lightning_channeld processes to
    consume ever-increasing amounts of memory, and iterating over the map
    was causing ever-increasing CPU utilization. There were FIXME comments
    suggesting that the intention was to remove HTLCs from the map upon
    their deaths. This commit implements that intention.
    
    Changelog-Fixed: channeld no longer retains dead HTLCs in memory.

--- channeld/full_channel.c
+++ channeld/full_channel.c
@@ -1110,10 +1110,15 @@ static int change_htlcs(struct channel *channel,
 		for (i = 0; i < n_hstates; i++) {
 			if (h->state == htlc_states[i]) {
 				htlc_incstate(channel, h, sidechanged, owed);
+				if (htlc_is_dead(h)) {
+					htlc_map_delval(channel->htlcs, &it);
+					tal_steal(htlcs ? *htlcs : tmpctx, h);
+				}
 				dump_htlc(h, prefix);
 				htlc_arr_append(htlcs, h);
 				cflags |= (htlc_state_flags(htlc_states[i])
 					   ^ htlc_state_flags(h->state));
+				break;
 			}
 		}
 	}
@@ -1391,20 +1396,24 @@ bool channel_sending_revoke_and_ack(struct channel *channel)
 	return (change & HTLC_REMOTE_F_PENDING);
 }
 
-size_t num_channel_htlcs(const struct channel *channel)
+static inline bool any_htlc_is_dead(const struct channel *channel)
 {
 	struct htlc_map_iter it;
 	const struct htlc *htlc;
-	size_t n = 0;
 
 	for (htlc = htlc_map_first(channel->htlcs, &it);
 	     htlc;
 	     htlc = htlc_map_next(channel->htlcs, &it)) {
-		/* FIXME: Clean these out! */
-		if (!htlc_is_dead(htlc))
-			n++;
+		if (htlc_is_dead(htlc))
+			return true;
 	}
-	return n;
+	return false;
+}
+
+size_t num_channel_htlcs(const struct channel *channel)
+{
+	assert(!any_htlc_is_dead(channel));
+	return htlc_map_count(channel->htlcs);
 }
 
 static bool adjust_balance(struct balance view_owed[NUM_SIDES][NUM_SIDES],
