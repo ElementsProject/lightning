8f94e8b94315d17d9316146b5b3c5ae64184432f
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-01-06 15:09:55 +0100

    comm: make sure that our version check is reliable
    Rework the logic of the version check used in the
    database migration, and make sure
    that it is full functional to avoid confusion
    at release time.
    
    Changelog-Fixed: database: Correctly identity official release versions for database upgrade.
    
    Reported-by: @urza
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- common/version.h
+++ common/version.h
@@ -1,9 +1,14 @@
 #ifndef LIGHTNING_COMMON_VERSION_H
 #define LIGHTNING_COMMON_VERSION_H
 #include "config.h"
+#include <stdbool.h>
 
 char *version_and_exit(const void *unused);
 const char *version(void);
+/* check if the current version is a release version.
+ *
+ * Released versions are of form v[year].[month]?(.patch)* */
+bool is_released_version(void);
 
 #define opt_register_version()						\
 	opt_register_early_noarg("--version|-V", version_and_exit, NULL, \
