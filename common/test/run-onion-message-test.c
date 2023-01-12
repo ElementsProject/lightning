a202ff32889a97ea2a5411b5481c562618df4c7b
Rusty Russell <rusty@rustcorp.com.au>
2023-01-12 14:25:38 +1030

    common: update to latest onion-message spec.
    ```
    make check-source-bolt CHECK_BOLT_PREFIX="--prefix=BOLT-onion-message" BOLTVERSION=guilt/offers
    ```
    
    Mainly textual, though I neatened the extra fields check for TLVs with
    blinding, and implemented the "no other fields" requirement for
    non-final onion message hops.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/test/run-onion-message-test.c
+++ common/test/run-onion-message-test.c
@@ -337,7 +337,7 @@ int main(int argc, char *argv[])
 	sphinx_path->session_key = &session_key;
 
 	/* BOLT-onion-message #4:
-	 * - SHOULD set `len` to 1366 or 32834.
+	 * - SHOULD set `onion_message_packet` `len` to 1366 or 32834.
 	 */
 	op = create_onionpacket(tmpctx, sphinx_path, ROUTING_INFO_SIZE,
 				&path_secrets);
