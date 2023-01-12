288f5df8d11f225f0e1996f9092e3d1b8848c84f
Rusty Russell <rusty@rustcorp.com.au>
2023-01-12 14:18:38 +1030

    ccan: update to fix recent gcc "comparison will always evaluate as 'false'" warning
    ```
    lightningd/jsonrpc.c: In function ‘destroy_json_command’:
    lightningd/jsonrpc.c:1180:63: error: the comparison will always evaluate as ‘false’ for the address of ‘canary’ will never be NULL [-Werror=address]
    lightningd/jsonrpc.c:108:53: note: ‘canary’ declared here
    ```
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- ccan/ccan/tcon/tcon.h
+++ ccan/ccan/tcon/tcon.h
@@ -147,8 +147,7 @@
  * It evaluates to @x so you can chain it.
  */
 #define tcon_check_ptr(x, canary, expr)				\
-	(sizeof(&(x)->_tcon[0].canary == (expr)) ? (x) : (x))
-
+	(sizeof((expr) ? (expr) : &(x)->_tcon[0].canary) ? (x) : (x))
 
 /**
  * tcon_type - the type within a container (or void *)
