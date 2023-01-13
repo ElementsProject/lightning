3736d452894c41ccc75a90fd4959462291b609c5
Christian Decker <decker.christian@gmail.com>
2023-01-13 15:00:15 +0100

    grpc: Allow conversion code to use deprecated fields
    The warning was rather superfluous, we should rather annotate the
    downstream structs so the developer gets a warning, not us library
    maintainers.

--- contrib/pyln-testing/pyln/testing/node_pb2_grpc.py
+++ contrib/pyln-testing/pyln/testing/node_pb2_grpc.py
@@ -239,6 +239,11 @@ class NodeStub(object):
                 request_serializer=node__pb2.SetchannelRequest.SerializeToString,
                 response_deserializer=node__pb2.SetchannelResponse.FromString,
                 )
+        self.SignInvoice = channel.unary_unary(
+                '/cln.Node/SignInvoice',
+                request_serializer=node__pb2.SigninvoiceRequest.SerializeToString,
+                response_deserializer=node__pb2.SigninvoiceResponse.FromString,
+                )
         self.SignMessage = channel.unary_unary(
                 '/cln.Node/SignMessage',
                 request_serializer=node__pb2.SignmessageRequest.SerializeToString,
@@ -524,6 +529,12 @@ class NodeServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def SignInvoice(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
     def SignMessage(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
@@ -763,6 +774,11 @@ def add_NodeServicer_to_server(servicer, server):
                     servicer.SetChannel,
                     request_deserializer=node__pb2.SetchannelRequest.FromString,
                     response_serializer=node__pb2.SetchannelResponse.SerializeToString,
+            ),
+            'SignInvoice': grpc.unary_unary_rpc_method_handler(
+                    servicer.SignInvoice,
+                    request_deserializer=node__pb2.SigninvoiceRequest.FromString,
+                    response_serializer=node__pb2.SigninvoiceResponse.SerializeToString,
             ),
             'SignMessage': grpc.unary_unary_rpc_method_handler(
                     servicer.SignMessage,
@@ -1546,6 +1562,23 @@ class Node(object):
         return grpc.experimental.unary_unary(request, target, '/cln.Node/SetChannel',
             node__pb2.SetchannelRequest.SerializeToString,
             node__pb2.SetchannelResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def SignInvoice(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/cln.Node/SignInvoice',
+            node__pb2.SigninvoiceRequest.SerializeToString,
+            node__pb2.SigninvoiceResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
