813401b2a6b811a5dfce08ec5d66565fea1fb4d7
Greg Sanders <gsanders87@gmail.com>
2023-02-07 12:27:39 -0500

    Update Bitcoin Core to 24.0.1 in other git ci locations

--- .github/scripts/install-bitcoind.sh
+++ .github/scripts/install-bitcoind.sh
@@ -4,13 +4,13 @@ set -e
 
 DIRNAME="bitcoin-${BITCOIN_VERSION}"
 EDIRNAME="elements-${ELEMENTS_VERSION}"
-FILENAME="${DIRNAME}-x86_64-linux-gnu.tar.bz2"
+FILENAME="${DIRNAME}-x86_64-linux-gnu.tar.gz"
 EFILENAME="${EDIRNAME}-x86_64-linux-gnu.tar.bz2"
 
 cd /tmp/
-wget "https://storage.googleapis.com/c-lightning-tests/$FILENAME"
+wget "https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/${FILENAME}"
 wget -q "https://storage.googleapis.com/c-lightning-tests/${EFILENAME}"
-tar -xaf "${FILENAME}"
+tar -xf "${FILENAME}"
 tar -xaf "${EFILENAME}"
 sudo mv "${DIRNAME}"/bin/* "/usr/local/bin"
 sudo mv "${EDIRNAME}"/bin/* "/usr/local/bin"
