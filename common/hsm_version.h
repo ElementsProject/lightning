fd04f46a921504ac8455e1244cbf5b2505b08897
Rusty Russell <rusty@rustcorp.com.au>
2023-03-14 15:45:50 +1030

    common/hsm_version: list sha256 for every known version.
    Makes it easier when we remove support for a version.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/hsm_version.h
+++ common/hsm_version.h
@@ -5,13 +5,11 @@
 /* We give a maximum and minimum compatibility version to HSM, to allow
  * some API adaptation. */
 
-/* wire/hsmd_wire.csv contents version:
- *    409cffa355ab6cc76bd298910adca9936a68223267ddc4815ba16aeac5d0acc3
+/* wire/hsmd_wire.csv contents by version:
+ * v1: 409cffa355ab6cc76bd298910adca9936a68223267ddc4815ba16aeac5d0acc3
+ * v2: dd89bf9323dff42200003fb864abb6608f3aa645b636fdae3ec81d804ac05196
+ * v3: edd3d288fc88a5470adc2f99abcbfe4d4af29fae0c7a80b4226f28810a815524
  */
 #define HSM_MIN_VERSION 1
-
-/* wire/hsmd_wire.csv contents version:
- *    edd3d288fc88a5470adc2f99abcbfe4d4af29fae0c7a80b4226f28810a815524
- */
 #define HSM_MAX_VERSION 3
 #endif /* LIGHTNING_COMMON_HSM_VERSION_H */
