355aa8f497a207475dc0fd60af4f203111225b3e
Rusty Russell <rusty@rustcorp.com.au>
2023-04-10 09:51:56 +0930

    zeroconf: don't accept channel_type with option_zeroconf unless we're really zeroconf.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Changelog-Fixed: Protocol: we will upfront reject channel_open which asks for a zeroconf channel unless we are going to do a zerconf channel.

--- openingd/dualopend.c
+++ openingd/dualopend.c
@@ -2284,7 +2284,8 @@ static void accepter_start(struct state *state, const u8 *oc2_msg)
 			channel_type_accept(state,
 					    open_tlv->channel_type,
 					    state->our_features,
-					    state->their_features);
+					    state->their_features,
+					    state->minimum_depth == 0);
 		if (!state->channel_type) {
 			negotiation_failed(state,
 					   "Did not support channel_type %s",
