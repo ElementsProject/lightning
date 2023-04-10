f20f77f66c4e07279a0b1d9d72df5bdd20bbcf9a
Christian Decker <decker.christian@gmail.com>
2023-04-11 06:57:45 +0930

    pyln: Exclude all `cln-` plugins from valgrind

--- contrib/pyln-testing/pyln/testing/utils.py
+++ contrib/pyln-testing/pyln/testing/utils.py
@@ -795,7 +795,7 @@ class LightningNode(object):
         if dsn is not None:
             self.daemon.opts['wallet'] = dsn
         if valgrind:
-            trace_skip_pattern = '*python*,*bitcoin-cli*,*elements-cli*,*cln-grpc'
+            trace_skip_pattern = '*python*,*bitcoin-cli*,*elements-cli*,*cln-*'
             if not valgrind_plugins:
                 trace_skip_pattern += ',*plugins*'
             self.daemon.cmd_prefix = [
