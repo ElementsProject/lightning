fc26675336eeee5ed22590f60087af8cfc0ccc63
Christian Decker <decker.christian@gmail.com>
2023-05-03 14:59:58 +0200

    msggen: Add DecodePay to the mappings
    Changelog-Added: grpc: Added mapping for `listpeerchannels`, `listclosedchannels`, `decode` and `decodepay` RPC method

--- contrib/pyln-testing/pyln/testing/node_pb2_grpc.py
+++ contrib/pyln-testing/pyln/testing/node_pb2_grpc.py
@@ -209,6 +209,16 @@ class NodeStub(object):
                 request_serializer=node__pb2.ListclosedchannelsRequest.SerializeToString,
                 response_deserializer=node__pb2.ListclosedchannelsResponse.FromString,
                 )
+        self.DecodePay = channel.unary_unary(
+                '/cln.Node/DecodePay',
+                request_serializer=node__pb2.DecodepayRequest.SerializeToString,
+                response_deserializer=node__pb2.DecodepayResponse.FromString,
+                )
+        self.Decode = channel.unary_unary(
+                '/cln.Node/Decode',
+                request_serializer=node__pb2.DecodeRequest.SerializeToString,
+                response_deserializer=node__pb2.DecodeResponse.FromString,
+                )
         self.Disconnect = channel.unary_unary(
                 '/cln.Node/Disconnect',
                 request_serializer=node__pb2.DisconnectRequest.SerializeToString,
@@ -508,6 +518,18 @@ class NodeServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def DecodePay(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def Decode(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
     def Disconnect(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
@@ -777,6 +799,16 @@ def add_NodeServicer_to_server(servicer, server):
                     servicer.ListClosedChannels,
                     request_deserializer=node__pb2.ListclosedchannelsRequest.FromString,
                     response_serializer=node__pb2.ListclosedchannelsResponse.SerializeToString,
+            ),
+            'DecodePay': grpc.unary_unary_rpc_method_handler(
+                    servicer.DecodePay,
+                    request_deserializer=node__pb2.DecodepayRequest.FromString,
+                    response_serializer=node__pb2.DecodepayResponse.SerializeToString,
+            ),
+            'Decode': grpc.unary_unary_rpc_method_handler(
+                    servicer.Decode,
+                    request_deserializer=node__pb2.DecodeRequest.FromString,
+                    response_serializer=node__pb2.DecodeResponse.SerializeToString,
             ),
             'Disconnect': grpc.unary_unary_rpc_method_handler(
                     servicer.Disconnect,
@@ -1508,6 +1540,40 @@ class Node(object):
         return grpc.experimental.unary_unary(request, target, '/cln.Node/ListClosedChannels',
             node__pb2.ListclosedchannelsRequest.SerializeToString,
             node__pb2.ListclosedchannelsResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def DecodePay(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/cln.Node/DecodePay',
+            node__pb2.DecodepayRequest.SerializeToString,
+            node__pb2.DecodepayResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def Decode(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/cln.Node/Decode',
+            node__pb2.DecodeRequest.SerializeToString,
+            node__pb2.DecodeResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
