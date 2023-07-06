ElementsProject/lightning

commit 64b1ddd761de30152154714433974b72bfb7f278

lightningd: clean up feerate handling, deprecate old terms.
Drop try_get_feerate() in favor of explicit feerate_for_deadline() and
smoothed_feerate_for_deadline().

This shows us everywhere we deal with old-style feerates by names.

`delayed_to_us` and `htlc_resolution` will be moving to dynamic fees,
so deprecate those.

Note that "penalty" is still used for generating penalty txs for
watchtowers, and "unilateral_close" still used until we get zero-fee
anchors.

Changelog-Added: JSON-RPC: `feerates` `estimates` array shows fee estimates by blockcount from underlying plugin (usually *bcli*).
Changelog-Changed: JSON-RPC: `close`, `fundchannel`, `fundpsbt`, `multifundchannel`, `multiwithdraw`, `txprepare`, `upgradewallet`, `withdraw` `feerate` (`feerange` for `close`) value *slow* is now 100 block-estimate, not half of 100-block estimate.
Changelog-Deprecated: JSON-RPC: `close`, `fundchannel`, `fundpsbt`, `multifundchannel`, `multiwithdraw`, `txprepare`, `upgradewallet`, `withdraw` `feerate` (`feerange` for `close`) expressed as, "delayed_to_us", "htlc_resolution", "max_acceptable" or "min_acceptable".  Use explicit block counts or *slow*/*normal*/*urgent*/*minimum*.
Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

lightningd/chaintopology.c
@@ -5,6 +5,7 @@
 #include <ccan/array_size/array_size.h>
 #include <ccan/io/io.h>
 #include <ccan/tal/str/str.h>
+#include <common/configdir.h>
 #include <common/htlc_tx.h>
 #include <common/json_command.h>
 #include <common/json_param.h>
@@ -545,34 +546,66 @@ static void start_fee_estimate(struct chain_topology *topo)
 	bitcoind_estimate_fees(topo->bitcoind, update_feerates);
 }
 
+struct rate_conversion {
+	u32 blockcount;
+};
+
+static struct rate_conversion conversions[] = {
+	[FEERATE_OPENING] = { 12 },
+	[FEERATE_MUTUAL_CLOSE] = { 100 },
+	[FEERATE_UNILATERAL_CLOSE] = { 6 },
+	[FEERATE_DELAYED_TO_US] = { 12 },
+	[FEERATE_HTLC_RESOLUTION] = { 6 },
+	[FEERATE_PENALTY] = { 12 },
+};
+
 u32 opening_feerate(struct chain_topology *topo)
 {
-	return try_get_feerate(topo, FEERATE_OPENING);
+	if (topo->ld->force_feerates)
+		return topo->ld->force_feerates[FEERATE_OPENING];
+	return feerate_for_deadline(topo,
+				    conversions[FEERATE_OPENING].blockcount);
 }
 
 u32 mutual_close_feerate(struct chain_topology *topo)
 {
-	return try_get_feerate(topo, FEERATE_MUTUAL_CLOSE);
+	if (topo->ld->force_feerates)
+		return topo->ld->force_feerates[FEERATE_MUTUAL_CLOSE];
+	return smoothed_feerate_for_deadline(topo,
+					     conversions[FEERATE_MUTUAL_CLOSE].blockcount);
 }
 
 u32 unilateral_feerate(struct chain_topology *topo)
 {
-	return try_get_feerate(topo, FEERATE_UNILATERAL_CLOSE);
+	if (topo->ld->force_feerates)
+		return topo->ld->force_feerates[FEERATE_UNILATERAL_CLOSE];
+	return smoothed_feerate_for_deadline(topo,
+					     conversions[FEERATE_UNILATERAL_CLOSE].blockcount)
+		* topo->ld->config.commit_fee_percent / 100;
 }
 
 u32 delayed_to_us_feerate(struct chain_topology *topo)
 {
-	return try_get_feerate(topo, FEERATE_DELAYED_TO_US);
+	if (topo->ld->force_feerates)
+		return topo->ld->force_feerates[FEERATE_DELAYED_TO_US];
+	return smoothed_feerate_for_deadline(topo,
+					     conversions[FEERATE_DELAYED_TO_US].blockcount);
 }
 
 u32 htlc_resolution_feerate(struct chain_topology *topo)
 {
-	return try_get_feerate(topo, FEERATE_HTLC_RESOLUTION);
+	if (topo->ld->force_feerates)
+		return topo->ld->force_feerates[FEERATE_HTLC_RESOLUTION];
+	return smoothed_feerate_for_deadline(topo,
+					     conversions[FEERATE_HTLC_RESOLUTION].blockcount);
 }
 
 u32 penalty_feerate(struct chain_topology *topo)
 {
-	return try_get_feerate(topo, FEERATE_PENALTY);
+	if (topo->ld->force_feerates)
+		return topo->ld->force_feerates[FEERATE_PENALTY];
+	return smoothed_feerate_for_deadline(topo,
+					     conversions[FEERATE_PENALTY].blockcount);
 }
 
 u32 get_feerate_floor(const struct chain_topology *topo)
@@ -588,39 +621,68 @@ static struct command_result *json_feerates(struct command *cmd,
 {
 	struct chain_topology *topo = cmd->ld->topology;
 	struct json_stream *response;
-	u32 feerates[NUM_FEERATES];
 	bool missing;
 	enum feerate_style *style;
+	u32 rate;
 
 	if (!param(cmd, buffer, params,
 		   p_req("style", param_feerate_style, &style),
 		   NULL))
 		return command_param_failed();
 
-	missing = false;
-	for (size_t i = 0; i < ARRAY_SIZE(feerates); i++) {
-		feerates[i] = try_get_feerate(topo, i);
-		if (!feerates[i])
-			missing = true;
-	}
+	missing = (tal_count(topo->feerates[0]) == 0);
 
 	response = json_stream_success(cmd);
-
 	if (missing)
 		json_add_string(response, "warning_missing_feerates",
 				"Some fee estimates unavailable: bitcoind startup?");
 
 	json_object_start(response, feerate_style_name(*style));
-	for (size_t i = 0; i < ARRAY_SIZE(feerates); i++) {
-		if (!feerates[i] || i == FEERATE_MIN || i == FEERATE_MAX)
-			continue;
-		json_add_num(response, feerate_name(i),
-			     feerate_to_style(feerates[i], *style));
+	rate = opening_feerate(topo);
+	if (rate)
+		json_add_num(response, "opening", feerate_to_style(rate, *style));
+	rate = mutual_close_feerate(topo);
+	if (rate)
+		json_add_num(response, "mutual_close",
+			     feerate_to_style(rate, *style));
+	rate = unilateral_feerate(topo);
+	if (rate)
+		json_add_num(response, "unilateral_close",
+			     feerate_to_style(rate, *style));
+	rate = penalty_feerate(topo);
+	if (rate)
+		json_add_num(response, "penalty",
+			     feerate_to_style(rate, *style));
+	if (deprecated_apis) {
+		rate = delayed_to_us_feerate(topo);
+		if (rate)
+			json_add_num(response, "delayed_to_us",
+				     feerate_to_style(rate, *style));
+		rate = htlc_resolution_feerate(topo);
+		if (rate)
+			json_add_num(response, "htlc_resolution",
+				     feerate_to_style(rate, *style));
 	}
+
 	json_add_u64(response, "min_acceptable",
 		     feerate_to_style(feerate_min(cmd->ld, NULL), *style));
 	json_add_u64(response, "max_acceptable",
 		     feerate_to_style(feerate_max(cmd->ld, NULL), *style));
+
+	json_array_start(response, "estimates");
+	assert(tal_count(topo->smoothed_feerates) == tal_count(topo->feerates[0]));
+	for (size_t i = 0; i < tal_count(topo->feerates[0]); i++) {
+		json_object_start(response, NULL);
+		json_add_num(response, "blockcount",
+			     topo->feerates[0][i].blockcount);
+		json_add_u64(response, "feerate",
+			     feerate_to_style(topo->feerates[0][i].rate, *style));
+		json_add_u64(response, "smoothed_feerate",
+			     feerate_to_style(topo->smoothed_feerates[i].rate,
+					      *style));
+		json_object_end(response);
+	}
+	json_array_end(response);
 	json_object_end(response);
 
 	if (!missing) {
@@ -998,62 +1060,9 @@ u32 get_network_blockheight(const struct chain_topology *topo)
 		return topo->headercount;
 }
 
-struct rate_conversion {
-	u32 blockcount;
-};
-
-static struct rate_conversion conversions[] = {
-	[FEERATE_OPENING] = { 12 },
-	[FEERATE_MUTUAL_CLOSE] = { 100 },
-	[FEERATE_UNILATERAL_CLOSE] = { 6 },
-	[FEERATE_DELAYED_TO_US] = { 12 },
-	[FEERATE_HTLC_RESOLUTION] = { 6 },
-	[FEERATE_PENALTY] = { 12 },
-};
-
-u32 try_get_feerate(const struct chain_topology *topo, enum feerate feerate)
-{
-	u32 val;
-
-	/* Max and min look over history as well. */
-	if (feerate == FEERATE_MAX) {
-		u32 max = 0;
-		for (size_t i = 0; i < ARRAY_SIZE(topo->feerates); i++) {
-			for (size_t j = 0; j < tal_count(topo->feerates[i]); j++) {
-				if (topo->feerates[i][j].rate > max)
-					max = topo->feerates[i][j].rate;
-			}
-		}
-		return max * topo->ld->config.max_fee_multiplier;
-	}
-
-	if (feerate == FEERATE_MIN) {
-		u32 min = 0xFFFFFFFF;
-		for (size_t i = 0; i < ARRAY_SIZE(topo->feerates); i++) {
-			for (size_t j = 0; j < tal_count(topo->feerates[i]); j++) {
-				if (topo->feerates[i][j].rate < min)
-					min = topo->feerates[i][j].rate;
-			}
-		}
-		if (min == 0xFFFFFFFF)
-			return 0;
-		/* FIXME: This is what bcli used to do: halve the slow feerate! */
-		min /= 2;
-		return min;
-	}
-
-	if (topo->ld->force_feerates)
-		val = topo->ld->force_feerates[feerate];
-	else
-		val = smoothed_feerate_for_deadline(topo, conversions[feerate].blockcount);
-	if (feerate == FEERATE_UNILATERAL_CLOSE)
-		val = val * topo->ld->config.commit_fee_percent / 100;
-
-	return val;
-}
-
 u32 feerate_min(struct lightningd *ld, bool *unknown)
 {
+	const struct chain_topology *topo = ld->topology;
 	u32 min;
 
 	if (unknown)
@@ -1063,21 +1072,32 @@ u32 feerate_min(struct lightningd *ld, bool *unknown)
 	if (ld->config.ignore_fee_limits)
 		min = 1;
 	else {
-		min = try_get_feerate(ld->topology, FEERATE_MIN);
-		if (!min) {
+		min = 0xFFFFFFFF;
+		for (size_t i = 0; i < ARRAY_SIZE(topo->feerates); i++) {
+			for (size_t j = 0; j < tal_count(topo->feerates[i]); j++) {
+				if (topo->feerates[i][j].rate < min)
+					min = topo->feerates[i][j].rate;
+			}
+		}
+		if (min == 0xFFFFFFFF) {
 			if (unknown)
 				*unknown = true;
+			min = 0;
 		}
+
+		/* FIXME: This is what bcli used to do: halve the slow feerate! */
+		min /= 2;
 	}
 
-	if (min < get_feerate_floor(ld->topology))
-		return get_feerate_floor(ld->topology);
+	if (min < get_feerate_floor(topo))
+		return get_feerate_floor(topo);
 	return min;
 }
 
 u32 feerate_max(struct lightningd *ld, bool *unknown)
 {
-	u32 feerate;
+	const struct chain_topology *topo = ld->topology;
+	u32 max = 0;
 
 	if (unknown)
 		*unknown = false;
@@ -1085,14 +1105,18 @@ u32 feerate_max(struct lightningd *ld, bool *unknown)
 	if (ld->config.ignore_fee_limits)
 		return UINT_MAX;
 
-	/* If we don't know feerate, don't limit other side. */
-	feerate = try_get_feerate(ld->topology, FEERATE_MAX);
-	if (!feerate) {
+	for (size_t i = 0; i < ARRAY_SIZE(topo->feerates); i++) {
+		for (size_t j = 0; j < tal_count(topo->feerates[i]); j++) {
+			if (topo->feerates[i][j].rate > max)
+				max = topo->feerates[i][j].rate;
+		}
+	}
+	if (!max) {
 		if (unknown)
 			*unknown = true;
 		return UINT_MAX;
 	}
-	return feerate;
+	return max * topo->ld->config.max_fee_multiplier;
 }
 
 /* On shutdown, channels get deleted last.  That frees from our list, so

cln-grpc/proto/node.proto
@@ -1130,6 +1130,7 @@ message FeeratesResponse {
 message FeeratesPerkb {
 	uint32 min_acceptable = 1;
 	uint32 max_acceptable = 2;
+	repeated FeeratesPerkbEstimates estimates = 9;
 	optional uint32 opening = 3;
 	optional uint32 mutual_close = 4;
 	optional uint32 unilateral_close = 5;
@@ -1138,15 +1139,28 @@ message FeeratesPerkb {
 	optional uint32 penalty = 8;
 }
 
+message FeeratesPerkbEstimates {
+	optional uint32 blockcount = 1;
+	optional uint32 feerate = 2;
+	optional uint32 smoothed_feerate = 3;
+}
+
 message FeeratesPerkw {
 	uint32 min_acceptable = 1;
 	uint32 max_acceptable = 2;
+	repeated FeeratesPerkwEstimates estimates = 9;
 	optional uint32 opening = 3;
 	optional uint32 mutual_close = 4;
 	optional uint32 unilateral_close = 5;
 	optional uint32 delayed_to_us = 6;
 	optional uint32 htlc_resolution = 7;
 	optional uint32 penalty = 8;
+}
+
+message FeeratesPerkwEstimates {
+	optional uint32 blockcount = 1;
+	optional uint32 feerate = 2;
+	optional uint32 smoothed_feerate = 3;
 }
 
 message FeeratesOnchain_fee_estimates {

cln-grpc/src/convert.rs
@@ -916,17 +916,42 @@ impl From<responses::DisconnectResponse> for pb::DisconnectResponse {
 }
 
 #[allow(unused_variables,deprecated)]
+impl From<responses::FeeratesPerkbEstimates> for pb::FeeratesPerkbEstimates {
+    fn from(c: responses::FeeratesPerkbEstimates) -> Self {
+        Self {
+            blockcount: c.blockcount, // Rule #2 for type u32?
+            feerate: c.feerate, // Rule #2 for type u32?
+            smoothed_feerate: c.smoothed_feerate, // Rule #2 for type u32?
+        }
+    }
+}
+
+#[allow(unused_variables,deprecated)]
 impl From<responses::FeeratesPerkb> for pb::FeeratesPerkb {
     fn from(c: responses::FeeratesPerkb) -> Self {
         Self {
             min_acceptable: c.min_acceptable, // Rule #2 for type u32
             max_acceptable: c.max_acceptable, // Rule #2 for type u32
+            estimates: c.estimates.map(|arr| arr.into_iter().map(|i| i.into()).collect()).unwrap_or(vec![]), // Rule #3
             opening: c.opening, // Rule #2 for type u32?
             mutual_close: c.mutual_close, // Rule #2 for type u32?
             unilateral_close: c.unilateral_close, // Rule #2 for type u32?
+            #[allow(deprecated)]
             delayed_to_us: c.delayed_to_us, // Rule #2 for type u32?
+            #[allow(deprecated)]
             htlc_resolution: c.htlc_resolution, // Rule #2 for type u32?
             penalty: c.penalty, // Rule #2 for type u32?
+        }
+    }
+}
+
+#[allow(unused_variables,deprecated)]
+impl From<responses::FeeratesPerkwEstimates> for pb::FeeratesPerkwEstimates {
+    fn from(c: responses::FeeratesPerkwEstimates) -> Self {
+        Self {
+            blockcount: c.blockcount, // Rule #2 for type u32?
+            feerate: c.feerate, // Rule #2 for type u32?
+            smoothed_feerate: c.smoothed_feerate, // Rule #2 for type u32?
         }
     }
 }
@@ -937,10 +962,13 @@ impl From<responses::FeeratesPerkw> for pb::FeeratesPerkw {
         Self {
             min_acceptable: c.min_acceptable, // Rule #2 for type u32
             max_acceptable: c.max_acceptable, // Rule #2 for type u32
+            estimates: c.estimates.map(|arr| arr.into_iter().map(|i| i.into()).collect()).unwrap_or(vec![]), // Rule #3
             opening: c.opening, // Rule #2 for type u32?
             mutual_close: c.mutual_close, // Rule #2 for type u32?
             unilateral_close: c.unilateral_close, // Rule #2 for type u32?
+            #[allow(deprecated)]
             delayed_to_us: c.delayed_to_us, // Rule #2 for type u32?
+            #[allow(deprecated)]
             htlc_resolution: c.htlc_resolution, // Rule #2 for type u32?
             penalty: c.penalty, // Rule #2 for type u32?
         }
@@ -3253,11 +3281,23 @@ impl From<pb::DisconnectResponse> for responses::DisconnectResponse {
 }
 
 #[allow(unused_variables,deprecated)]
+impl From<pb::FeeratesPerkbEstimates> for responses::FeeratesPerkbEstimates {
+    fn from(c: pb::FeeratesPerkbEstimates) -> Self {
+        Self {
+            blockcount: c.blockcount, // Rule #1 for type u32?
+            feerate: c.feerate, // Rule #1 for type u32?
+            smoothed_feerate: c.smoothed_feerate, // Rule #1 for type u32?
+        }
+    }
+}
+
+#[allow(unused_variables,deprecated)]
 impl From<pb::FeeratesPerkb> for responses::FeeratesPerkb {
     fn from(c: pb::FeeratesPerkb) -> Self {
         Self {
             min_acceptable: c.min_acceptable, // Rule #1 for type u32
             max_acceptable: c.max_acceptable, // Rule #1 for type u32
+            estimates: Some(c.estimates.into_iter().map(|s| s.into()).collect()), // Rule #4
             opening: c.opening, // Rule #1 for type u32?
             mutual_close: c.mutual_close, // Rule #1 for type u32?
             unilateral_close: c.unilateral_close, // Rule #1 for type u32?
@@ -3269,11 +3309,23 @@ impl From<pb::FeeratesPerkb> for responses::FeeratesPerkb {
 }
 
 #[allow(unused_variables,deprecated)]
+impl From<pb::FeeratesPerkwEstimates> for responses::FeeratesPerkwEstimates {
+    fn from(c: pb::FeeratesPerkwEstimates) -> Self {
+        Self {
+            blockcount: c.blockcount, // Rule #1 for type u32?
+            feerate: c.feerate, // Rule #1 for type u32?
+            smoothed_feerate: c.smoothed_feerate, // Rule #1 for type u32?
+        }
+    }
+}
+
+#[allow(unused_variables,deprecated)]
 impl From<pb::FeeratesPerkw> for responses::FeeratesPerkw {
     fn from(c: pb::FeeratesPerkw) -> Self {
         Self {
             min_acceptable: c.min_acceptable, // Rule #1 for type u32
             max_acceptable: c.max_acceptable, // Rule #1 for type u32
+            estimates: Some(c.estimates.into_iter().map(|s| s.into()).collect()), // Rule #4
             opening: c.opening, // Rule #1 for type u32?
             mutual_close: c.mutual_close, // Rule #1 for type u32?
             unilateral_close: c.unilateral_close, // Rule #1 for type u32?

cln-rpc/src/model.rs
@@ -3218,17 +3218,31 @@ pub mod responses {
 	}
 
 	#[derive(Clone, Debug, Deserialize, Serialize)]
+	pub struct FeeratesPerkbEstimates {
+	    #[serde(skip_serializing_if = "Option::is_none")]
+	    pub blockcount: Option<u32>,
+	    #[serde(skip_serializing_if = "Option::is_none")]
+	    pub feerate: Option<u32>,
+	    #[serde(skip_serializing_if = "Option::is_none")]
+	    pub smoothed_feerate: Option<u32>,
+	}
+
+	#[derive(Clone, Debug, Deserialize, Serialize)]
 	pub struct FeeratesPerkb {
 	    pub min_acceptable: u32,
 	    pub max_acceptable: u32,
+	    #[serde(skip_serializing_if = "crate::is_none_or_empty")]
+	    pub estimates: Option<Vec<FeeratesPerkbEstimates>>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub opening: Option<u32>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub mutual_close: Option<u32>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub unilateral_close: Option<u32>,
+	    #[deprecated]
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub delayed_to_us: Option<u32>,
+	    #[deprecated]
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub htlc_resolution: Option<u32>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
@@ -3236,17 +3250,31 @@ pub mod responses {
 	}
 
 	#[derive(Clone, Debug, Deserialize, Serialize)]
+	pub struct FeeratesPerkwEstimates {
+	    #[serde(skip_serializing_if = "Option::is_none")]
+	    pub blockcount: Option<u32>,
+	    #[serde(skip_serializing_if = "Option::is_none")]
+	    pub feerate: Option<u32>,
+	    #[serde(skip_serializing_if = "Option::is_none")]
+	    pub smoothed_feerate: Option<u32>,
+	}
+
+	#[derive(Clone, Debug, Deserialize, Serialize)]
 	pub struct FeeratesPerkw {
 	    pub min_acceptable: u32,
 	    pub max_acceptable: u32,
+	    #[serde(skip_serializing_if = "crate::is_none_or_empty")]
+	    pub estimates: Option<Vec<FeeratesPerkwEstimates>>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub opening: Option<u32>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub mutual_close: Option<u32>,
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub unilateral_close: Option<u32>,
+	    #[deprecated]
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub delayed_to_us: Option<u32>,
+	    #[deprecated]
 	    #[serde(skip_serializing_if = "Option::is_none")]
 	    pub htlc_resolution: Option<u32>,
 	    #[serde(skip_serializing_if = "Option::is_none")]

common/jsonrpc_errors.h
@@ -71,6 +71,7 @@ enum jsonrpc_errcode {
 
 	/* bitcoin-cli plugin errors */
 	BCLI_ERROR = 500,
+	BCLI_NO_FEE_ESTIMATES = 501,
 
 	/* Errors from `invoice` or `delinvoice` commands */
 	INVOICE_LABEL_ALREADY_EXISTS = 900,

contrib/pyln-testing/pyln/testing/grpc2py.py
@@ -729,10 +729,19 @@ def disconnect2py(m):
     })
 
 
+def feerates_perkb_estimates2py(m):
+    return remove_default({
+        "blockcount": m.blockcount,  # PrimitiveField in generate_composite
+        "feerate": m.feerate,  # PrimitiveField in generate_composite
+        "smoothed_feerate": m.smoothed_feerate,  # PrimitiveField in generate_composite
+    })
+
+
 def feerates_perkb2py(m):
     return remove_default({
         "min_acceptable": m.min_acceptable,  # PrimitiveField in generate_composite
         "max_acceptable": m.max_acceptable,  # PrimitiveField in generate_composite
+        "estimates": [feerates_perkb_estimates2py(i) for i in m.estimates],  # ArrayField[composite] in generate_composite
         "opening": m.opening,  # PrimitiveField in generate_composite
         "mutual_close": m.mutual_close,  # PrimitiveField in generate_composite
         "unilateral_close": m.unilateral_close,  # PrimitiveField in generate_composite
@@ -742,10 +751,19 @@ def feerates_perkb2py(m):
     })
 
 
+def feerates_perkw_estimates2py(m):
+    return remove_default({
+        "blockcount": m.blockcount,  # PrimitiveField in generate_composite
+        "feerate": m.feerate,  # PrimitiveField in generate_composite
+        "smoothed_feerate": m.smoothed_feerate,  # PrimitiveField in generate_composite
+    })
+
+
 def feerates_perkw2py(m):
     return remove_default({
         "min_acceptable": m.min_acceptable,  # PrimitiveField in generate_composite
         "max_acceptable": m.max_acceptable,  # PrimitiveField in generate_composite
+        "estimates": [feerates_perkw_estimates2py(i) for i in m.estimates],  # ArrayField[composite] in generate_composite
         "opening": m.opening,  # PrimitiveField in generate_composite
         "mutual_close": m.mutual_close,  # PrimitiveField in generate_composite
         "unilateral_close": m.unilateral_close,  # PrimitiveField in generate_composite

lightningd/chaintopology.h
@@ -181,14 +181,12 @@ u32 get_network_blockheight(const struct chain_topology *topo);
 u32 feerate_for_deadline(const struct chain_topology *topo, u32 blockcount);
 u32 smoothed_feerate_for_deadline(const struct chain_topology *topo, u32 blockcount);
 
-/* Get fee rate in satoshi per kiloweight, or 0 if unavailable! */
-u32 try_get_feerate(const struct chain_topology *topo, enum feerate feerate);
-
 /* Get range of feerates to insist other side abide by for normal channels.
  * If we have to guess, sets *unknown to true, otherwise false. */
 u32 feerate_min(struct lightningd *ld, bool *unknown);
 u32 feerate_max(struct lightningd *ld, bool *unknown);
 
+/* These return 0 if unknown */
 u32 opening_feerate(struct chain_topology *topo);
 u32 mutual_close_feerate(struct chain_topology *topo);
 u32 unilateral_feerate(struct chain_topology *topo);

lightningd/channel_control.c
@@ -38,12 +38,12 @@ static void update_feerates(struct lightningd *ld, struct channel *channel)
 		  feerate,
 		  feerate_min(ld, NULL),
 		  feerate_max(ld, NULL),
-		  try_get_feerate(ld->topology, FEERATE_PENALTY));
+		  penalty_feerate(ld->topology));
 
 	msg = towire_channeld_feerates(NULL, feerate,
 				       feerate_min(ld, NULL),
 				       feerate_max(ld, NULL),
-				       try_get_feerate(ld->topology, FEERATE_PENALTY));
+				       penalty_feerate(ld->topology));
 	subd_send_msg(channel->owner, take(msg));
 }
 
@@ -736,7 +736,7 @@ bool peer_start_channeld(struct channel *channel,
 				       channel->fee_states,
 				       feerate_min(ld, NULL),
 				       feerate_max(ld, NULL),
-				       try_get_feerate(ld->topology, FEERATE_PENALTY),
+				       penalty_feerate(ld->topology),
 				       &channel->last_sig,
 				       &channel->channel_info.remote_fundingkey,
 				       &channel->channel_info.theirbase,
@@ -1146,8 +1146,7 @@ static struct command_result *json_dev_feerate(struct command *cmd,
 	msg = towire_channeld_feerates(NULL, *feerate,
 				       feerate_min(cmd->ld, NULL),
 				       feerate_max(cmd->ld, NULL),
-				       try_get_feerate(cmd->ld->topology,
-						       FEERATE_PENALTY));
+				       penalty_feerate(cmd->ld->topology));
 	subd_send_msg(channel->owner, take(msg));
 
 	response = json_stream_success(cmd);

commit af420f142cd6ab46ada36008bf85c5a0f8789428

This commit fixes a bug introduced in 64b1ddd761de30152154714433974b72bfb7f278 that does not ignore the min fee as specified by the user setting.
We explicitly allow the user to ignore the fee limits, although this comes with inherent risks.

By enabling this option, users are explicitly
I was aware of the potential dangers.
There are situations, such as the one described in [1], where it
becomes necessary to bypass the fee limits to resolve issues like a stuck channel.

BTW experimental-anchors should fix this.

[1] https://github.com/ElementsProject/lightning/issues/6362

Reported-by: @pabpas
Fixes: 64b1ddd761de30152154714433974b72bfb7f278
Link: https://github.com/ElementsProject/lightning/issues/6362
Changelog-Fixes: do not ignore the ignore-fee-limit option during
update_fee
Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

lightningd/chaintopology.c
@@ -1137,7 +1137,19 @@ u32 feerate_min(struct lightningd *ld, bool *unknown)
 	if (unknown)
 		*unknown = false;
 
-	/* We can't allow less than feerate_floor, since that won't relay */
+        /* We allow the user to ignore the fee limits,
+	 * although this comes with inherent risks.
+	 *
+	 * By enabling this option, users are explicitly
+	 * made aware of the potential dangers.
+	 * There are situations, such as the one described in [1],
+	 * where it becomes necessary to bypass the fee limits to resolve
+	 * issues like a stuck channel.
+	 *
+	 * BTW experimental-anchors feature provides a solution to this problem.
+	 *
+	 * [1] https://github.com/ElementsProject/lightning/issues/6362
+	 * */
 	if (ld->config.ignore_fee_limits)
 		min = 1;
 	else {
@@ -1156,10 +1168,12 @@ u32 feerate_min(struct lightningd *ld, bool *unknown)
 
 		/* FIXME: This is what bcli used to do: halve the slow feerate! */
 		min /= 2;
+
+		/* We can't allow less than feerate_floor, since that won't relay */
+		if (min < get_feerate_floor(topo))
+			return get_feerate_floor(topo);
 	}
 
-	if (min < get_feerate_floor(topo))
-		return get_feerate_floor(topo);
 	return min;
 }
 