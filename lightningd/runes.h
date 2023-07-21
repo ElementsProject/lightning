917708450548c81bad537c07ee55f990087a6cb9
Rusty Russell <rusty@rustcorp.com.au>
2023-07-21 09:53:26 +0930

    lightningd: new runes infrastructure.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ lightningd/runes.h
@@ -0,0 +1,7 @@
+#ifndef LIGHTNING_LIGHTNINGD_RUNES_H
+#define LIGHTNING_LIGHTNINGD_RUNES_H
+#include "config.h"
+
+struct runes *runes_init(struct lightningd *ld);
+
+#endif /* LIGHTNING_LIGHTNINGD_RUNES_H */
