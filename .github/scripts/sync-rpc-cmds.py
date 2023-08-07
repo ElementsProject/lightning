dcd512ed1e9ed72d45a7171cd15256fd5cffd26a
Adi Shankara <adishankara@tuta.io>
2023-08-07 18:30:57 +0400

    fix E123 error

--- .github/scripts/sync-rpc-cmds.py
+++ .github/scripts/sync-rpc-cmds.py
@@ -14,41 +14,41 @@ def checkIfDocIsPresent(title, headers):
     check_url = URL + "/" + title
     response = requests.get(check_url, headers=headers)
 
-    if (response.status_code == 200):
+    if response.status_code == 200:
         return True
     else:
         return False
 
 
 def publishDoc(title, body, order):
-    key = os.environ.get('README_API_KEY')
+    key = os.environ.get("README_API_KEY")
     payload = {
         "title": title,
         "type": "basic",
         "body": body,
         "category": CATEGORY_ID,
         "hidden": False,
-        "order": order
+        "order": order,
     }
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "authorization": "Basic " + key
+        "authorization": "Basic " + key,
     }
 
     isDocPresent = checkIfDocIsPresent(title, headers)
-    if (isDocPresent):
+    if isDocPresent:
         # update doc
         update_url = URL + "/" + title  # title == slug
         response = requests.put(update_url, json=payload, headers=headers)
-        if (response.status_code != 200):
+        if response.status_code != 200:
             print(response.text)
         else:
             print("Updated ", title)
     else:
         # create doc
         response = requests.post(URL, json=payload, headers=headers)
-        if (response.status_code != 201):
+        if response.status_code != 201:
             print(response.text)
         else:
             print("Created ", title)
@@ -56,22 +56,21 @@ def publishDoc(title, body, order):
 
 def extract_rpc_commands(rst_content):
     manpages_block = re.search(
-            r"\.\. block_start manpages(.*?)"
-            r"\.\. block_end manpages",
-            rst_content, re.DOTALL)
+        r"\.\. block_start manpages(.*?)" r"\.\. block_end manpages",
+        rst_content,
+        re.DOTALL,
+    )
     if manpages_block:
         commands = re.findall(
-                r"\b([a-zA-Z0-9_-]+)"
-                r"\s+<([^>]+)>\n",
-                manpages_block.group(1)
-                )
+            r"\b([a-zA-Z0-9_-]+)" r"\s+<([^>]+)>\n", manpages_block.group(1)
+        )
         return commands
     return []
 
 
 def main():
     # path to the rst file from where we fetch all the RPC commands
-    path_to_rst = 'doc/index.rst'
+    path_to_rst = "doc/index.rst"
     with open(path_to_rst, "r") as file:
         rst_content = file.read()
 
