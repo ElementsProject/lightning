9a74900c3f5d44a66526a70dcce6867f25ed3b97
Matt Morehouse <mattmorehouse@gmail.com>
2023-08-21 15:51:18 -0500

    fuzz: target for ping
    Fuzz the decoding and encoding of ping.

---
+++ tests/fuzz/fuzz-wire-ping.c
@@ -0,0 +1,37 @@
+#include "config.h"
+#include <ccan/mem/mem.h>
+#include <tests/fuzz/libfuzz.h>
+#include <tests/fuzz/wire.h>
+#include <wire/peer_wire.h>
+
+struct ping {
+	u16 num_pong_bytes;
+	u8 *ignored;
+};
+
+static void *encode(const tal_t *ctx, const struct ping *s)
+{
+	return towire_ping(ctx, s->num_pong_bytes, s->ignored);
+}
+
+static struct ping *decode(const tal_t *ctx, const void *p)
+{
+	struct ping *s = tal(ctx, struct ping);
+
+	if (fromwire_ping(s, p, &s->num_pong_bytes, &s->ignored))
+		return s;
+	return tal_free(s);
+}
+
+static bool equal(const struct ping *x, const struct ping *y)
+{
+	if (x->num_pong_bytes != y->num_pong_bytes)
+		return false;
+	return memeq(x->ignored, tal_bytelen(x->ignored), y->ignored,
+		     tal_bytelen(y->ignored));
+}
+
+void run(const u8 *data, size_t size)
+{
+	test_decode_encode(data, size, WIRE_PING, struct ping);
+}
