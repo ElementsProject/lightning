bfea9ebde46f154a5b42c37c2357b24bf06fd6a5
Matt Morehouse <mattmorehouse@gmail.com>
2023-08-22 16:27:50 -0500

    fuzz: target for splice_locked
    Fuzz the decoding and encoding of splice_locked.

---
+++ tests/fuzz/fuzz-wire-splice_locked.c
@@ -0,0 +1,33 @@
+#include "config.h"
+#include <tests/fuzz/libfuzz.h>
+#include <tests/fuzz/wire.h>
+#include <wire/peer_wire.h>
+
+struct splice_locked {
+	struct channel_id channel_id;
+};
+
+static void *encode(const tal_t *ctx, const struct splice_locked *s)
+{
+	return towire_splice_locked(ctx, &s->channel_id);
+}
+
+static struct splice_locked *decode(const tal_t *ctx, const void *p)
+{
+	struct splice_locked *s = tal(ctx, struct splice_locked);
+
+	if (fromwire_splice_locked(p, &s->channel_id))
+		return s;
+	return tal_free(s);
+}
+
+static bool equal(const struct splice_locked *x, const struct splice_locked *y)
+{
+	return memcmp(x, y, sizeof(*x)) == 0;
+}
+
+void run(const u8 *data, size_t size)
+{
+	test_decode_encode(data, size, WIRE_SPLICE_LOCKED,
+			   struct splice_locked);
+}
