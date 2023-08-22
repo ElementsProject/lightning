3850dce5a81eb4766d9bc98447f8c78572b2cb0d
Matt Morehouse <mattmorehouse@gmail.com>
2023-08-22 16:20:21 -0500

    fuzz: target for peer_storage
    Fuzz the decoding and encoding of peer_storage.

---
+++ tests/fuzz/fuzz-wire-peer_storage.c
@@ -0,0 +1,34 @@
+#include "config.h"
+#include <ccan/mem/mem.h>
+#include <tests/fuzz/libfuzz.h>
+#include <tests/fuzz/wire.h>
+#include <wire/peer_wire.h>
+
+struct peer_storage {
+	u8 *blob;
+};
+
+static void *encode(const tal_t *ctx, const struct peer_storage *s)
+{
+	return towire_peer_storage(ctx, s->blob);
+}
+
+static struct peer_storage *decode(const tal_t *ctx, const void *p)
+{
+	struct peer_storage *s = tal(ctx, struct peer_storage);
+
+	if (fromwire_peer_storage(s, p, &s->blob))
+		return s;
+	return tal_free(s);
+}
+
+static bool equal(const struct peer_storage *x, const struct peer_storage *y)
+{
+	return memeq(x->blob, tal_bytelen(x->blob), y->blob,
+		     tal_bytelen(y->blob));
+}
+
+void run(const u8 *data, size_t size)
+{
+	test_decode_encode(data, size, WIRE_PEER_STORAGE, struct peer_storage);
+}
