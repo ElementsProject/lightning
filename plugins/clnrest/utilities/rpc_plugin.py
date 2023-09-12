632e7abd05852097f95f7dd8ad0bfb829a09cb2b
ShahanaFarooqui <shahana.farooqui@gmail.com>
2023-09-11 21:27:13 -0700

    plugins/clnrest: Add `rest-cors-origins` option for config

--- plugins/clnrest/utilities/rpc_plugin.py
+++ plugins/clnrest/utilities/rpc_plugin.py
@@ -7,3 +7,4 @@ plugin.add_option(name="rest-certs", default=os.getcwd(), description="Path for 
 plugin.add_option(name="rest-protocol", default="https", description="REST server protocol", opt_type="string", deprecated=False)
 plugin.add_option(name="rest-host", default="127.0.0.1", description="REST server host", opt_type="string", deprecated=False)
 plugin.add_option(name="rest-port", default=None, description="REST server port to listen", opt_type="int", deprecated=False)
+plugin.add_option(name="rest-cors-origins", default="*", description="Cross origin resource sharing origins", opt_type="string", deprecated=False, multi=True)
