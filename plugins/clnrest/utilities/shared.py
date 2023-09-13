51578e1f773706c6b552d9b0192e0919184c8c29
ShahanaFarooqui <shahana.farooqui@gmail.com>
2023-09-12 21:53:08 -0700

    plugins/clnrest: Add `rest-csp` option for config

--- plugins/clnrest/utilities/shared.py
+++ plugins/clnrest/utilities/shared.py
@@ -2,17 +2,18 @@ import json5
 import re
 import json
 
-CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT, REST_CORS_ORIGINS = "", "", "", "", []
+CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT, REST_CSP, REST_CORS_ORIGINS = "", "", "", "", "", []
 
 
 def set_config(options):
     if 'rest-port' not in options:
         return "`rest-port` option is not configured"
-    global CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT, REST_CORS_ORIGINS
+    global CERTS_PATH, REST_PROTOCOL, REST_HOST, REST_PORT, REST_CSP, REST_CORS_ORIGINS
     CERTS_PATH = str(options["rest-certs"])
     REST_PROTOCOL = str(options["rest-protocol"])
     REST_HOST = str(options["rest-host"])
     REST_PORT = int(options["rest-port"])
+    REST_CSP = str(options["rest-csp"])
     cors_origins = options["rest-cors-origins"]
     REST_CORS_ORIGINS.clear()
     for origin in cors_origins:
