48026194ebecb8feabedbee69a7e4b9e068850ca
Christian Decker <decker.christian@gmail.com>
2023-09-13 18:36:17 +0200

    gci: Only install one of elementsd or bitcoind
    We were accidentally testing against `bitcoind` rather than
    `elementsd` which meant we believed everything was find, when in
    reality some tests broke. By only installing the required daemon we
    ensure that such a mixup causes a CI failure.

--- .github/scripts/install-bitcoind.sh
+++ .github/scripts/install-bitcoind.sh
@@ -8,12 +8,21 @@ FILENAME="${DIRNAME}-x86_64-linux-gnu.tar.gz"
 EFILENAME="${EDIRNAME}-x86_64-linux-gnu.tar.gz"
 
 cd /tmp/
-wget "https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/${FILENAME}"
-wget "https://github.com/ElementsProject/elements/releases/download/elements-${ELEMENTS_VERSION}/${EFILENAME}"
-tar -xf "${FILENAME}"
-tar -xf "${EFILENAME}"
-sudo mv "${DIRNAME}"/bin/* "/usr/local/bin"
-sudo mv "${EDIRNAME}"/bin/* "/usr/local/bin"
 
+# Since we inadvertently broke `elementsd` support in the past we only
+# want to download and enable the daemon that is actually going to be
+# used when running in CI. Otherwise we could end up accidentally
+# testing against `bitcoind` but still believe that we ran against
+# `elementsd`.
+if [ "$TEST_NETWORK" = "liquid-regtest" ]; then
+    wget "https://github.com/ElementsProject/elements/releases/download/elements-${ELEMENTS_VERSION}/${EFILENAME}"
+    tar -xf "${EFILENAME}"
+    sudo mv "${EDIRNAME}"/bin/* "/usr/local/bin"
+    rm -rf "${EFILENAME}" "${EDIRNAME}"
+else
+    wget "https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/${FILENAME}"
+    tar -xf "${FILENAME}"
+    sudo mv "${DIRNAME}"/bin/* "/usr/local/bin"
+    rm -rf "${FILENAME}" "${DIRNAME}"
+fi
 
-rm -rf "${FILENAME}" "${EFILENAME}" "${DIRNAME}" "${EDIRNAME}"
