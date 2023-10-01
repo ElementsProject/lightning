bd5e8d29752ec1172fcf9b1a2fadaaedc28d8efa
Rusty Russell <rusty@rustcorp.com.au>
2023-10-02 09:29:52 +1030

    lightningd: ensure we *always* watch channel spend.
    Now we're not always using the same functions to watch during
    dual-funding opening, we need to make sure we're watching the close
    (in particular, df close before the opening is confirmed).
    
    So, keep a pointer, and if it's not set in drop_to_chain, set it.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/channel.c
+++ lightningd/channel.c
@@ -234,6 +234,7 @@ struct channel *new_unsaved_channel(struct peer *peer,
 	channel->next_index[LOCAL] = 1;
 	channel->next_index[REMOTE] = 1;
 	channel->next_htlc_id = 0;
+	channel->funding_spend_watch = NULL;
 	/* FIXME: remove push when v1 deprecated */
 	channel->push = AMOUNT_MSAT(0);
 	channel->closing_fee_negotiation_step = 50;
@@ -465,6 +466,7 @@ struct channel *new_channel(struct peer *peer, u64 dbid,
 	channel->next_htlc_id = next_htlc_id;
 	channel->funding = *funding;
 	channel->funding_sats = funding_sats;
+	channel->funding_spend_watch = NULL;
 	channel->push = push;
 	channel->our_funds = our_funds;
 	channel->remote_channel_ready = remote_channel_ready;
