6802a44d24b92dbcc920a5e71de20b3e461b8476
Peter Neuroth <pet.v.ne@gmail.com>
2023-10-23 16:13:10 +0200

    grpc: Regenerate proto stubs
    Hopefully this will fix the ci.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- contrib/pyln-grpc-proto/pyln/grpc/primitives_pb2.py
+++ contrib/pyln-grpc-proto/pyln/grpc/primitives_pb2.py
@@ -19,7 +19,6 @@ _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'primitives_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
   _globals['_CHANNELSIDE']._serialized_start=718
   _globals['_CHANNELSIDE']._serialized_end=754
