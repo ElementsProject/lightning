d89d4d5a3cce38c74a62c26cae782051805b101d
Peter Neuroth <pet.v.ne@gmail.com>
2023-10-23 16:13:10 +0200

    grpc: Regenerate proto stubs
    Hopefully this will fix the ci.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- contrib/pyln-grpc-proto/pyln/grpc/node_pb2.py
+++ contrib/pyln-grpc-proto/pyln/grpc/node_pb2.py
@@ -20,7 +20,6 @@ _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'node_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
   _globals['_GETINFOREQUEST']._serialized_start=37
   _globals['_GETINFOREQUEST']._serialized_end=53
