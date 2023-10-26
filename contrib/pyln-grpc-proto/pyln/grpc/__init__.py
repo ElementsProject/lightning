faec61bf4f0c0c4035b6f641ffbfe8b21268347e
Christian Decker <decker.christian@gmail.com>
2023-10-26 13:05:17 +0200

    pyln: Add missing upgrade-version target to pyln-grpc

--- contrib/pyln-grpc-proto/pyln/grpc/__init__.py
+++ contrib/pyln-grpc-proto/pyln/grpc/__init__.py
@@ -1,3 +1,4 @@
 from pyln.grpc.primitives_pb2 import *  # noqa: F401,F403
 from pyln.grpc.node_pb2 import *  # noqa: F401,F403
 from pyln.grpc.node_pb2_grpc import *  # noqa: F401,F403
+__version__ = "0.1.1"
