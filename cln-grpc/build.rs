88ee4d3b291585f8c98a50d23eb5e5147642f165
ok300 <106775972+ok300@users.noreply.github.com>
2024-01-25 18:47:35 +0100

    cln-grpc: Derive serde Serialize/Deserialize for types

--- cln-grpc/build.rs
+++ cln-grpc/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     let builder = tonic_build::configure();
     builder
+        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["proto/node.proto"], &["proto"])
         .unwrap();
