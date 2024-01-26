140e33b13a422411a16d6bc3fc131e2f4bac5cea
Alex Myers <alex@endothermic.dev>
2024-01-26 12:32:47 -0600

    pytest: add tests for pip and poetry installed virtual envs
    Also test checkout of a git tag on installation.

--- tests/data/recklessrepo/lightningd/testplugpyproj/testplugpyproj.py
+++ tests/data/recklessrepo/lightningd/testplugpyproj/testplugpyproj.py
@@ -3,8 +3,6 @@ from pyln.client import Plugin
 
 plugin = Plugin()
 
-__version__ = 'v1'
-
 
 @plugin.init()
 def init(options, configuration, plugin, **kwargs):
@@ -14,12 +12,6 @@ def init(options, configuration, plugin, **kwargs):
 @plugin.method("testmethod")
 def testmethod(plugin):
     return ("I live.")
-
-
-@plugin.method("gettestplugversion")
-def gettestplugversion(plugin):
-    "to test commit/tag checkout"
-    return __version__
 
 
 plugin.run()
