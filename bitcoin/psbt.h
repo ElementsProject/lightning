292b0903e83d0b0e1a500324e53452b21ab6fd94
Dusty Daemon <dustinpaystaxes@gmail.com>
2024-02-13 18:12:06 -0500

    splice: Update splice fee maximums
    The fee maximum is used to issue a warning to the user their feerate is high in case they accidentally make a large donation to miners.
    
    During python testing the feerates are high on purpose so we raise the warning level to at least the penality feerate.

--- bitcoin/psbt.h
+++ bitcoin/psbt.h
@@ -188,7 +188,7 @@ WARN_UNUSED_RESULT bool psbt_input_have_signature(const struct wally_psbt *psbt,
 
 /* Returns false on error. On success *sig is set to the signature otherwise
  * *sig is set to NULL. */
-WARN_UNUSED_RESULT bool psbt_input_get_signature(const tal_t *ctx,
+WARN_UNUSED_RESULT bool psbt_input_get_ecdsa_sig(const tal_t *ctx,
 						 const struct wally_psbt *psbt,
 						 size_t in,
 						 const struct pubkey *pubkey,
