458cc4bd3ffbdf01a0dae592567efc180a0ea644
Lagrang3 <lagrang3@protonmail.com>
2024-02-29 15:52:13 +0100

    renepay: preferred variable name payment over p

--- plugins/renepay/mods.c
+++ plugins/renepay/mods.c
@@ -8,14 +8,14 @@
 
 #define INVALID_ID UINT32_MAX
 
-void payment_continue(struct payment *p)
+void payment_continue(struct payment *payment)
 {
-	const struct payment_modifier *mod = payment_modifier_pop(p);
+	const struct payment_modifier *mod = payment_modifier_pop(payment);
 	if (mod != NULL) {
 		/* There is another modifier, so call it. */
 		plugin_log(pay_plugin->plugin, LOG_DBG, "Calling modifier %s",
 			   mod->name);
-		return mod->post_step_cb(p);
+		return mod->post_step_cb(payment);
 	}
 	plugin_err(pay_plugin->plugin,
 		   "Finished all modifiers but we have not come to conclusion");
@@ -25,7 +25,7 @@ void payment_continue(struct payment *p)
 static struct command_result *payment_rpc_failure(struct command *cmd,
 						  const char *buffer,
 						  const jsmntok_t *toks,
-						  struct payment *p)
+						  struct payment *payment)
 {
 	const jsmntok_t *codetok = json_get_member(buffer, toks, "code");
 	u32 errcode;
@@ -36,10 +36,10 @@ static struct command_result *payment_rpc_failure(struct command *cmd,
 
 	// TODO flag a payment as failed
 	// payment_set_fail(
-	//     p, errcode,
+	//     payment, errcode,
 	//     "Failing a partial payment due to a failed RPC call: %.*s",
 	//     json_tok_full_len(toks), json_tok_full(buffer, toks));
-	return payment_finish(p);
+	return payment_finish(payment);
 }
 
 /*****************************************************************************
@@ -54,7 +54,7 @@ static struct command_result *payment_rpc_failure(struct command *cmd,
 static struct command_result *listsendpays_ok(struct command *cmd,
 					      const char *buf,
 					      const jsmntok_t *result,
-					      struct payment *p)
+					      struct payment *payment)
 {
 	size_t i;
 	const jsmntok_t *t, *arr;
@@ -79,10 +79,10 @@ static struct command_result *listsendpays_ok(struct command *cmd,
 	if (!arr || arr->type != JSMN_ARRAY) {
 		// TODO
 		// payment_set_fail(
-		//     p, LIGHTNINGD,
+		//     payment, LIGHTNINGD,
 		//     "Unexpected non-array result from listsendpays: %.*s",
 		//     json_tok_full_len(result), json_tok_full(buf, result));
-		return payment_finish(p);
+		return payment_finish(payment);
 	}
 
 	json_for_each_arr(i, t, arr)
@@ -123,10 +123,10 @@ static struct command_result *listsendpays_ok(struct command *cmd,
 			if (complete_groupid != INVALID_ID &&
 			    groupid != complete_groupid) {
 				// TODO
-				// payment_set_fail(p, PAY_STATUS_UNEXPECTED,
+				// payment_set_fail(payment, PAY_STATUS_UNEXPECTED,
 				// 		 "Multiple complete groupids "
 				// 		 "for this payment?");
-				return payment_finish(p);
+				return payment_finish(payment);
 			}
 			complete_groupid = groupid;
 			/* Now we know the payment completed. */
@@ -152,10 +152,10 @@ static struct command_result *listsendpays_ok(struct command *cmd,
 			if (pending_group_id != INVALID_ID &&
 			    groupid != pending_group_id) {
 				// TODO
-				// payment_set_fail(p, PAY_STATUS_UNEXPECTED,
+				// payment_set_fail(payment, PAY_STATUS_UNEXPECTED,
 				// 		 "Multiple pending groups for "
 				// 		 "this payment?");
-				return payment_finish(p);
+				return payment_finish(payment);
 			}
 			pending_group_id = groupid;
 			if (partid > max_pending_partid)
@@ -176,25 +176,26 @@ static struct command_result *listsendpays_ok(struct command *cmd,
 	if (complete_groupid != INVALID_ID) {
 		/* There are completed sendpays, we don't need to do anything
 		 * but summarize the result. */
-		p->status = PAYMENT_SUCCESS;
-		p->start_time.ts.tv_sec = complete_created_at;
-		p->start_time.ts.tv_nsec = 0;
+		payment->status = PAYMENT_SUCCESS;
+		payment->start_time.ts.tv_sec = complete_created_at;
+		payment->start_time.ts.tv_nsec = 0;
 
-		p->total_delivering = complete_msat;
-		p->total_sent = complete_sent;
-		p->next_partid = complete_parts + 1;
-		p->groupid = complete_groupid;
-		p->preimage = tal_dup(p, struct preimage, &complete_preimage);
+		payment->total_delivering = complete_msat;
+		payment->total_sent = complete_sent;
+		payment->next_partid = complete_parts + 1;
+		payment->groupid = complete_groupid;
+		payment->preimage =
+		    tal_dup(payment, struct preimage, &complete_preimage);
 
-		payment_note(p, LOG_DBG,
+		payment_note(payment, LOG_DBG,
 			     "Payment completed by a previous sendpay.");
-		return payment_finish(p);
+		return payment_finish(payment);
 	} else if (pending_group_id != INVALID_ID) {
 		/* Continue where we left off? */
-		p->groupid = pending_group_id;
-		p->next_partid = max_pending_partid + 1;
-		p->total_sent = pending_sent;
-		p->total_delivering = pending_msat;
+		payment->groupid = pending_group_id;
+		payment->next_partid = max_pending_partid + 1;
+		payment->total_sent = pending_sent;
+		payment->total_delivering = pending_msat;
 
 		plugin_log(pay_plugin->plugin, LOG_DBG,
 			   "There are pending sendpays to this invoice. "
@@ -203,39 +204,40 @@ static struct command_result *listsendpays_ok(struct command *cmd,
 			   "last_partid = %" PRIu32,
 			   pending_group_id,
 			   type_to_string(tmpctx, struct amount_msat,
-					  &p->total_delivering),
+					  &payment->total_delivering),
 			   max_pending_partid);
 
-		if (amount_msat_greater_eq(p->total_delivering, p->amount)) {
+		if (amount_msat_greater_eq(payment->total_delivering,
+					   payment->amount)) {
 			/* Pending payment already pays the full amount, we
 			 * better stop. */
 			// TODO
-			// payment_set_fail(p, PAY_IN_PROGRESS,
+			// payment_set_fail(payment, PAY_IN_PROGRESS,
 			// 		 "Payment is pending with full amount "
 			// 		 "already commited");
-			return payment_finish(p);
+			return payment_finish(payment);
 		}
 	} else {
 		/* There are no pending nor completed sendpays, get me the last
 		 * sendpay group. */
-		p->groupid = max_group_id + 1;
-		p->next_partid = 1;
+		payment->groupid = max_group_id + 1;
+		payment->next_partid = 1;
 	}
 
-	payment_continue(p);
+	payment_continue(payment);
 	return command_still_pending(cmd);
 }
 
-static void previous_sendpays_cb(struct payment *p)
+static void previous_sendpays_cb(struct payment *payment)
 {
-	struct command *cmd = payment_command(p);
+	struct command *cmd = payment_command(payment);
 	assert(cmd);
 
-	struct out_req *req =
-	    jsonrpc_request_start(cmd->plugin, cmd, "listsendpays",
-				  listsendpays_ok, payment_rpc_failure, p);
+	struct out_req *req = jsonrpc_request_start(
+	    cmd->plugin, cmd, "listsendpays", listsendpays_ok,
+	    payment_rpc_failure, payment);
 
-	json_add_sha256(req->js, "payment_hash", &p->payment_hash);
+	json_add_sha256(req->js, "payment_hash", &payment->payment_hash);
 	send_outreq(cmd->plugin, req);
 }
 
@@ -246,14 +248,14 @@ REGISTER_PAYMENT_MODIFIER(previous_sendpays, previous_sendpays_cb);
  *
  * Some checks on a payment about to start.
  */
-static void initial_sanity_checks_cb(struct payment *p)
+static void initial_sanity_checks_cb(struct payment *payment)
 {
-	assert(amount_msat_zero(p->total_sent));
-	assert(amount_msat_zero(p->total_delivering));
-	assert(!p->preimage);
-	assert(tal_count(p->cmd_array) == 1);
+	assert(amount_msat_zero(payment->total_sent));
+	assert(amount_msat_zero(payment->total_delivering));
+	assert(!payment->preimage);
+	assert(tal_count(payment->cmd_array) == 1);
 
-	payment_continue(p);
+	payment_continue(payment);
 }
 
 REGISTER_PAYMENT_MODIFIER(initial_sanity_checks, initial_sanity_checks_cb);
@@ -268,7 +270,7 @@ REGISTER_PAYMENT_MODIFIER(initial_sanity_checks, initial_sanity_checks_cb);
 static struct command_result *selfpay_success(struct command *cmd,
 					      const char *buf,
 					      const jsmntok_t *result,
-					      struct payment *p)
+					      struct payment *payment)
 {
 	struct preimage preimage;
 	const char *err;
@@ -279,46 +281,48 @@ static struct command_result *selfpay_success(struct command *cmd,
 		    cmd->plugin, "selfpay didn't have payment_preimage? %.*s",
 		    json_tok_full_len(result), json_tok_full(buf, result));
 
-	p->preimage = tal_dup(p, struct preimage, &preimage);
-	p->status = PAYMENT_SUCCESS;
-	payment_note(p, LOG_DBG, "Paid with self-pay.");
-	return payment_finish(p);
+	payment->preimage = tal_dup(payment, struct preimage, &preimage);
+	payment->status = PAYMENT_SUCCESS;
+	payment_note(payment, LOG_DBG, "Paid with self-pay.");
+	return payment_finish(payment);
 }
 
-static void selfpay_cb(struct payment *p)
+static void selfpay_cb(struct payment *payment)
 {
-	if (!node_id_eq(&pay_plugin->my_id, &p->destination)) {
-		payment_continue(p);
+	if (!node_id_eq(&pay_plugin->my_id, &payment->destination)) {
+		payment_continue(payment);
 		return;
 	}
 
-	struct command *cmd = payment_command(p);
+	struct command *cmd = payment_command(payment);
 	if (!cmd)
 		plugin_err(pay_plugin->plugin,
 			   "Selfpay: cannot get a valid cmd.");
 	struct out_req *req;
-	req = jsonrpc_request_start(cmd->plugin, cmd, "sendpay",
-				    selfpay_success, payment_rpc_failure, p);
+	req =
+	    jsonrpc_request_start(cmd->plugin, cmd, "sendpay", selfpay_success,
+				  payment_rpc_failure, payment);
 	/* Empty route means "to-self" */
 	json_array_start(req->js, "route");
 	json_array_end(req->js);
-	json_add_sha256(req->js, "payment_hash", &p->payment_hash);
-	if (p->label)
-		json_add_string(req->js, "label", p->label);
-	json_add_amount_msat(req->js, "amount_msat", p->amount);
-	json_add_string(req->js, "bolt11", p->invstr);
-	if (p->payment_secret)
-		json_add_secret(req->js, "payment_secret", p->payment_secret);
-	json_add_u32(req->js, "groupid", p->groupid);
-	if (p->payment_metadata)
+	json_add_sha256(req->js, "payment_hash", &payment->payment_hash);
+	if (payment->label)
+		json_add_string(req->js, "label", payment->label);
+	json_add_amount_msat(req->js, "amount_msat", payment->amount);
+	json_add_string(req->js, "bolt11", payment->invstr);
+	if (payment->payment_secret)
+		json_add_secret(req->js, "payment_secret",
+				payment->payment_secret);
+	json_add_u32(req->js, "groupid", payment->groupid);
+	if (payment->payment_metadata)
 		json_add_hex_talarr(req->js, "payment_metadata",
-				    p->payment_metadata);
-	if (p->description)
-		json_add_string(req->js, "description", p->description);
+				    payment->payment_metadata);
+	if (payment->description)
+		json_add_string(req->js, "description", payment->description);
 
 	/* Pretend we have sent partid=1 with the total amount. */
-	p->next_partid = 2;
-	p->total_sent = p->amount;
+	payment->next_partid = 2;
+	payment->total_sent = payment->amount;
 	send_outreq(cmd->plugin, req);
 }
 
@@ -377,27 +381,27 @@ static void gossmod_cb(struct gossmap_localmods *mods,
 static struct command_result *listpeerchannels_ok(struct command *cmd,
 						  const char *buf,
 						  const jsmntok_t *result,
-						  struct payment *p)
+						  struct payment *payment)
 {
 	// FIXME: should local gossmods be global (ie. member of pay_plugin) or
 	// local (ie. member of payment)?
-	p->local_gossmods = gossmods_from_listpeerchannels(
-	    p, &pay_plugin->my_id, buf, result, gossmod_cb, p);
+	payment->local_gossmods = gossmods_from_listpeerchannels(
+	    payment, &pay_plugin->my_id, buf, result, gossmod_cb, payment);
 
-	payment_continue(p);
+	payment_continue(payment);
 	return command_still_pending(cmd);
 }
 
-static void getmychannels_cb(struct payment *p)
+static void getmychannels_cb(struct payment *payment)
 {
-	struct command *cmd = payment_command(p);
+	struct command *cmd = payment_command(payment);
 	if (!cmd)
 		plugin_err(pay_plugin->plugin,
 			   "getmychannels_pay_mod: cannot get a valid cmd.");
 
-	struct out_req *req =
-	    jsonrpc_request_start(cmd->plugin, cmd, "listpeerchannels",
-				  listpeerchannels_ok, payment_rpc_failure, p);
+	struct out_req *req = jsonrpc_request_start(
+	    cmd->plugin, cmd, "listpeerchannels", listpeerchannels_ok,
+	    payment_rpc_failure, payment);
 	send_outreq(cmd->plugin, req);
 }
 
@@ -408,7 +412,7 @@ REGISTER_PAYMENT_MODIFIER(getmychannels, getmychannels_cb);
  *
  * Update the gossmap.
  */
-static void refreshgossmap_cb(struct payment *p)
+static void refreshgossmap_cb(struct payment *payment)
 {
 	assert(pay_plugin->gossmap); // gossmap must be already initialized
 
@@ -426,7 +430,7 @@ static void refreshgossmap_cb(struct payment *p)
 		uncertainty_network_update(pay_plugin->gossmap,
 					   pay_plugin->chan_extra_map);
 
-	payment_continue(p);
+	payment_continue(payment);
 }
 
 REGISTER_PAYMENT_MODIFIER(refreshgossmap, refreshgossmap_cb);
@@ -438,13 +442,13 @@ REGISTER_PAYMENT_MODIFIER(refreshgossmap, refreshgossmap_cb);
  * network.
  */
 // TODO check how this is done in pay.c
-static void routehints_cb(struct payment *p)
+static void routehints_cb(struct payment *payment)
 {
 	// TODO(eduardo): are there route hints for B12?
 	// TODO: use unetwork instead of chan_extra_map
 	uncertainty_network_add_routehints(pay_plugin->chan_extra_map,
-					   p->routes, p);
-	payment_continue(p);
+					   payment->routes, payment);
+	payment_continue(payment);
 }
 
 REGISTER_PAYMENT_MODIFIER(routehints, routehints_cb);
@@ -514,13 +518,13 @@ REGISTER_PAYMENT_MODIFIER(compute_routes, compute_routes_cb);
  *
  * A dummy modifier used to end the payment, just for testing.
  */
-static void end_cb(struct payment *p)
+static void end_cb(struct payment *payment)
 {
 	// TODO flag a payment as failed
 	// payment_set_fail(
-	//     p, LIGHTNINGD,
+	//     payment, LIGHTNINGD,
 	//     "Failing the payment on purpose (call to end_pay_mod)");
-	payment_finish(p);
+	payment_finish(payment);
 }
 
 REGISTER_PAYMENT_MODIFIER(end, end_cb);
