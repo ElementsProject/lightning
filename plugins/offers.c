ElementsProject/lightning

commit 847208f5d80cffea30482cfca561609266be8dff

offers: put correct CLTV limit inside blinded paths.
At plugin startup, we don't have an accurate blockheight and can get 0!

Fixes: https://github.com/ElementsProject/lightning/issues/7161
Reported-by: @carlaKC
Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

plugins/offers.c
@@ -1227,8 +1227,12 @@ static const char *init(struct plugin *p,
 {
 	rpc_scan(p, "getinfo",
 		 take(json_out_obj(NULL, NULL, NULL)),
-		 "{id:%}", JSON_SCAN(json_to_pubkey, &id),
-		 "{blockheight:%}", JSON_SCAN(json_to_u32, &blockheight));
+		 "{id:%}", JSON_SCAN(json_to_pubkey, &id));
+
+	rpc_scan(p, "getchaininfo",
+		 take(json_out_obj(NULL, "last_height", "0")),
+		 "{headercount:%}", JSON_SCAN(json_to_u32, &blockheight));
+	assert(blockheight);
 
 	rpc_scan(p, "listconfigs",
 		 take(json_out_obj(NULL, NULL, NULL)),

lightningd/peer_htlcs.c
@@ -1102,7 +1102,8 @@ htlc_accepted_hook_final(struct htlc_accepted_hook_payload *request STEALS)
 	/* *Now* we barf if it failed to decode */
 	if (!request->payload) {
 		log_debug(channel->log,
-			  "Failing HTLC because of an invalid payload");
+			  "Failing HTLC because of an invalid payload (TLV %"PRIu64" pos %zu)",
+			  request->failtlvtype, request->failtlvpos);
 		local_fail_in_htlc(hin,
 				   take(towire_invalid_onion_payload(
 						NULL, request->failtlvtype,

commit 6cb4a722ef22041e21cd14119efcb9d00168ffe2

offer: fix type to the getchaininfo rpc
This is violate what the docs of getchainfo is telling us that the
last_block_height is a u64.

```
2024-05-17T09:53:23.464Z **BROKEN** plugin-offers: Got error reply to getchaininfo: '{\"error\":{\"code\":-1,\"data\":null,\"message\":\"invalid type: string \\\"0\\\", expected u64\"},\"id\":\"init/offers:getchaininfo#1\",\"jsonrpc\":\"2.0\"}\n\n'
2024-05-17T09:53:23.496Z INFO    plugin-offers: Killing plugin: exited before replying to init
2024-05-17T09:53:23.496Z **BROKEN** plugin-offers: Plugin marked as important, shutting down lightningd!
2024-05-17T09:53:23.496Z DEBUG   lightningd: io_break: lightningd_exit
2024-05-17T09:53:23.496Z **BROKEN** plugin-topology: Reading JSON input: Connection reset by peer
2024-05-17T09:53:23.504Z INFO    plugin-topology: Killing plugin: exited before replying to init
2024-05-17T09:53:23.504Z **BROKEN** plugin-topology: Plugin marked as important, shutting down lightningd!
2024-05-17T09:53:23.504Z DEBUG   lightningd: io_break: lightningd_exit
2024-05-17T09:53:23.504Z DEBUG   plugin-bookkeeper: Setting up database at sqlite3://accounts.sqlite3
2024-05-17T09:53:23.504Z DEBUG   connectd: REPLY WIRE_CONNECTD_START_SHUTDOWN_REPLY with 0 fds
2024-05-17T09:53:23.504Z DEBUG   lightningd: io_break: connectd_start_shutdown_reply
```

Fixing the following crash

```
Got error reply to getchaininfo: '{"error":{"code":-1,"data":null,"message":"invalid type: string \"0\", expected u64"},"id":"init/offers:getchaininfo#1","jsonrpc":"2.0"}

'lightningd: lightningd already running? Error locking PID file: Resource temporarily unavailable
```

Fixes: 847208f5d8
Changelog-None: offer: fix type to the getchaininfo rpc
Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

plugins/offers.c
@@ -1230,7 +1230,7 @@ static const char *init(struct plugin *p,
 		 "{id:%}", JSON_SCAN(json_to_pubkey, &id));
 
 	rpc_scan(p, "getchaininfo",
-		 take(json_out_obj(NULL, "last_height", "0")),
+		 take(json_out_obj(NULL, "last_height", NULL)),
 		 "{headercount:%}", JSON_SCAN(json_to_u32, &blockheight));
 	assert(blockheight);
 