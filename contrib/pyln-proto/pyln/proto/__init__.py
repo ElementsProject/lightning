c9f7b78473f46ca925cc062c1ab96c8d09256c7c
Alex Myers <alex@endothermic.dev>
2024-05-23 16:46:44 -0500

    meta: update python versions to 24.05
    Changelog-None

--- contrib/pyln-proto/pyln/proto/__init__.py
+++ contrib/pyln-proto/pyln/proto/__init__.py
@@ -4,7 +4,7 @@ from .invoice import Invoice
 from .onion import OnionPayload, TlvPayload, LegacyOnionPayload
 from .wire import LightningConnection, LightningServerSocket
 
-__version__ = "24.02"
+__version__ = "24.05rc1"
 
 __all__ = [
     "Invoice",
