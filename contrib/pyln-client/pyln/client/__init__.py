17b3f7a18a365fa4ac0591dbf6543d508c5d2e9f
Alex Myers <alex@endothermic.dev>
2024-05-29 15:59:15 -0500

    meta: update changelog for 24.05rc2

--- contrib/pyln-client/pyln/client/__init__.py
+++ contrib/pyln-client/pyln/client/__init__.py
@@ -3,7 +3,7 @@ from .plugin import Plugin, monkey_patch, RpcException
 from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapHalfchannel, GossmapNodeId, LnFeatureBits
 from .gossmapstats import GossmapStats
 
-__version__ = "24.05rc1"
+__version__ = "24.05rc2"
 
 __all__ = [
     "LightningRpc",
