0d4b9ad6ce2d42de49a81db2cf2e1a31f0719692
Rusty Russell <rusty@rustcorp.com.au>
2024-07-17 12:53:25 +0930

    offers: check they use the blinded path if one is specified.
    Now we can specify a blinded path in an offer, we have to check they
    used it (or didn't use it, if we didn't have one!).
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- plugins/offers_inv_hook.h
+++ plugins/offers_inv_hook.h
@@ -6,6 +6,7 @@
 /* We got an onionmessage with an invoice!  reply_path could be NULL. */
 struct command_result *handle_invoice(struct command *cmd,
 				      const u8 *invbin,
-				      struct blinded_path *reply_path STEALS);
+				      struct blinded_path *reply_path STEALS,
+				      const struct secret *secret);
 
 #endif /* LIGHTNING_PLUGINS_OFFERS_INV_HOOK_H */
