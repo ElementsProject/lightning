1db07a26535e30ac240799f75446da1f8003b1a7
Joseph Goulden <joegoulden@protonmail.com>
2024-09-16 09:18:06 +0100

    build: Add simple flake
    Changelog-None

---
+++ nix/pkgs/default.nix
@@ -0,0 +1,94 @@
+{
+  self,
+  lib,
+  pkgs,
+}:
+with pkgs;
+let
+  version = builtins.readFile ../../.version;
+  py3 = python3.withPackages (p: [
+    p.mako
+    p.grpcio-tools
+  ]);
+in
+stdenv.mkDerivation {
+  name = "cln";
+  src = ../../.;
+  inherit version;
+  makeFlags = [
+    "VERSION=${version}"
+    "MTIME=${self.lastModifiedDate}"
+  ];
+
+  # when building on darwin we need cctools to provide the correct libtool
+  # as libwally-core detects the host as darwin and tries to add the -static
+  # option to libtool, also we have to add the modified gsed package.
+  nativeBuildInputs =
+    [
+      autoconf
+      autogen
+      automake
+      cargo
+      gettext
+      gitMinimal
+      libtool
+      lowdown
+      protobuf
+      py3
+      unzip
+      which
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      cctools
+      darwin.autoSignDarwinBinariesHook
+    ];
+
+  buildInputs = [
+    gmp
+    jq
+    libsodium
+    sqlite
+    zlib
+  ];
+
+  # this causes some python trouble on a darwin host so we skip this step.
+  # also we have to tell libwally-core to use sed instead of gsed.
+  postPatch =
+    if !stdenv.isDarwin then
+      ''
+        patchShebangs \
+          tools/generate-wire.py \
+          tools/update-mocks.sh \
+          tools/mockup.sh \
+          tools/fromschema.py \
+          devtools/sql-rewrite.py
+      ''
+    else
+      ''
+        substituteInPlace external/libwally-core/tools/autogen.sh --replace gsed sed && \
+        substituteInPlace external/libwally-core/configure.ac --replace gsed sed
+      '';
+
+  configureFlags = [ "--disable-valgrind" ];
+
+  enableParallelBuilding = true;
+
+  # workaround for build issue, happens only x86_64-darwin, not aarch64-darwin
+  # ccan/ccan/fdpass/fdpass.c:16:8: error: variable length array folded to constant array as an extension [-Werror,-Wgnu-folding-constant]
+  #                 char buf[CMSG_SPACE(sizeof(fd))];
+  env.NIX_CFLAGS_COMPILE = lib.optionalString (
+    stdenv.isDarwin && stdenv.isx86_64
+  ) "-Wno-error=gnu-folding-constant";
+
+  # The `clnrest` plugin requires a Python environment to run
+  postInstall = ''
+    rm -r $out/libexec/c-lightning/plugins/clnrest
+  '';
+
+  meta = with lib; {
+    description = "Core Lightning (CLN): A specification compliant Lightning Network implementation in C";
+    homepage = "https://github.com/ElementsProject/lightning";
+    license = licenses.mit;
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
