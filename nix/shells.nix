fe7168b3351128c2ec91fdca934ce319539f0bcf
Joseph Goulden <joegoulden@protonmail.com>
2024-09-30 16:33:53 +0100

    build: Exclude python plugins from flake build

--- nix/shells.nix
+++ nix/shells.nix
@@ -9,6 +9,7 @@
     }:
     {
       devShells = {
+        default = pkgs.mkShell { inputsFrom = [ config.packages.default ]; };
         rust = pkgs.craneLib.devShell {
           checks = {
             inherit (self.checks.${system}) rust;
