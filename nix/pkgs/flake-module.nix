d49b5a53fbe21945654e892a3d52a5ab85598d4a
Joseph Goulden <joegoulden@protonmail.com>
2024-09-30 23:16:07 +0100

    build: Add cln-postgres package to nix flake

--- nix/pkgs/flake-module.nix
+++ nix/pkgs/flake-module.nix
@@ -6,6 +6,10 @@
       packages = rec {
         # This package depends on git submodules so use a shell command like 'nix build .?submodules=1'.
         cln = pkgs.callPackage ./default.nix { inherit self pkgs config; };
+        cln-postgres = pkgs.callPackage ./default.nix {
+          inherit self pkgs config;
+          postgresSupport = true;
+        };
         rust = pkgs.callPackage ./rust.nix { craneLib = pkgs.craneLib; };
         default = cln;
       };
--- nix/shells.nix
+++ nix/shells.nix
@@ -10,6 +10,7 @@
     {
       devShells = {
         default = pkgs.mkShell { inputsFrom = [ config.packages.default ]; };
+        postgres = pkgs.mkShell { inputsFrom = [ config.packages.cln-postgres ]; };
         rust = pkgs.craneLib.devShell {
           checks = {
             inherit (self.checks.${system}) rust;
