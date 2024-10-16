dda4704fc4d74516521f0c76d8d37b7436740a02
Lagrang3 <lagrang3@protonmail.com>
2024-10-16 08:54:46 +0100

    askrene: add dijkstra algorithm
    Changelog-EXPERIMENTAL: askrene: add dijkstra algorithm
    
    Signed-off-by: Lagrang3 <lagrang3@protonmail.com>

--- plugins/askrene/algorithm.h
+++ plugins/askrene/algorithm.h
@@ -33,4 +33,40 @@ bool BFS_path(const tal_t *ctx, const struct graph *graph,
 	      const struct node source, const struct node destination,
 	      const s64 *capacity, const s64 cap_threshold, struct arc *prev);
 
+
+/* Computes the distance from the source to every other node in the network
+ * using Dijkstra's algorithm.
+ *
+ * input:
+ * @ctx: tal context for internal allocation
+ * @graph: topological information of the graph
+ * @source: source node
+ * @destination: destination node
+ * @prune: if prune is true the algorithm stops when the optimal path is found
+ * for the destination node
+ * @capacity: arcs capacity
+ * @cap_threshold: an arc i is traversable if capacity[i]>=cap_threshold
+ * @cost: arc's cost
+ * @potential: nodes' potential, ie. reduced cost for an arc
+ * 	c_ij = cost_ij - potential[i] + potential[j]
+ *
+ * output:
+ * @prev: for each node, this is the arc that was used to arrive to it, this can
+ * be used to reconstruct the path from the destination to the source,
+ * @distance: node's best distance
+ * returns true if an optimal path is found for the destination, false otherwise
+ *
+ * precondition:
+ * |capacity|=|cost|=graph_max_num_arcs
+ * |prev|=|distance|=graph_max_num_nodes
+ * cost[i]>=0
+ * if prune is true the destination must be valid
+ * */
+bool dijkstra_path(const tal_t *ctx, const struct graph *graph,
+		   const struct node source, const struct node destination,
+		   bool prune, const s64 *capacity, const s64 cap_threshold,
+		   const s64 *cost, const s64 *potential, struct arc *prev,
+		   s64 *distance);
+
+
 #endif /* LIGHTNING_PLUGINS_ASKRENE_ALGORITHM_H */
