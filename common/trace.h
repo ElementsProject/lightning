4f3ea8c048d358152bace84a99a4a4632ea2d484
Christian Decker <decker.christian@gmail.com>
2024-11-21 12:16:56 +0100

    common: Add some debuggig capabilities to the trace subsystem
    After adding the DB query instrumentation we ran into a couple of
    issues, with spans not being resumed correctly, and it was rather hard
    to identify the problem. This adds debug statements so we can trace
    the tracing (traception if you will).
    
    Changelog-None

--- common/trace.h
+++ common/trace.h
@@ -8,10 +8,14 @@
 
 void trace_span_start(const char *name, const void *key);
 void trace_span_end(const void *key);
-void trace_span_suspend(const void *key);
-void trace_span_resume(const void *key);
 void trace_span_tag(const void *key, const char *name, const char *value);
 void trace_cleanup(void);
 void trace_span_remote(u8 trace_id[TRACE_ID_SIZE], u8 span_id[SPAN_ID_SIZE]);
+
+#define TRACE_LBL __FILE__ ":" stringify(__LINE__)
+void trace_span_suspend_(const void *key, const char *lbl);
+void trace_span_resume_(const void *key, const char *lbl);
+#define trace_span_suspend(key) trace_span_suspend_(key, TRACE_LBL)
+#define trace_span_resume(key) trace_span_resume_(key, TRACE_LBL)
 
 #endif /* LIGHTNING_COMMON_TRACE_H */
