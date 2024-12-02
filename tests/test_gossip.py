95652dede81088561a0ddd9f91452cfc4157c1ce
Rusty Russell <rusty@rustcorp.com.au>
2024-12-02 14:23:27 +1030

    pytest: fix test in test_gossip_pruning
    It's possible that listchannels doesn't show the channel yet:
    
    ```
        
            l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
            l2.rpc.connect(l3.info['id'], 'localhost', l3.port)
        
            scid1, _ = l1.fundchannel(l2, 10**6)
            scid2, _ = l2.fundchannel(l3, 10**6)
        
            mine_funding_to_announce(bitcoind, [l1, l2, l3])
    >       l1_initial_cupdate_timestamp = only_one(l1.rpc.listchannels(source=l1.info['id'])['channels'])['last_update']
    
    tests/test_gossip.py:43: 
    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
    
    arr = []
    
        def only_one(arr):
            """Many JSON RPC calls return an array; often we only expect a single entry
            """
    >       assert len(arr) == 1
    E       AssertionError
    ```
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/test_gossip.py
+++ tests/test_gossip.py
@@ -40,6 +40,7 @@ def test_gossip_pruning(node_factory, bitcoind):
     scid2, _ = l2.fundchannel(l3, 10**6)
 
     mine_funding_to_announce(bitcoind, [l1, l2, l3])
+    wait_for(lambda: l1.rpc.listchannels(source=l1.info['id'])['channels'] != [])
     l1_initial_cupdate_timestamp = only_one(l1.rpc.listchannels(source=l1.info['id'])['channels'])['last_update']
 
     # Get timestamps of initial updates, so we can ensure they change.
