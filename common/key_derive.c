35efd777c1e5c6908f4d74a0966d7c24f9888616
Rusty Russell <rusty@rustcorp.com.au>
2025-02-11 08:38:36 +1030

    common: update bolt to neaten pubkey descriptions.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- common/key_derive.c
+++ common/key_derive.c
@@ -13,12 +13,11 @@
  *
  *	pubkey = basepoint + SHA256(per_commitment_point || basepoint) * G
  *
- * The `localpubkey` uses the local node's `payment_basepoint`;
- * The `remotepubkey` uses the remote node's `payment_basepoint`;
- * the `local_htlcpubkey` uses the local node's `htlc_basepoint`;
- * the `remote_htlcpubkey` uses the remote node's `htlc_basepoint`;
- * the `local_delayedpubkey` uses the local node's `delayed_payment_basepoint`;
- * and the `remote_delayedpubkey` uses the remote node's `delayed_payment_basepoint`.
+ * - The `localpubkey` uses the local node's `payment_basepoint`;
+ * - The `local_htlcpubkey` uses the local node's `htlc_basepoint`;
+ * - The `remote_htlcpubkey` uses the remote node's `htlc_basepoint`;
+ * - The `local_delayedpubkey` uses the local node's `delayed_payment_basepoint`;
+ * - The `remote_delayedpubkey` uses the remote node's `delayed_payment_basepoint`.
  *...
  * The `remotepubkey` is simply the remote node's `payment_basepoint`.
  */
