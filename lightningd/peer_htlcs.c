72d913237f2be1b2ef1d41c3eb696caed044ec80
Aditya Sharma <aditya.sharma.met19@itbhu.ac.in>
2025-05-16 11:13:39 +0530

    Calculate hold_time for attribution data
    We initialise time of creating htlc_out and calculate the time when
    this htlc_out is failed.
    
    Key Changes:
     - Add 'send_timestamp' to htlc_out.
     - Calculate hold_time when htlc_out is failed.

--- lightningd/peer_htlcs.c
+++ lightningd/peer_htlcs.c
@@ -309,7 +309,12 @@ static void fail_out_htlc(struct htlc_out *hout, const char *localfail)
 			       hout->failmsg,
 			       localfail);
 	} else if (hout->in) {
-		const struct onionreply *failonion;
+		const struct onionreply *final_failonion;
+		struct onionreply *failonion;
+
+		struct timerel delta = time_between(hout->send_timestamp, hout->in->received_time);
+		long total_ms = delta.ts.tv_sec * 1000 + delta.ts.tv_nsec / 1000000;
+		u32 hold_times = total_ms / 100;
 
 		/* If we have an onion, simply copy it. */
 		if (hout->failonion)
@@ -731,7 +736,7 @@ const u8 *send_htlc_out(const tal_t *ctx,
 			      path_key, extra_tlvs,
 			      in == NULL,
 			      final_msat,
-			      partid, groupid, in);
+			      partid, groupid, in, time_now());
 	tal_add_destructor(*houtp, destroy_hout_subd_died);
 
 	/* Give channel 30 seconds to commit this htlc. */
@@ -1142,7 +1147,7 @@ static bool htlc_accepted_hook_deserialize(struct htlc_accepted_hook_payload *re
 					   " Ignoring 'failure_message'.");
 
 			fail_in_htlc(hin, take(new_onionreply(NULL,
-							      failonion)));
+							      failonion, NULL)));
 			return false;
 		}
 		if (!failmsgtok) {
