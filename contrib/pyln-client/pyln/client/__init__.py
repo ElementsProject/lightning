77f9a643c61ea669f326ea75d94d01b47d8bf839
Alex Myers <alex@endothermic.dev>
2025-05-16 13:03:20 -0500

    release: update version for 25.05rc1
    Changelog-None

--- contrib/pyln-client/pyln/client/__init__.py
+++ contrib/pyln-client/pyln/client/__init__.py
@@ -4,7 +4,7 @@ from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapHalfchannel, G
 from .gossmapstats import GossmapStats
 from .version import NodeVersion
 
-__version__ = "25.02"
+__version__ = "25.05rc1"
 
 __all__ = [
     "LightningRpc",
