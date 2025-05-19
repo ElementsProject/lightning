bc0a4d3038ce77954d6203efa1d88a6b717f8065
Chandra Pratap <chandrapratap3519@gmail.com>
2025-05-19 04:37:05 +0000

    fuzz-tests: Add a roundtrip check for `bigsize_put()`
    Add a roundtrip check for `bigsize_put()` using `bigsize_get()`.
    This enforces a stricter check for the former and adds a test
    for the latter, which is currently untested.

--- tests/fuzz/fuzz-bigsize.c
+++ tests/fuzz/fuzz-bigsize.c
@@ -14,15 +14,17 @@ void run(const uint8_t *data, size_t size)
 	const uint8_t **wire_chunks, *wire_ptr;
 	size_t wire_max;
 
-	for (size_t max = 1; max <= BIGSIZE_MAX_LEN; max++) {
-		wire_chunks = get_chunks(NULL, data, size, max);
+	for (size_t chunk_size = 1; chunk_size <= BIGSIZE_MAX_LEN; chunk_size++) {
+		wire_chunks = get_chunks(NULL, data, size, chunk_size);
 		for (size_t i = 0; i < tal_count(wire_chunks); i++) {
 			wire_max = tal_count(wire_chunks[i]);
 			wire_ptr = wire_chunks[i];
 
-			bigsize_t bs = fromwire_bigsize(&wire_ptr, &wire_max);
+			bigsize_t bs = fromwire_bigsize(&wire_ptr, &wire_max), bs_decoded;
 			assert(bigsize_put(buff, bs) > 0);
 			assert(bigsize_len(bs));
+			assert(bigsize_get(buff, sizeof(buff), &bs_decoded) == bigsize_len(bs));
+			assert(bs_decoded == bs);
 
 			wire_buff = tal_arr(NULL, uint8_t, 8);
 			towire_bigsize(&wire_buff, bs);
