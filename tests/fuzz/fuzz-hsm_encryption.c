4d94a114b5cdd773e12fdb103e73c5bf3ddbcadb
Chandra Pratap <chandrapratap3519@gmail.com>
2025-06-05 05:25:08 +0000

    fuzz-tests: get rid of magic numbers in `fuzz-hsm_encryption.c`
    Changelog-None: `fuzz-hsm_encryption.c` hard codes the lengths
    sizeof(struct secret) as 32 and crypto_pwhash_argon2id_PASSWD_MAX
    as 4294967295. Replace the latter with the former to improve
    readability and maintainability.
    
    While at it, replace the `tal_free()` call on our secret key with
    `discard_key()`. This has the benefit of testing `discard_key()`.

--- tests/fuzz/fuzz-hsm_encryption.c
+++ tests/fuzz/fuzz-hsm_encryption.c
@@ -11,9 +11,9 @@ void init(int *argc, char ***argv)
 
 void run(const uint8_t *data, size_t size)
 {
-	/* 4294967295 is crypto_pwhash_argon2id_PASSWD_MAX. libfuzzer won't
-	 * generate inputs that large in practice, but hey. */
-	if (size > 32 && size < 4294967295) {
+	/* LibFuzzer won't generate inputs larger than
+	 * crypto_pwhash_argon2id_PASSWD_MAX in practice, but hey. */
+	if (size > sizeof(struct secret) && size < crypto_pwhash_argon2id_PASSWD_MAX) {
 		struct secret *hsm_secret, decrypted_hsm_secret, encryption_key;
 		char *passphrase;
 		struct encrypted_hsm_secret encrypted_secret;
@@ -21,8 +21,8 @@ void run(const uint8_t *data, size_t size)
 
 		/* Take the first 32 bytes as the plaintext hsm_secret seed,
 		 * and the remaining ones as the passphrase. */
-		hsm_secret = (struct secret *)tal_dup_arr(NULL, u8, data, 32, 0);
-		passphrase = to_string(NULL, data + 32, size - 32);
+		hsm_secret = (struct secret *)tal_dup_arr(NULL, u8, data, sizeof(struct secret), 0);
+		passphrase = to_string(NULL, data + sizeof(struct secret), size - sizeof(struct secret));
 
 		/* A valid seed, a valid passphrase. This should not fail. */
 		assert(!hsm_secret_encryption_key_with_exitcode(passphrase, &encryption_key, &emsg));
@@ -35,7 +35,7 @@ void run(const uint8_t *data, size_t size)
 			     decrypted_hsm_secret.data,
 			     sizeof(decrypted_hsm_secret.data)));
 
-		tal_free(hsm_secret);
+		discard_key(hsm_secret);
 		tal_free(passphrase);
 	}
 }
