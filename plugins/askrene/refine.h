f7ba8f1ca9ac16348bcd4411f29c44954dd5b2dc
Lagrang3 <lagrang3@protonmail.com>
2025-07-09 20:34:55 +0100

    askrene: refine: remove refine_with_fee_and_limits
    and remove unused helper functions.
    
    Changelog-None
    
    Signed-off-by: Lagrang3 <lagrang3@protonmail.com>

--- plugins/askrene/refine.h
+++ plugins/askrene/refine.h
@@ -14,23 +14,6 @@ void create_flow_reservations(const struct route_query *rq,
 			      struct reserve_hop **reservations,
 			      const struct flow *flow);
 
-/* We got an answer from min-cost-flow, but we now need to:
- * 1. Add fixup exact delivery amounts since MCF deals in larger granularity than msat.
- * 2. Add fees which accumulate through the route.
- * 3. Check for htlc_minimum_msat violations (we simply remove those flows).
- * 4. Trim any flows which (after fees) now violate maximum htlc_minimum_msat/capacity bounds.
- *
- * We try to reassign missing sats to the remaining flows, which is usually easy.
- *
- * Returns NULL on success, or an error message for the caller.
- */
-const char *
-refine_with_fees_and_limits(const tal_t *ctx,
-			    struct route_query *rq,
-			    struct amount_msat deliver,
-			    struct flow ***flows,
-			    double *flowset_probability);
-
 /* create flow reservations, but first verify that the flow indeeds fits in the
  * liquidity constraints. Takes into account reservations that include per HTLC
  * extra amounts to pay for onchain fees. */
