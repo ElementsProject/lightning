da7d3057ed8367937b0666d690159814f33ce4d9
Sangbida Chaudhuri <101164840+sangbida@users.noreply.github.com>
2025-08-19 09:35:21 +0930

    Run version script for rc1.
    Run version script

--- contrib/pyln-client/pyln/client/__init__.py
+++ contrib/pyln-client/pyln/client/__init__.py
@@ -4,7 +4,7 @@ from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapHalfchannel, G
 from .gossmapstats import GossmapStats
 from .version import NodeVersion
 
-__version__ = "25.05"
+__version__ = "25.09rc1"
 
 __all__ = [
     "LightningRpc",
