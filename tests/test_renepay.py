5c536719b8bd911426676f6c87e2a9be0f7232b2
Matt Whitlock <c-lightning@mattwhitlock.name>
2025-08-24 05:16:57 -0400

    test_renepay.py: remove data dump into /tmp
    Changelog-None

--- tests/test_renepay.py
+++ tests/test_renepay.py
@@ -379,18 +379,10 @@ def test_hardmpp(node_factory):
         ]
     )
 
-    with open("/tmp/l1-chans.txt", "w") as f:
-        print(json.dumps(l1.rpc.listchannels()), file=f)
-
     inv = l4.rpc.invoice("any", "any", "description")
     l2.rpc.call("pay", {"bolt11": inv["bolt11"], "amount_msat": 2000000000})
     l2.wait_for_htlcs()
     assert l4.rpc.listinvoices()["invoices"][0]["amount_received_msat"] == 2000000000
-
-    with open("/tmp/l2-peerchan.txt", "w") as f:
-        print(json.dumps(l2.rpc.listpeerchannels()), file=f)
-    with open("/tmp/l3-peerchan.txt", "w") as f:
-        print(json.dumps(l3.rpc.listpeerchannels()), file=f)
 
     inv2 = l6.rpc.invoice("1800000sat", "inv2", "description")
 
