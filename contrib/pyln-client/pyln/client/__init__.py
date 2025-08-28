81838fabf9c100f688f837a626dcb3f004f04400
madelinevibes <madeline@blockstream.com>
2025-08-28 12:01:17 +0930

    updates for rc4: name changes and uv

--- contrib/pyln-client/pyln/client/__init__.py
+++ contrib/pyln-client/pyln/client/__init__.py
@@ -4,7 +4,7 @@ from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapHalfchannel, G
 from .gossmapstats import GossmapStats
 from .version import NodeVersion
 
-__version__ = "25.09rc3"
+__version__ = "25.09rc4"
 
 __all__ = [
     "LightningRpc",
