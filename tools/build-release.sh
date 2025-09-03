b1c29ca526934c185c250c18e3c9445f666061a3
Rusty Russell <rusty@rustcorp.com.au>
2025-09-03 13:24:01 +0930

    tools: insist on lowdown for zipfile.
    On GitHub where we build the zipfile, it exists, so we don't check out
    the submodule.  If you do, your zipfile won't match!
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -127,6 +127,8 @@ if [ "$WITHOUT_ZIP" = "false" ]; then
     # submodcheck needs to know if we have lowdown
     touch config.vars
     ./configure --reconfigure
+    # If you don't have lowdown, your zip file will include it: we assume everyone has it now!
+    grep -q "HAVE_LOWDOWN=1" config.vars || (echo "Please install lowdown" >&2; exit 1)
     # If it's a completely clean directory, we need submodules!
     make submodcheck
 
