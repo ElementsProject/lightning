221fc2c975f9697928ab0a2a35d535736da0684e
Rusty Russell <rusty@rustcorp.com.au>
2025-09-13 00:47:37 +0930

    lightningd: save funding_psbt in channel, database.
    Interestingly, @niftynei added a funding_psbt column to the db in 2020,
    but we don't use it (it was removed early 2021 with the "inflight"
    architecture).  So we don't need to add a new column, just plumb it
    back in.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lightningd/opening_control.c
+++ lightningd/opening_control.c
@@ -98,7 +98,8 @@ wallet_commit_channel(struct lightningd *ld,
 		      u32 feerate,
 		      const u8 *our_upfront_shutdown_script,
 		      const u8 *remote_upfront_shutdown_script,
-		      const struct channel_type *type)
+		      const struct channel_type *type,
+		      const struct wally_psbt *funding_psbt)
 {
 	struct channel *channel;
 	struct amount_msat our_msat;
@@ -237,7 +238,8 @@ wallet_commit_channel(struct lightningd *ld,
 			      NULL,
 			      0,
 			      &zero_channel_stats,
-			      tal_arr(NULL, struct channel_state_change *, 0));
+			      tal_arr(NULL, struct channel_state_change *, 0),
+			      funding_psbt);
 
 	/* Now we finally put it in the database. */
 	wallet_channel_insert(ld->wallet, channel);
@@ -445,7 +447,8 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 					feerate,
 					fc->our_upfront_shutdown_script,
 					remote_upfront_shutdown_script,
-					type);
+					type,
+					fc->funding_psbt);
 	if (!channel) {
 		was_pending(command_fail(fc->cmd, LIGHTNINGD,
 					 "Key generation failure"));
@@ -548,7 +551,8 @@ static void opening_fundee_finished(struct subd *openingd,
 					feerate,
 					local_upfront_shutdown_script,
 					remote_upfront_shutdown_script,
-					type);
+					type,
+					NULL);
 	if (!channel) {
 		uncommitted_channel_disconnect(uc, LOG_BROKEN,
 					       "Commit channel failed");
@@ -1091,6 +1095,8 @@ static struct command_result *json_fundchannel_complete(struct command *cmd,
 
 	if (command_check_only(cmd))
 		return command_check_done(cmd);
+
+	fc->funding_psbt = tal_steal(fc, funding_psbt);
 
 	/* Set the cmd to this new cmd */
 	peer->uncommitted_channel->fc->cmd = cmd;
@@ -1639,7 +1645,8 @@ static struct channel *stub_chan(struct command *cmd,
 			      NULL,
 			      0,
 			      &zero_channel_stats,
-			      tal_arr(NULL, struct channel_state_change *, 0));
+			      tal_arr(NULL, struct channel_state_change *, 0),
+			      NULL);
 
 	/* We don't want to gossip about this, ever. */
 	channel->channel_gossip = tal_free(channel->channel_gossip);
