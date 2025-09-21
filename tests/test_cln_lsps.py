581eb3076f6fd2e0d7bbb738bc963a2147a3af48
Peter Neuroth <pet.v.ne@gmail.com>
2025-09-21 20:09:46 +0200

    lsp_plugin: add lsps2_getinfo handler and call
    This commit adds the lsps2_get_info call defined by BLIP052. It also
    adds a test policy plugin that the LSP service plugin uses to fetch the
    actual fee menu from to separate the concerns of providing a spec
    compliant implementation of an LSP and making business decisions about
    fee prices.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- tests/test_cln_lsps.py
+++ tests/test_cln_lsps.py
@@ -30,6 +30,7 @@ def test_lsps0_listprotocols(node_factory):
     res = l1.rpc.lsps_listprotocols(lsp_id=l2.info['id'])
     assert res
 
+
 def test_lsps2_enabled(node_factory):
     l1, l2 = node_factory.get_nodes(2, opts=[
         {"dev-lsps-client-enabled": None},
@@ -44,3 +45,22 @@ def test_lsps2_enabled(node_factory):
 
     res = l1.rpc.lsps_listprotocols(lsp_id=l2.info['id'])
     assert res['protocols'] == [2]
+
+
+def test_lsps2_getinfo(node_factory):
+    plugin = os.path.join(os.path.dirname(__file__), 'plugins/lsps2_policy.py')
+
+    l1, l2 = node_factory.get_nodes(2, opts=[
+        {"dev-lsps-client-enabled": None},
+        {
+            "dev-lsps-service-enabled": None,
+            "dev-lsps2-service-enabled": None,
+            "dev-lsps2-promise-secret": "0" * 64,
+            "plugin": plugin
+        }
+    ])
+
+    node_factory.join_nodes([l1, l2], fundchannel=False)
+
+    res = l1.rpc.lsps_lsps2_getinfo(lsp_id=l2.info['id'])
+    assert res["opening_fee_params_menu"]
