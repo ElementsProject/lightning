a68f52918bf8200ec67afc9bbbe082b2be5569a1
Peter Neuroth <pet.v.ne@gmail.com>
2025-10-10 17:01:14 +0200

    test: refactor plugin to use parameter not envvar
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- tests/plugins/feature-test.py
+++ tests/plugins/feature-test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from pyln.client import Plugin
-import os
 
 # Register a different set feature of feature bits for each location so we can
 # later check that they are being passed correctly.
@@ -15,10 +14,10 @@ plugin = Plugin(
 
 @plugin.init()
 def init(configuration, options, plugin):
-    disable = os.getenv("PLUGIN_DISABLE")
-    if disable:
+    if options.get('disable-on-init'):
         return {'disable': 'init saying disable'}
     return {}
 
 
+plugin.add_option('disable-on-init', False, 'disable plugin on init', opt_type='bool')
 plugin.run()
