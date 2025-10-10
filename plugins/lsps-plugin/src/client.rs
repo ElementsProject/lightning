1a2d522d90bc6c60dcfd721f9051e2df1a9dc55b
Peter Neuroth <pet.v.ne@gmail.com>
2025-10-10 16:40:45 +0200

    lsp_plugin: remove reserve from hook response
    This slipped in during development but actually, we don't want to mess
    with the channel reservere here.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- plugins/lsps-plugin/src/client.rs
+++ plugins/lsps-plugin/src/client.rs
@@ -520,7 +520,6 @@ async fn on_openchannel(
         // found in the ds record.
         return Ok(serde_json::json!({
             "result": "continue",
-            "reserve": "0msat",
             "mindepth": 0,
         }));
     } else {
