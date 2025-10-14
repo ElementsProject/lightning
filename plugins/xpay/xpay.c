de516d1ed141c7f6fbea83a6122bea85eb83f066
Lagrang3 <lagrang3@protonmail.com>
2025-10-14 12:20:09 +0100

    xpay: penalize nodes that have failing channels
    Changelog-None
    
    Signed-off-by: Lagrang3 <lagrang3@protonmail.com>

--- plugins/xpay/xpay.c
+++ plugins/xpay/xpay.c
@@ -843,7 +843,7 @@ strange_error:
 			   describe_scidd(attempt, index));
 
 disable_channel:
-	/* We only do this for the current payment */
+	/* We disable this channel for the current payment */
 	req = payment_ignored_req(aux_cmd, attempt, "askrene-update-channel");
 	json_add_string(req->js, "layer", attempt->payment->private_layer);
 	json_add_short_channel_id_dir(req->js,
@@ -851,6 +851,31 @@ disable_channel:
 				      attempt->hops[index].scidd);
 	json_add_bool(req->js, "enabled", false);
 	send_payment_req(aux_cmd, attempt->payment, req);
+
+	/* We add a negative bias to this channel to penalize it for other
+	 * payments.
+	 * Biases are nice way to penalize or encourage the use of
+	 * channels. But it has some limitations. For example the bias would
+	 * have no effect if the channel already provides 0 cost routing. */
+	req = payment_ignored_req(aux_cmd, attempt, "askrene-bias-channel");
+	json_add_string(req->js, "layer", XPAY_GLOBAL_LAYER);
+	json_add_short_channel_id_dir(req->js, "short_channel_id_dir",
+				      attempt->hops[index].scidd);
+	json_add_s32(req->js, "bias", -5);
+	json_add_bool(req->js, "relative", true);
+	send_payment_req(aux_cmd, attempt->payment, req);
+
+	/* We add a negative bias to the node that owns this half channel. */
+	if (index) {
+		req =
+		    payment_ignored_req(aux_cmd, attempt, "askrene-bias-node");
+		json_add_string(req->js, "layer", XPAY_GLOBAL_LAYER);
+		json_add_pubkey(req->js, "node",
+				&attempt->hops[index - 1].next_node);
+		json_add_s32(req->js, "bias", -1);
+		json_add_bool(req->js, "relative", true);
+		send_payment_req(aux_cmd, attempt->payment, req);
+	}
 	goto check_previous_success;
 
 channel_capacity:
