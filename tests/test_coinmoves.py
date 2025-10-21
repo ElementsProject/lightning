5bf5f3b3ae7bb70a076589996d65dc73336eda59
Rusty Russell <rusty@rustcorp.com.au>
2025-10-22 10:27:23 +1030

    pytest: print useful information if we don't get our channelmoves/chainmoves
    The equality check will fail, but it will show is what is missing, rather than:
    
    FAILED tests/test_coinmoves.py::test_coinmoves_unilateral_htlc_fulfill - ValueError: Timeout while waiting for <function check_chain_moves.<locals>.<lambda> at 0x7f7800941ab0>
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/test_coinmoves.py
+++ tests/test_coinmoves.py
@@ -37,13 +37,21 @@ def check_moves(moves, expected):
 
 
 def check_channel_moves(node, expected):
-    wait_for(lambda: len(node.rpc.listchannelmoves()['channelmoves']) == len(expected))
+    # If this times out, show the result anyway.
+    try:
+        wait_for(lambda: len(node.rpc.listchannelmoves()['channelmoves']) == len(expected))
+    except ValueError:
+        print("*** Didn't see enough channelmoves")
     check_moves(node.rpc.listchannelmoves()['channelmoves'], expected)
     check_sql(node, "channelmoves", expected)
 
 
 def check_chain_moves(node, expected):
-    wait_for(lambda: len(node.rpc.listchainmoves()['chainmoves']) == len(expected))
+    # If this times out, show the result anyway.
+    try:
+        wait_for(lambda: len(node.rpc.listchainmoves()['chainmoves']) == len(expected))
+    except ValueError:
+        print("*** Didn't see enough chainmoves")
     check_moves(node.rpc.listchainmoves()['chainmoves'], expected)
     check_sql(node, "chainmoves", expected)
     # Check extra_tags.
