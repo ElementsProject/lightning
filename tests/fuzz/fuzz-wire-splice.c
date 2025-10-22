6225924068a10f25dc5aebd5958d712a3594c299
Rusty Russell <rusty@rustcorp.com.au>
2025-10-22 19:44:29 +1030

    fuzz: fix build undr FreeBSD.
    They have a `struct splice` already:
    
    ```
    2025-10-22T08:33:39.2432203Z tests/fuzz/fuzz-wire-splice.c:6:8: error: redefinition of 'splice'
    2025-10-22T08:33:39.2434453Z     6 | struct splice {
    2025-10-22T08:33:39.2435520Z       |        ^
    2025-10-22T08:33:39.2436087Z /usr/include/sys/socket.h:683:8: note: previous definition is here
    2025-10-22T08:33:39.2436709Z   683 | struct splice {
    ```
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/fuzz/fuzz-wire-splice.c
+++ tests/fuzz/fuzz-wire-splice.c
@@ -3,7 +3,7 @@
 #include <tests/fuzz/wire.h>
 #include <wire/peer_wire.h>
 
-struct splice {
+struct fuzzsplice {
 	struct channel_id channel_id;
 	s64 relative_satoshis;
 	u32 funding_feerate_perkw;
@@ -11,16 +11,16 @@ struct splice {
 	struct pubkey funding_pubkey;
 };
 
-static void *encode(const tal_t *ctx, const struct splice *s)
+static void *encode(const tal_t *ctx, const struct fuzzsplice *s)
 {
 	return towire_splice(ctx, &s->channel_id,
 			     s->relative_satoshis, s->funding_feerate_perkw,
 			     s->locktime, &s->funding_pubkey);
 }
 
-static struct splice *decode(const tal_t *ctx, const void *p)
+static struct fuzzsplice *decode(const tal_t *ctx, const void *p)
 {
-	struct splice *s = tal(ctx, struct splice);
+	struct fuzzsplice *s = tal(ctx, struct fuzzsplice);
 
 	if (fromwire_splice(p, &s->channel_id,
 			    &s->relative_satoshis, &s->funding_feerate_perkw,
@@ -29,12 +29,12 @@ static struct splice *decode(const tal_t *ctx, const void *p)
 	return tal_free(s);
 }
 
-static bool equal(const struct splice *x, const struct splice *y)
+static bool equal(const struct fuzzsplice *x, const struct fuzzsplice *y)
 {
 	return memcmp(x, y, sizeof(*x)) == 0;
 }
 
 void run(const u8 *data, size_t size)
 {
-	test_decode_encode(data, size, WIRE_SPLICE, struct splice);
+	test_decode_encode(data, size, WIRE_SPLICE, struct fuzzsplice);
 }
