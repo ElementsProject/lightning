097c8e57b55abe3a166c42481d5f90c28e83bd94
Rusty Russell <rusty@rustcorp.com.au>
2025-10-22 13:58:32 +1030

    lightningd: re-xmit funding txs on startup.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Changelog-Fixed: Protocol: we now re-transmit unseen funding transactions on startup, for more robustness.

--- wallet/test/run-wallet.c
+++ wallet/test/run-wallet.c
@@ -66,6 +66,16 @@ void bitcoind_getutxout_(const tal_t *ctx UNNEEDED,
 				    void *) UNNEEDED,
 			 void *arg UNNEEDED)
 { fprintf(stderr, "bitcoind_getutxout_ called!\n"); abort(); }
+/* Generated stub for bitcoind_sendrawtx_ */
+void bitcoind_sendrawtx_(const tal_t *ctx UNNEEDED,
+			 struct bitcoind *bitcoind UNNEEDED,
+			 const char *id_prefix TAKES UNNEEDED,
+			 const char *hextx UNNEEDED,
+			 bool allowhighfees UNNEEDED,
+			 void (*cb)(struct bitcoind * UNNEEDED,
+				    bool success UNNEEDED, const char *msg UNNEEDED, void *) UNNEEDED,
+			 void *arg UNNEEDED)
+{ fprintf(stderr, "bitcoind_sendrawtx_ called!\n"); abort(); }
 /* Generated stub for blinding_hash_e_and_ss */
 void blinding_hash_e_and_ss(const struct pubkey *e UNNEEDED,
 			    const struct secret *ss UNNEEDED,
