631a8ccdc1fbcc82e311728bfa0398b94ae63354
Rusty Russell <rusty@rustcorp.com.au>
2025-10-24 13:57:49 +1030

    fuzz: translate fuzz-hsm_encryption to fuzz-hsm_secret.
    Copy corpora, since it's the same code paths.
    
    Thanks Claude!
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ tests/fuzz/fuzz-hsm_secret.c
@@ -0,0 +1,56 @@
+#include "config.h"
+#include <assert.h>
+
+#include <ccan/mem/mem.h>
+#include <common/hsm_secret.h>
+#include <common/setup.h>
+#include <stdlib.h>
+#include <tests/fuzz/libfuzz.h>
+
+void init(int *argc, char ***argv)
+{
+	/* Don't run as a unit test under valgrind: too slow! */
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+	if (getenv("VALGRIND") && strcmp(getenv("VALGRIND"), "1") == 0) {
+		common_shutdown();
+		exit(0);
+	}
+#endif
+}
+
+void run(const uint8_t *data, size_t size)
+{
+	/* 4294967295 is crypto_pwhash_argon2id_PASSWD_MAX. libfuzzer won't
+	 * generate inputs that large in practice, but hey. */
+	if (size > 32 && size < 4294967295) {
+		struct secret *hsm_secret, *encryption_key;
+		char *passphrase;
+		u8 encrypted_data[ENCRYPTED_HSM_SECRET_LEN];
+		struct hsm_secret *decrypted_hsm;
+		enum hsm_secret_error err;
+
+		/* Take the first 32 bytes as the plaintext hsm_secret seed,
+		 * and the remaining ones as the passphrase. */
+		hsm_secret = (struct secret *)tal_dup_arr(NULL, u8, data, 32, 0);
+		passphrase = to_string(NULL, data + 32, size - 32);
+
+		/* A valid seed, a valid passphrase. This should not fail. */
+		encryption_key = get_encryption_key(NULL, passphrase);
+		assert(encryption_key);
+
+		/* Roundtrip: encrypt then decrypt */
+		assert(encrypt_legacy_hsm_secret(encryption_key, hsm_secret, encrypted_data));
+		decrypted_hsm = extract_hsm_secret(NULL, encrypted_data, ENCRYPTED_HSM_SECRET_LEN,
+						   passphrase, &err);
+		assert(decrypted_hsm);
+		assert(err == HSM_SECRET_OK);
+		assert(decrypted_hsm->type == HSM_SECRET_ENCRYPTED);
+		assert(memeq(hsm_secret->data, sizeof(hsm_secret->data),
+			     decrypted_hsm->secret_data, sizeof(hsm_secret->data)));
+
+		tal_free(hsm_secret);
+		tal_free(passphrase);
+		tal_free(encryption_key);
+		tal_free(decrypted_hsm);
+	}
+}
