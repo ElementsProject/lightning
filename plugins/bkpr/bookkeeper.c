673729bdb9a0e8115fa0df8feeac0df0f0a2b344
Rusty Russell <rusty@rustcorp.com.au>
2025-11-20 10:37:45 +1030

    Revert "bookkeeper: don't flood logs if we have many channelmoves all at once."
    This reverts commit 1dda0c07530d01dfa50f6121ba30530659a1475b so we can test
    what its like to be flooded with logs again.
    
    This benefits from other improvements we've made this release, to handling
    plugin input (i.e. converting to use common/jsonrpc_io), so this doesn't
    make much difference.
    
    tests/test_coinmoves.py::test_generate_coinmoves (100,000, sqlite3):
    	Time (from start to end of l2 node):	211 seconds
    	Worst latency:				108 seconds
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- plugins/bkpr/bookkeeper.c
+++ plugins/bkpr/bookkeeper.c
@@ -84,8 +84,7 @@ static void
 parse_and_log_channel_move(struct command *cmd,
 			   const char *buf,
 			   const jsmntok_t *channelmove,
-			   struct refresh_info *rinfo,
-			   bool log);
+			   struct refresh_info *rinfo);
 
 static struct command_result *datastore_done(struct command *cmd,
 					     const char *method,
@@ -118,15 +117,8 @@ static struct command_result *listchannelmoves_done(struct command *cmd,
 	be64 be_index;
 
 	moves = json_get_member(buf, result, "channelmoves");
-	if (moves->size > 2) {
-		plugin_log(cmd->plugin, LOG_DBG,
-			   "%u channelmoves, only logging first and last",
-			   moves->size);
-	}
-
 	json_for_each_arr(i, t, moves)
-		parse_and_log_channel_move(cmd, buf, t, rinfo,
-					   i == 0 || i == moves->size - 1);
+		parse_and_log_channel_move(cmd, buf, t, rinfo);
 
 	be_index = cpu_to_be64(bkpr->channelmoves_index);
 	jsonrpc_set_datastore_binary(cmd, "bookkeeper/channelmoves_index",
@@ -1268,8 +1260,7 @@ static void
 parse_and_log_channel_move(struct command *cmd,
 			   const char *buf,
 			   const jsmntok_t *channelmove,
-			   struct refresh_info *rinfo,
-			   bool log)
+			   struct refresh_info *rinfo)
 {
 	struct channel_event *e = tal(cmd, struct channel_event);
 	struct account *acct;
@@ -1316,12 +1307,11 @@ parse_and_log_channel_move(struct command *cmd,
 		err = tal_free(err);
 	}
 
-	if (log)
-		plugin_log(cmd->plugin, LOG_DBG, "coin_move 2 (%s) %s -%s %s %"PRIu64,
-			   e->tag,
-			   fmt_amount_msat(tmpctx, e->credit),
-			   fmt_amount_msat(tmpctx, e->debit),
-			   CHANNEL_MOVE, e->timestamp);
+	plugin_log(cmd->plugin, LOG_DBG, "coin_move 2 (%s) %s -%s %s %"PRIu64,
+		   e->tag,
+		   fmt_amount_msat(tmpctx, e->credit),
+		   fmt_amount_msat(tmpctx, e->debit),
+		   CHANNEL_MOVE, e->timestamp);
 
 	/* Go find the account for this event */
 	acct = find_account(bkpr, acct_name);
