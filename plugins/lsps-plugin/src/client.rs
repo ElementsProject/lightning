cbd2cf0e5d179b05ccee0f5c1b34a218332eba14
Peter Neuroth <pet.v.ne@gmail.com>
2025-11-24 14:40:18 +0100

    plugins: lsps: move feature types into proto module
    This commit is part of a series that refactor the lsp plugin crate into
    a modularized crate with the goal to separate the actual plugin runtime
    as much as possible from library code to make it accessible for 3rd
    party plugin implementations.
    
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- plugins/lsps-plugin/src/client.rs
+++ plugins/lsps-plugin/src/client.rs
@@ -14,10 +14,11 @@ use cln_lsps::lsps2::model::{
     compute_opening_fee, Lsps2BuyRequest, Lsps2BuyResponse, Lsps2GetInfoRequest,
     Lsps2GetInfoResponse, OpeningFeeParams,
 };
-use cln_lsps::proto::lsps0::{Lsps0listProtocolsRequest, Lsps0listProtocolsResponse};
+use cln_lsps::proto::lsps0::{
+    Lsps0listProtocolsRequest, Lsps0listProtocolsResponse, LSP_FEATURE_BIT,
+};
 use cln_lsps::proto::primitives::Msat;
 use cln_lsps::util;
-use cln_lsps::LSP_FEATURE_BIT;
 use cln_plugin::options;
 use cln_rpc::model::requests::{
     DatastoreMode, DatastoreRequest, DeldatastoreRequest, DelinvoiceRequest, DelinvoiceStatus,
