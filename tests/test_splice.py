d9bf91107ce4424859baea8dd0562cfcd16b8b9d
Dusty Daemon <dustinpaystaxes@gmail.com>
2026-01-20 11:59:50 -0500

    splice: Add easy spliceout command
    Adds a RPC command for easily splicing out. Built on top of the splice script framework.
    
    Changelog-Added: New command `spliceout` for easily splicing out of channels

--- tests/test_splice.py
+++ tests/test_splice.py
@@ -621,7 +621,6 @@ def test_script_two_chan_splice_z(node_factory, bitcoind):
                    [500000 + int(100000 * 0.7), 500000 + int(100000 * 0.3)], [-0.7, -0.3])
 
 
-@pytest.mark.xfail(strict=True)
 @pytest.mark.openchannel('v1')
 @pytest.mark.openchannel('v2')
 @unittest.skipIf(TEST_NETWORK != 'regtest', 'elementsd doesnt yet support PSBT features we need')
