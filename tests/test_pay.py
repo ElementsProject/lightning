dae9140a8bba7c6ce9fafc5dd768da05dddefdbd
Rusty Russell <rusty@rustcorp.com.au>
2026-01-21 06:29:28 +1030

    pytest: parameterize test_payerkey and test_exposesecret.
    These are explicitly written to check that the values don't accidentally change,
    which applies to both old and new styles.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/test_pay.py
+++ tests/test_pay.py
@@ -5350,18 +5350,28 @@ def test_pay_middle_fail(node_factory, bitcoind, executor):
 
 
 @unittest.skipIf(TEST_NETWORK != 'regtest', "Invoice is network specific")
+@pytest.mark.parametrize("old_hsmsecret", [True, False])
 @pytest.mark.slow_test
-def test_payerkey(node_factory):
+def test_payerkey(node_factory, old_hsmsecret):
     """payerkey calculation should not change across releases!"""
-    nodes = node_factory.get_nodes(7)
+    nodes = node_factory.get_nodes(7, opts={'old_hsmsecret': old_hsmsecret})
 
-    expected_keys = ["035e43e4ec029ee6cc0e320ebefdf863bc0f284ec0208275f780837d17e21bba32",
-                     "02411811b24f4940de49ad460ee14ecb96810e29ca49cdd3600a985da2eda06b87",
-                     "036a19f00424ff244af1841715e89f3716c08f1f62a8e5d9bd0f69a21aa96a7b8d",
-                     "026d8b82fe6039fe16f8ef376174b630247e821331b90620315a1e9c3db8384056",
-                     "0393fb950e04916c063a585aa644df3d72642c16de4eb44ccf5dbede194836140f",
-                     "030b68257230f7057e694222bbd54d9d108decced6b647a90da6f578360af53f7d",
-                     "02f402bd7374a1304b07c7236d9c683b83f81072517195ddede8ab328026d53157"]
+    if old_hsmsecret:
+        expected_keys = ["035e43e4ec029ee6cc0e320ebefdf863bc0f284ec0208275f780837d17e21bba32",
+                         "02411811b24f4940de49ad460ee14ecb96810e29ca49cdd3600a985da2eda06b87",
+                         "036a19f00424ff244af1841715e89f3716c08f1f62a8e5d9bd0f69a21aa96a7b8d",
+                         "026d8b82fe6039fe16f8ef376174b630247e821331b90620315a1e9c3db8384056",
+                         "0393fb950e04916c063a585aa644df3d72642c16de4eb44ccf5dbede194836140f",
+                         "030b68257230f7057e694222bbd54d9d108decced6b647a90da6f578360af53f7d",
+                         "02f402bd7374a1304b07c7236d9c683b83f81072517195ddede8ab328026d53157"]
+    else:
+        expected_keys = ["027c4ec2cf63aeb101109d6d16b9a97e74cd9a149860c16a9fd6ddddf18d337193",
+                         "028cd5b653ba55558095e66c0c42c43dd1077598a8d08a097387383dece4c2a325",
+                         "02bf5fe9675aed4728c7ed6afb3dab02143cbb693d2c49f62d4c28704d9877fbd0",
+                         "037b07ee0335a92f47e803d6ce5500dca721b43a1a9bc4a6ea6b75db4508c7b7de",
+                         "02484674b6acc7f3ddb4c5b13ef14ab0b018e748f4f3bffc55ab36dc57fdff6ec5",
+                         "024d11164cfd85c0f9383dd78703d3ad533e5af57bc5a09c1c1dae595f212941ae",
+                         "0369fb4a73973a31f7d357f4ec39fcc5663f9abae8b5fd47d1a3ada04ee4ae5656"]
 
     bolt12tool = os.path.join(os.path.dirname(__file__), "..", "devtools", "bolt12-cli")
 
