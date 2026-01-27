17e72b37119e5aab3522d0397d2e9d765fff4bec
Rusty Russell <rusty@rustcorp.com.au>
2026-01-28 09:06:18 +1030

    askrene: expose additional_costs htable so child can access it.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- plugins/askrene/askrene.c
+++ plugins/askrene/askrene.c
@@ -22,37 +22,13 @@
 #include <inttypes.h>
 #include <math.h>
 #include <plugins/askrene/askrene.h>
+#include <plugins/askrene/child/additional_costs.h>
 #include <plugins/askrene/child/child_log.h>
 #include <plugins/askrene/child/entry.h>
 #include <plugins/askrene/layer.h>
 #include <plugins/askrene/reserve.h>
 #include <sys/wait.h>
 #include <wire/wire_sync.h>
-
-/* "spendable" for a channel assumes a single HTLC: for additional HTLCs,
- * the need to pay for fees (if we're the owner) reduces it */
-struct per_htlc_cost {
-	struct short_channel_id_dir scidd;
-	struct amount_msat per_htlc_cost;
-};
-
-static const struct short_channel_id_dir *
-per_htlc_cost_key(const struct per_htlc_cost *phc)
-{
-	return &phc->scidd;
-}
-
-static inline bool per_htlc_cost_eq_key(const struct per_htlc_cost *phc,
-					const struct short_channel_id_dir *scidd)
-{
-	return short_channel_id_dir_eq(scidd, &phc->scidd);
-}
-
-HTABLE_DEFINE_NODUPS_TYPE(struct per_htlc_cost,
-			  per_htlc_cost_key,
-			  hash_scidd,
-			  per_htlc_cost_eq_key,
-			  additional_cost_htable);
 
 static bool have_layer(const char **layers, const char *name)
 {
