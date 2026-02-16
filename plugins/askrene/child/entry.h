33e2f0a47bc587609a30d107c9d0af29ba1431b1
Rusty Russell <rusty@rustcorp.com.au>
2026-02-16 17:31:33 +1030

    askrene: move fork() entry point into its own file.
    Now there's only one file clearly shared by both parent and child.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ plugins/askrene/child/entry.h
@@ -0,0 +1,29 @@
+#ifndef LIGHTNING_PLUGINS_ASKRENE_CHILD_ENTRY_H
+#define LIGHTNING_PLUGINS_ASKRENE_CHILD_ENTRY_H
+#include "config.h"
+#include <ccan/short_types/short_types.h>
+#include <ccan/time/time.h>
+#include <common/amount.h>
+#include <stdbool.h>
+
+struct route_query;
+struct gossmap_node;
+struct json_filter;
+
+/* Entry point to the child process. */
+int fork_router_child(struct route_query *rq,
+		      bool single_path,
+		      struct timemono deadline,
+		      const struct gossmap_node *srcnode,
+		      const struct gossmap_node *dstnode,
+		      struct amount_msat amount, struct amount_msat maxfee,
+		      u32 finalcltv, u32 maxdelay,
+		      bool include_fees,
+		      const char *cmd_id,
+		      struct json_filter *cmd_filter,
+		      int *log_fd,
+		      int *child_pid);
+
+/* FIXME: Remove this */
+extern bool am_child;
+#endif /* LIGHTNING_PLUGINS_ASKRENE_CHILD_ENTRY_H */
