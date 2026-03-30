ElementsProject/lightning

commit 40dd780ea79478950e6c84bd22d8ea5768069237

bitcoin_block_from_hex: avoid creating PSBT wrappers for finalized block txs
Changelog-None

Signed-off-by: Jon Griffiths <jon_p_griffiths@yahoo.com>

bitcoin/block.c
@@ -210,7 +210,7 @@ bitcoin_block_from_hex(const tal_t *ctx, const struct chainparams *chainparams,
 	b->tx = tal_arr(b, struct bitcoin_tx *, num);
 	b->txids = tal_arr(b, struct bitcoin_txid, num);
 	for (i = 0; i < num; i++) {
-		b->tx[i] = pull_bitcoin_tx(b->tx, &p, &len);
+		b->tx[i] = pull_bitcoin_tx_only(b->tx, &p, &len);
 		b->tx[i]->chainparams = chainparams;
 		bitcoin_txid(b->tx[i], &b->txids[i]);
 	}

commit 38c5e56e34107c21b2e6e184ce78955890860ed0

bitcoin/block.c: add null check after pull_bitcoin_tx_only
Commit 40dd780ea switched from pull_bitcoin_tx to pull_bitcoin_tx_only
but did not add a null check, causing a segfault (FATAL SIGNAL 11) if
pull_bitcoin_tx_only returns NULL due to a malformed block response
from bitcoind.

Add null check consistent with existing patterns in the codebase and
with pull_bitcoin_tx itself.

Fixes: 40dd780ea bitcoin_block_from_hex: avoid creating PSBT wrappers for finalized block txs

bitcoin/block.c
@@ -210,6 +210,8 @@ bitcoin_block_from_hex(const tal_t *ctx, const struct chainparams *chainparams,
 	b->txids = tal_arr(b, struct bitcoin_txid, num);
 	for (i = 0; i < num; i++) {
 		b->tx[i] = pull_bitcoin_tx_only(b->tx, &p, &len);
+		if (!b->tx[i])
+    			return tal_free(b);
 		b->tx[i]->chainparams = chainparams;
 		bitcoin_txid(b->tx[i], &b->txids[i]);
 	}