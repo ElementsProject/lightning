2797a8c4afbef093fd65a00464c24262f1627e0f
Sangbida Chaudhuri <sangbidac@gmail.com>
2026-04-23 13:29:30 +0930

    bwatch: add skeleton and makefile
    bwatch is an async block scanner that consumes blocks from bcli or any
    other bitcoind interface and communicates with lightningd by sending
    it updates. In this commit we're only introducing the plugin and some
    files that we will populate in future commits.
    
    Changelog-Added: bwatch plugin, this is to handle block processing outside of lightningd. Not yet hooked up to lightningd.

---
+++ plugins/bwatch/bwatch_store.h
@@ -0,0 +1,13 @@
+#ifndef LIGHTNING_PLUGINS_BWATCH_BWATCH_STORE_H
+#define LIGHTNING_PLUGINS_BWATCH_BWATCH_STORE_H
+
+#include "config.h"
+#include <plugins/bwatch/bwatch.h>
+
+/* Block-history and watch storage layer for bwatch.
+ *
+ * Subsequent commits populate this with hash tables for each watch type
+ * (scriptpubkey, outpoint, scid, blockdepth) plus the lightningd datastore
+ * persistence helpers. */
+
+#endif /* LIGHTNING_PLUGINS_BWATCH_BWATCH_STORE_H */
