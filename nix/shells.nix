e124bff57296d7654a1831b977d1e2a0d474aca0
niftynei <niftynei@gmail.com>
2026-06-23 12:39:12 +0300

    nix shells: add uv to packages
    Changelog-Added: nix: development shells now include `uv` for Python dependency and command execution.

--- nix/shells.nix
+++ nix/shells.nix
@@ -9,12 +9,19 @@
     }:
     {
       devShells = {
-        default = pkgs.mkShell { inputsFrom = [ config.packages.default ]; };
-        postgres = pkgs.mkShell { inputsFrom = [ config.packages.cln-postgres ]; };
+        default = pkgs.mkShell {
+          inputsFrom = [ config.packages.default ];
+          packages = [ pkgs.uv ];
+        };
+        postgres = pkgs.mkShell {
+          inputsFrom = [ config.packages.cln-postgres ];
+          packages = [ pkgs.uv ];
+        };
         rust = pkgs.craneLib.devShell {
           checks = {
             inherit (self.checks.${system}) rust;
           };
+          packages = [ pkgs.uv ];
         };
       };
     };
