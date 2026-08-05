5c8bf7841c142b0622af8ffae4e3edc338d30a23
Sangbida Chaudhuri <sangbidac@gmail.com>
2026-08-05 12:23:31 +0930

    tests: drop obsolete x-prefix in compacter-slow.sh
    Shellcheck 0.11 (SC2268) rejects the old x"$1" comparison idiom.
    
    Prevents: "tests/plugins/compacter-slow.sh:5:6: note: Avoid x-prefix in comparisons as it no longer serves a purpose. [SC2268]"

--- tests/plugins/compacter-slow.sh
+++ tests/plugins/compacter-slow.sh
@@ -2,7 +2,7 @@
 # This pretends to be lightning_gossip_compactd, but waits until the file "compactd-continue"
 # exists.  This lets us test race conditions.
 
-if [ x"$1" != x"--version" ]; then
+if [ "$1" != "--version" ]; then
     while [ ! -f "compactd-continue" ]; do
 	sleep 1
     done
