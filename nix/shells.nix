8175caf63b53cbc9da6d4d685ec7ce6b87cac63a
niftynei <niftynei@gmail.com>
2026-08-08 16:35:06 -0500

    nix shell: add shellhook for cleaning up contrib/startup_regtest.sh
    If you're running any startup_regtest daemons in the shell, this
    will attempt to stop them on exit from the development shell.
    
    Changelog-Added: nix: development shells now stop `startup_regtest.sh` daemons when the shell exits.

--- nix/shells.nix
+++ nix/shells.nix
@@ -20,22 +20,34 @@
         pkgs.uv
         gsed
       ];
+      shellHook = ''
+        _cln_stop_regtest_on_exit() {
+          if command -v stop_ln >/dev/null 2>&1; then
+            stop_ln >/dev/null 2>&1 || true
+          fi
+        }
+
+        trap _cln_stop_regtest_on_exit EXIT
+      '';
     in
     {
       devShells = {
         default = pkgs.mkShell {
           inputsFrom = [ config.packages.default ];
           packages = devTools;
+          inherit shellHook;
         };
         postgres = pkgs.mkShell {
           inputsFrom = [ config.packages.cln-postgres ];
           packages = devTools;
+          inherit shellHook;
         };
         rust = pkgs.craneLib.devShell {
           checks = {
             inherit (self.checks.${system}) rust;
           };
           packages = devTools;
+          inherit shellHook;
         };
       };
     };
